acd3a941a9001a8cfad924cb2259c61708e15855
Moritz Kassner <moritzkassner@gmail.com>
2012-12-22 11:58:16 +0100

    more clean-up.

--- Capture/main.py
+++ Capture/main.py
@@ -15,22 +15,17 @@ from methods import Temp, local_grab
 # import waveatb.
 # from audio import normalize, trim, add_silence
 
-from ctypes import *
+from ctypes import c_bool, c_int
 
 def main():
 	#assign the right id to the cameras
 	eye_src = 1
 	world_src = 0
+
+	#video size
 	eye_size = (640,320)
 	world_size = (1280,720)
-	# world_size = (640,480)
-
-
-	# when using some h264 compression cameras
-	# you can't run world camera grabber in its own process
-	# it must reside in the main loop
-	# this is all taken care of by setting this to true
-	# muliprocess_cam = 1
+	# world_size = (640,480) # if your cpu is to slow use this setting
 
 	#use video for debugging
 	use_video = 0
@@ -40,16 +35,11 @@ def main():
 	# use the player: a seperate window for video playback and 9 point calibration animation
 	use_player = 1
 
+
+
 	if use_video:
 		eye_src = "/Users/mkassner/MIT/pupil_google_code/wiki/videos/green_eye_VISandIR_2.mov" # using a path to a videofiles allows for developement without a headset.
 		world_src = 0
-
-
-	# if muliprocess_cam:
-	# 	world_id = world_src
-	# 	world_src, world_feed = Pipe()
-		# world_id = world_src
-		# world_src, world_feed = Pipe()
 
 	# create shared globals
 	g_pool = Temp()
@@ -64,7 +54,6 @@ def main():
 	g_pool.cal9_step = Value('i', 0)
 	g_pool.cal9_circle_id = RawValue('i' ,0)
 	g_pool.pos_record = Value(c_bool, 0)
-	g_pool.eye_rx, g_pool.eye_tx = Pipe(False)
 	g_pool.audio_record = Value(c_bool,False)
 	g_pool.audio_rx, g_pool.audio_tx = Pipe(False)
 	g_pool.player_refresh = Event()
@@ -74,28 +63,21 @@ def main():
 
 	# set up sub processes
 	p_eye = Process(target=eye, args=(eye_src,eye_size, g_pool))
-	# p_world = Process(target=world, args=(world_src,world_size,g_pool))
 	if use_player: p_player = Process(target=player, args=(g_pool,))
 	if audio: p_audio = Process(target=record_audio, args=(audio_rx,audio_record,3))
 
 	# spawn sub processes
-	# p_world.start()
-	# sleep(.3)
 	p_eye.start()
-	sleep(.3)
 	if use_player: p_player.start()
 	if audio: p_audio.start()
 
-
+	# when using some cameras (like our current worldcamera logitch c510)
+	# you can't run world camera grabber in its own process
+	# it must reside in the main loop
 	world(world_src,world_size,g_pool)
-	# if muliprocess_cam:
-	# 	local_grab(world_feed,world_id,g_pool)
-		# local_grab_threaded(world_feed,world_id,eye_feed,eye_id,g_pool)
-
 
 	# exit / clean-up
 	p_eye.join()
-	# p_world.join()
 	if use_player: p_player.join()
 	if audio: p_audio.join()
 	print "main exit"
