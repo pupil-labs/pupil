cbdbd7abb0f65c2843bee0545a0b45cbaa0bd18b
Moritz Kassner <moritzkassner@gmail.com>
2013-03-19 11:48:01 +0100

    removed profiler

--- Capture/main.py
+++ Capture/main.py
@@ -107,7 +107,7 @@ def main():
 	# end shared globals
 
 	# set up sub processes
-	p_eye = Process(target=eye_profiled, args=(eye_src,eye_size, g_pool))
+	p_eye = Process(target=eye, args=(eye_src,eye_size, g_pool))
 	if use_player: p_player = Process(target=player, args=(g_pool,player_size))
 	if audio: p_audio = Process(target=record_audio, args=(g_pool.audio_rx,g.g_pool.audio_record,3))
 
@@ -119,7 +119,7 @@ def main():
 	# when using some cameras (like our current worldcamera logitch c510)
 	# you can't run world camera grabber in its own process
 	# it must reside in the main loop when you run on MacOS.
-	world_profiled(world_src,world_size,g_pool)
+	world(world_src,world_size,g_pool)
 
 	# exit / clean-up
 	p_eye.join()
