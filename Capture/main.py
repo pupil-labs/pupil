28b6d6246ef3e7a687dbd9c3f912f095944a8e94
Moritz Kassner <moritzkassner@gmail.com>
2013-03-27 00:36:58 +0100

    added a sleep between world and eye process start to give the linux camera driver some time.

--- Capture/main.py
+++ Capture/main.py
@@ -19,30 +19,30 @@ from ctypes import c_bool, c_int
 
 
 def main():
-	#assign the right id to the cameras
-	eye_src = 1
-	world_src = 0
+    #assign the right id to the cameras
+    eye_src = 0
+    world_src = 1
 
-	#video size
-	eye_size = (640,360)
-	"""
-		HD-6000 v4l2-ctl -d /dev/video0 --list-formats-ext
-		Size: Discrete 640x480
-		Size: Discrete 1280x720
-		Size: Discrete 960x544
-		Size: Discrete 800x448
-		Size: Discrete 640x360
-		Size: Discrete 800x600
-		Size: Discrete 416x240
-		Size: Discrete 352x288
-		Size: Discrete 176x144
-		Size: Discrete 320x240
-		Size: Discrete 160x120
-	"""
-	world_size = (1280,720)
-	"""
-		c-525 v4l2-ctl -d /dev/video0 --list-formats-ext
-	    Size: Discrete 640x480
+    #video size
+    eye_size = (640,360)
+    """
+        HD-6000 v4l2-ctl -d /dev/video0 --list-formats-ext
+        Size: Discrete 640x480
+        Size: Discrete 1280x720
+        Size: Discrete 960x544
+        Size: Discrete 800x448
+        Size: Discrete 640x360
+        Size: Discrete 800x600
+        Size: Discrete 416x240
+        Size: Discrete 352x288
+        Size: Discrete 176x144
+        Size: Discrete 320x240
+        Size: Discrete 160x120
+    """
+    world_size = (1280,720)
+    """
+        c-525 v4l2-ctl -d /dev/video0 --list-formats-ext
+        Size: Discrete 640x480
         Size: Discrete 160x120
         Size: Discrete 176x144
         Size: Discrete 320x176
@@ -69,63 +69,66 @@ def main():
         Size: Discrete 1920x1080
     """
 
-	player_size = (800,600)
+    player_size = (800,600)
 
-	#use video for debugging
-	use_video = 0
+    #use video for debugging
+    use_video = 0
 
-	audio = False
+    audio = False
 
-	# use the player: a seperate window for video playback and 9 point calibration animation
-	use_player = 1
+    # use the player: a seperate window for video playback and 9 point calibration animation
+    use_player = 1
 
 
-	if use_video:
-		eye_src = "/Users/mkassner/Pupil/pupil_google_code/wiki/videos/green_eye_VISandIR_2.mov" # using a path to a videofiles allows for developement without a headset.
-		world_src = 0
+    if use_video:
+        eye_src = "/Users/mkassner/Pupil/pupil_google_code/wiki/videos/green_eye_VISandIR_2.mov" # using a path to a videofiles allows for developement without a headset.
+        world_src = 0
 
-	# create shared globals
-	g_pool = Temp()
-	g_pool.gaze_x = Value('d', 0.0)
-	g_pool.gaze_y = Value('d', 0.0)
-	g_pool.pattern_x = Value('d', 0.0)
-	g_pool.pattern_y = Value('d', 0.0)
-	g_pool.frame_count_record = Value('i', 0)
-	g_pool.calibrate = RawValue(c_bool, 0)
-	g_pool.cal9 = RawValue(c_bool, 0)
-	g_pool.cal9_stage = Value('i', 0)
-	g_pool.cal9_step = Value('i', 0)
-	g_pool.cal9_circle_id = RawValue('i' ,0)
-	g_pool.pos_record = Value(c_bool, 0)
-	g_pool.eye_rx, g_pool.eye_tx = Pipe(False)
+    # create shared globals
+    g_pool = Temp()
+    g_pool.gaze_x = Value('d', 0.0)
+    g_pool.gaze_y = Value('d', 0.0)
+    g_pool.pattern_x = Value('d', 0.0)
+    g_pool.pattern_y = Value('d', 0.0)
+    g_pool.frame_count_record = Value('i', 0)
+    g_pool.calibrate = RawValue(c_bool, 0)
+    g_pool.cal9 = RawValue(c_bool, 0)
+    g_pool.cal9_stage = Value('i', 0)
+    g_pool.cal9_step = Value('i', 0)
+    g_pool.cal9_circle_id = RawValue('i' ,0)
+    g_pool.pos_record = Value(c_bool, 0)
+    g_pool.eye_rx, g_pool.eye_tx = Pipe(False)
 
-	g_pool.audio_record = Value(c_bool,False)
-	g_pool.audio_rx, g_pool.audio_tx = Pipe(False)
-	g_pool.player_refresh = Event()
-	g_pool.play = RawValue(c_bool,0)
-	g_pool.quit = RawValue(c_bool,0)
-	# end shared globals
+    g_pool.audio_record = Value(c_bool,False)
+    g_pool.audio_rx, g_pool.audio_tx = Pipe(False)
+    g_pool.player_refresh = Event()
+    g_pool.play = RawValue(c_bool,0)
+    g_pool.quit = RawValue(c_bool,0)
+    # end shared globals
 
-	# set up sub processes
-	p_eye = Process(target=eye, args=(eye_src,eye_size, g_pool))
-	if use_player: p_player = Process(target=player, args=(g_pool,player_size))
-	if audio: p_audio = Process(target=record_audio, args=(g_pool.audio_rx,g.g_pool.audio_record,3))
+    # set up sub processes
+    p_eye = Process(target=eye, args=(eye_src,eye_size, g_pool))
+    if use_player: p_player = Process(target=player, args=(g_pool,player_size))
+    if audio: p_audio = Process(target=record_audio, args=(g_pool.audio_rx,g.g_pool.audio_record,3))
 
-	# spawn sub processes
-	p_eye.start()
-	if use_player: p_player.start()
-	if audio: p_audio.start()
+    # spawn sub processes
+    if use_player: p_player.start()
+    p_eye.start()
+    if audio: p_audio.start()
 
-	# when using some cameras (like our current worldcamera logitch c510)
-	# you can't run world camera grabber in its own process
-	# it must reside in the main loop when you run on MacOS.
-	world(world_src,world_size,g_pool)
+    # on linux we need to give the camera driver some time before you request another camera
+    sleep(0.2)
 
-	# exit / clean-up
-	p_eye.join()
-	if use_player: p_player.join()
-	if audio: p_audio.join()
-	print "main exit"
+    # on Mac, when using some cameras (like our current worldcamera logitch c510)
+    # you can't run world camera grabber in its own process
+    # it must reside in the main loop when you run on MacOS.
+    world(world_src,world_size,g_pool)
+
+    # exit / clean-up
+    p_eye.join()
+    if use_player: p_player.join()
+    if audio: p_audio.join()
+    print "main exit"
 
 if __name__ == '__main__':
-	main()
+    main()
