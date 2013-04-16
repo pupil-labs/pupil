fd87730e7ff87a8feef638abee3787c6c869094a
Moritz Kassner <moritzkassner@gmail.com>
2013-04-16 17:50:45 +0200

    fix on coarse pupil detection. clamp the hart-filter size to be between 8 and 80 pixels.

--- Capture/c_methods/methods.c
+++ Capture/c_methods/methods.c
@@ -61,7 +61,7 @@ inline eye_t make_eye(int h){
 void filter(const float *img, const int rows, const int cols, int * x_pos,int *y_pos,int *width)
 {
     point_t img_size = {rows,cols};
-    int min_h = 1;
+    int min_h = 8;
     int max_h = 80;
     int h, i, j;
     float best_response = -10000;
