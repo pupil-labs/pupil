b6157f3f17c25442650a874e160ba5f5914a2150
Will Patera <willpatera@gmail.com>
2013-07-29 10:23:57 +0200

    Cosmetic change - removing glut import from vertex_buffer module

--- pupil_src/shared_modules/gl_utils/vertex_buffer.py
+++ pupil_src/shared_modules/gl_utils/vertex_buffer.py
@@ -12,7 +12,6 @@ import numpy as np
 import OpenGL
 import OpenGL.GL as gl
 import OpenGL.GLU as glu
-import OpenGL.GLUT as glut
 
 
 class VertexAttribute(object):
@@ -200,68 +199,74 @@ class VertexBuffer(object):
 
 
 
-# -----------------------------------------------------------------------------
-def on_display():
-    global cube, theta, phi, frame, time, timebase
 
-    frame += 1
-    time = glut.glutGet( glut.GLUT_ELAPSED_TIME )
-    if (time - timebase > 1000):
-        print frame*1000.0/(time-timebase)
-        timebase = time;		
-        frame = 0;
-
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-
-    gl.glPushMatrix()
-    gl.glRotatef(theta, 0,0,1)
-    gl.glRotatef(phi, 0,1,0)
-    gl.glDisable( gl.GL_BLEND )
-    gl.glEnable( gl.GL_LIGHTING )
-    gl.glEnable( gl.GL_DEPTH_TEST )
-    gl.glEnable( gl.GL_POLYGON_OFFSET_FILL )
-    gl.glPolygonMode( gl.GL_FRONT_AND_BACK, gl.GL_FILL )
-    cube.draw( gl.GL_QUADS, 'pnc' )
-    gl.glDisable( gl.GL_POLYGON_OFFSET_FILL )
-    gl.glEnable( gl.GL_BLEND )
-    gl.glDisable( gl.GL_LIGHTING )
-    gl.glPolygonMode( gl.GL_FRONT_AND_BACK, gl.GL_LINE )
-    gl.glDepthMask( gl.GL_FALSE )
-    gl.glColor( 0.0, 0.0, 0.0, 0.5 )
-    cube.draw( gl.GL_QUADS, 'p' )
-    gl.glDepthMask( gl.GL_TRUE )
-    gl.glPopMatrix()
-
-    glut.glutSwapBuffers()
-    
-def on_reshape(width, height):
-    gl.glViewport(0, 0, width, height)
-    gl.glMatrixMode( gl.GL_PROJECTION )
-    gl.glLoadIdentity( )
-    glu.gluPerspective( 45.0, float(width)/float(height), 2.0, 10.0 )
-    gl.glMatrixMode( gl.GL_MODELVIEW )
-    gl.glLoadIdentity( )
-    gl.glTranslatef( 0.0, 0.0, -5.0 )
-
-def on_keyboard(key, x, y):
-    if key == '\033':
-        sys.exit()
-
-def on_timer(value):
-    global theta, phi
-    theta += 0.25
-    phi += 0.25
-    glut.glutPostRedisplay()
-    glut.glutTimerFunc(10, on_timer, 0)
-
-def on_idle():
-    global theta, phi
-    theta += 0.25
-    phi += 0.25
-    glut.glutPostRedisplay()
 
 
 if __name__ == '__main__':
+
+    import OpenGL.GLUT as glut
+
+    # -----------------------------------------------------------------------------
+    def on_display():
+        global cube, theta, phi, frame, time, timebase
+
+        frame += 1
+        time = glut.glutGet( glut.GLUT_ELAPSED_TIME )
+        if (time - timebase > 1000):
+            print frame*1000.0/(time-timebase)
+            timebase = time;        
+            frame = 0;
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+        gl.glPushMatrix()
+        gl.glRotatef(theta, 0,0,1)
+        gl.glRotatef(phi, 0,1,0)
+        gl.glDisable( gl.GL_BLEND )
+        gl.glEnable( gl.GL_LIGHTING )
+        gl.glEnable( gl.GL_DEPTH_TEST )
+        gl.glEnable( gl.GL_POLYGON_OFFSET_FILL )
+        gl.glPolygonMode( gl.GL_FRONT_AND_BACK, gl.GL_FILL )
+        cube.draw( gl.GL_QUADS, 'pnc' )
+        gl.glDisable( gl.GL_POLYGON_OFFSET_FILL )
+        gl.glEnable( gl.GL_BLEND )
+        gl.glDisable( gl.GL_LIGHTING )
+        gl.glPolygonMode( gl.GL_FRONT_AND_BACK, gl.GL_LINE )
+        gl.glDepthMask( gl.GL_FALSE )
+        gl.glColor( 0.0, 0.0, 0.0, 0.5 )
+        cube.draw( gl.GL_QUADS, 'p' )
+        gl.glDepthMask( gl.GL_TRUE )
+        gl.glPopMatrix()
+
+        glut.glutSwapBuffers()
+        
+    def on_reshape(width, height):
+        gl.glViewport(0, 0, width, height)
+        gl.glMatrixMode( gl.GL_PROJECTION )
+        gl.glLoadIdentity( )
+        glu.gluPerspective( 45.0, float(width)/float(height), 2.0, 10.0 )
+        gl.glMatrixMode( gl.GL_MODELVIEW )
+        gl.glLoadIdentity( )
+        gl.glTranslatef( 0.0, 0.0, -5.0 )
+
+    def on_keyboard(key, x, y):
+        if key == '\033':
+            sys.exit()
+
+    def on_timer(value):
+        global theta, phi
+        theta += 0.25
+        phi += 0.25
+        glut.glutPostRedisplay()
+        glut.glutTimerFunc(10, on_timer, 0)
+
+    def on_idle():
+        global theta, phi
+        theta += 0.25
+        phi += 0.25
+        glut.glutPostRedisplay()
+
+
     p = ( ( 1, 1, 1), (-1, 1, 1), (-1,-1, 1), ( 1,-1, 1),
           ( 1,-1,-1), ( 1, 1,-1), (-1, 1,-1), (-1,-1,-1) )
     n = ( ( 0, 0, 1), (1, 0, 0), ( 0, 1, 0),
