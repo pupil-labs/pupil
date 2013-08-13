0f621bfeecbcef51714d9943a18740d7dcbbe491
Moritz Kassner <moritzkassner@gmail.com>
2013-08-13 12:53:48 +0200

    major rework in uvc_capture

--- pupil_src/shared_modules/uvc_capture/__init__.py
+++ pupil_src/shared_modules/uvc_capture/__init__.py
@@ -19,7 +19,7 @@ it requires:
     - on MacOS: uvcc (binary is distributed with this module)
 """
 
-from cv2 import VideoCapture, cvtColor, COLOR_RGB2BGR,COLOR_RGB2HSV, cv
+from cv2 import VideoCapture
 import numpy as np
 from os.path import isfile
 
@@ -30,93 +30,12 @@ del platform
 
 ###OS specific imports and defs
 if os_name == "Linux":
-    import v4l2_ctl as uvc
-    from v4l2_capture import VideoCapture
+    from linux_video import Camera_Capture,Camera_List
 elif os_name == "Darwin":
-    import uvcc as uvc
+    from mac_video import Camera_Capture,Camera_List
 else:
-    ### On OS without UVC Camera control support we use empty classes.
-    class uvc(object):
-        """dummy uvc class that exposes similar methods but does nothing"""
-        def __init__(self):
-            pass
+    from other_video import Camera_Capture,Camera_List
 
-        class Camera_List(list):
-            """empty class"""
-            def __init__(self):
-                pass
-
-        class Camera(object):
-            """empty class"""
-            def __init__(self):
-                pass
-###end OS imports and defs
-
-
-class CameraCapture(uvc.Camera):
-    """
-    CameraCapture Class for Image grabbing and control
-    inherits from an OS specitic Camera that defines all uvc control methods
-    """
-    def __init__(self, cam,size=(640,480)):
-        super(CameraCapture, self).__init__(cam)
-
-        ###add cv videocapture capabilities
-        self.cap = VideoCapture(self.cvId)
-        # self.set_size(size)
-        self.read = self.cap.read
-
-    def set_size(self,size):
-        width,height = size
-        self.cap.set(3, width)
-        self.cap.set(4, height)
-
-    def get_size(self):
-        return self.cap.get(3),self.cap.get(4)
-
-    def read_RGB(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2BGR,img)
-        return s,img
-
-    def read_HSV(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2HSV,img)
-        return s,img
-
-class noUVCCapture():
-    """
-    VideoCapture without uvc control
-    """
-    def __init__(self, src,size=(640,480)):
-        self.controls = None
-        self.cvId = src
-        ###add cv videocapture capabilities
-        self.cap = VideoCapture(src)
-        self.set_size(size)
-        self.read = self.cap.read
-
-    def set_size(self,size):
-        width,height = size
-        self.cap.set(3, width)
-        self.cap.set(4, height)
-
-    def get_size(self):
-        return self.cap.get(3),self.cap.get(4)
-
-    def read_RGB(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2BGR,img)
-        return s,img
-
-    def read_HSV(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2HSV,img)
-        return s,img
 
 class FileCapture():
     """
@@ -133,6 +52,12 @@ class FileCapture():
     def get_size(self):
         return self.cap.get(3),self.cap.get(4)
 
+    def set_fps(self):
+        pass
+
+    def get_fps(self):
+        return None
+
     def read(self):
         s, img =self._get_frame_()
         if  self.auto_rewind and not s:
@@ -140,55 +65,44 @@ class FileCapture():
             s, img = self._get_frame_()
         return s,img
 
-    def read_RGB(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2BGR,img)
-        return s,img
-
-    def read_HSV(self):
-        s,img = self.read()
-        if s:
-            cvtColor(img,COLOR_RGB2HSV,img)
-        return s,img
 
     def rewind(self):
         self.cap.set(1,0) #seek to the beginning
 
 
 
-def autoCreateCapture(src,size=(640,480)):
+def autoCreateCapture(src,size=(640,480),fps=30):
     # checking src and handling all cases:
     src_type = type(src)
 
     #looking for attached cameras that match the suggested names
     if src_type is list:
         matching_devices = []
-        for device in uvc.Camera_List():
+        for device in Camera_List():
             if any([s in device.name for s in src]):
                 matching_devices.append(device)
 
         if len(matching_devices) >1:
-            print "Warning: found",len(matching_devices),"devices that match the src string pattern. Using the first one"
+            print "Warning: found",len(matching_devices),"devices that match the src string pattern. Using the first one."
         if len(matching_devices) ==0:
             print "ERROR: No device found that matched",src,
             return
 
-        cap = CameraCapture(matching_devices[0],size)
-        print "camera selected: %s  with id: %s" %(cap.name,cap.cvId)
+        cap = Camera_Capture(matching_devices[0],size,fps)
+        print "camera selected: %s  with id: %s" %(cap.name,cap.src_id)
         return cap
 
     #looking for attached cameras that match cv_id
     elif src_type is int:
-        for device in uvc.Camera_List():
-            if device.cvId == src:
-                cap = CameraCapture(device,size)
-                print "camera selected: %s  with id: %s" %(cap.name,cap.cvId)
+        for device in Camera_List():
+            if device.src_id == src:
+                cap = Camera_Capture(device,size,fps)
+                print "camera selected: %s  with id: %s" %(cap.name,cap.src_id)
                 return cap
 
-        #no matching id found or uvc control not supported: trying capture without uvc controls
-        cap = noUVCCapture(src,size)
-        print "WARNING: no UVC support: Using camera with id: %s" %(cap.cvId)
+        #control not supported: trying capture without uvc controls
+        cap = Camera_Capture(src,size,fps)
+        print "WARNING: no UVC support: Using camera with id: %s" %(src)
         return cap
 
 
@@ -202,9 +116,7 @@ def autoCreateCapture(src,size=(640,480)):
 
 
 if __name__ == '__main__':
-    cap = autoCreateCapture(["FaceTime"],(1280,720))
+    cap = autoCreateCapture(1,(1280,720))
     if cap:
         print cap.controls
-        # cap.v4l2_set_default()
-        # cap.uvc_camera.update_from_device()
     print "done"
