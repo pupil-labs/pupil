bdf4107479f0ef5aadf164adfee760bc605039ee
Moritz Kassner <moritzkassner@gmail.com>
2013-10-07 15:42:01 +0200

    removed unused chmod

--- deploy/finalize_bundle_ubuntu.py
+++ deploy/finalize_bundle_ubuntu.py
@@ -8,10 +8,6 @@ pupil_capture_exc = os.path.join(pupil_capture_dir,'pupil_capture')
 v4l2_exc = os.path.join(pupil_capture_dir,'v4l2-ctl')
 
 
-os.chmod(pupil_capture_exc,0775)
-os.chmod(v4l2_exc,0775)
-print "gave pupil_capture and v4l2 excecutables proper rights"
-
 shutil.copy('make_shortcut.sh',os.path.join(distribtution_dir,'make_shortcut.sh'))
 print "copied a small script that creates a shortcut for the user into distribtution_dir"
 os.chmod(os.path.join(distribtution_dir,'make_shortcut.sh'),0775)
