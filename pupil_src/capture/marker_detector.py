c9e81a676d75653b8368d47dbffa224369fca033
Moritz Kassner <moritzkassner@gmail.com>
2013-11-20 19:11:14 +0100

    added robust orientation. marker id tracking and feedback.

--- pupil_src/capture/marker_detector.py
+++ pupil_src/capture/marker_detector.py
@@ -31,6 +31,8 @@ class Marker_Detector(Plugin):
         self.rects = []
 
         self.aperture = c_int(9)
+        self.min_marker_circumferece = 70
+
         self.window_should_open = False
         self.window_should_close = False
         self._window = None
@@ -109,18 +111,12 @@ class Marker_Detector(Plugin):
     def update(self,frame,recent_pupil_positions):
         img = frame.img
         gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        # self.candidate_points = self.detector.detect(s_img)
 
-        # get threshold image used to get crisp-clean edges
         edges = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, self.aperture.value, 9)
-        # cv2.flip(edges,1 ,dst = edges,)
-        # display the image for debugging purpuses
-        # img[:] = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
-         # from edges to contours to ellipses CV_RETR_CCsOMP ls fr hole
+
         contours, hierarchy = cv2.findContours(edges,
                                         mode=cv2.RETR_TREE,
                                         method=cv2.CHAIN_APPROX_SIMPLE,offset=(0,0)) #TC89_KCOS
-
 
         # remove extra encapsulation
         hierarchy = hierarchy[0]
@@ -131,34 +127,25 @@ class Marker_Detector(Plugin):
         # turn on to debug contours
         # cv2.drawContours(img, contours,-1, (0,255,255))
         # cv2.drawContours(img, contained_contours,-1, (0,0,255))
-        # aprox_contours = [cv2.approxPolyDP(c,epsilon=2,closed=True) for c in contained_contours]
-        # squares = [c for c in aprox_contours if c.shape[0]==4]
         # cv2.drawContours(img, aprox_contours,-1, (255,0,0))
-
-        # any rectagle will be made of 4 segemnts in its approximation
-        # squares = [c for c in contained_contours if cv2.approxPolyDP(c,epsilon=2,closed=True).shape[0]==4]
 
         # contained_contours = contours #overwrite parent children check
 
+        #filter out rects
         aprox_contours = [cv2.approxPolyDP(c,epsilon=2.5,closed=True) for c in contained_contours]
-
-        rect_cand = [c for c in aprox_contours if c.shape[0]==4]
-
-        rects = np.array(rect_cand,dtype=np.float32)
-
-
-        # for r in rects:
-        #     cv2.polylines(img,[np.int0(r)],isClosed=True,color=(0,0,200))
-
-
-        rects_shape = rects.shape
-        rects.shape = (-1,2) #flatten for rectsubPix
+        # any rectagle will be made of 4 segemnts in its approximation we dont need to find a marker so small that we cannot read it in the end...
+        #also we want all contours to be counter clockwise oriented, we use convex hull fot this:
+        rect_cand = [cv2.convexHull(c) for c in aprox_contours if c.shape[0]==4 and cv2.arcLength(c,closed=True) > self.min_marker_circumferece]
+        # a covex quadrangle is not what we are looking for.
+        rect_cand = [r for r in rect_cand if r.shape[0]==4]
 
         # subpixel corner fitting
+        rects = np.array(rect_cand,dtype=np.float32)
+        rects_shape = rects.shape
+        rects.shape = (-1,2) #flatten for rectsubPix
         # define the criteria to stop and refine the rects
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
         cv2.cornerSubPix(gray_img,rects,(3,3),(-1,-1),criteria)
-
         rects.shape = rects_shape #back to old layout [[rect],[rect],[rect]...] with rect = [corner,corner,corncer,corner]
 
         def decode(square_img,grid):
@@ -172,6 +159,7 @@ class Marker_Detector(Plugin):
             # strip border to get the message
             msg = msg[1:-1,1:-1]/255
 
+
             # B|*|*|W   ^
             # *|*|*|*  / \
             # *|*|*|*   |  UP
@@ -182,29 +170,35 @@ class Marker_Detector(Plugin):
             if corners == (0,1,1,1):
                 angle = 0
             elif corners == (1,0,1,1):
-                angle = 270
+                angle = 90
             elif corners == (1,1,0,1):
                 angle = 180
             elif corners == (1,1,1,0):
-                angle = 90
+                angle = 270
             else:
                 # logger.debug("This marker does not have valid orientation: \n %s " %msg)
                 return None
+            msg = np.rot90(msg,-angle/90)
+            # B|LSB| 1 |W      ^
+            # 2| 3 | 4 |5     / \
+            # 6| 7 | 8 |9      |  UP
+            # W| 10|MSB|W      |
+            # print angle
+            # print msg.transpose() # we align the output of print with the pixels
+            msg = msg.tolist()
 
-            msg = np.rot90(msg,angle/90)
+            #strip orientation corners from marker
+            del msg[0][0]
+            del msg[0][-1]
+            del msg[-1][0]
+            del msg[-1][-1]
+            #flatten list
 
-            # #this assumes a 6*6 grid marker key is the center 4 can be used in a addion to the orientation key...
-            # # B|*|*|W   ^
-            # # *|W|B|*  / \
-            # # *|B|W|*   |  UP
-            # # W|*|*|W   |
-            # key = msg[1:3,1:3].flatten().tolist()
-            # if key == [1,0,0,1]:
-            #     return angle, msg
-            # else:
-            #     return None
-
-            return angle,msg
+            msg = [item for sublist in msg for item in sublist]
+            i = msg.pop()
+            while msg:
+                i = (i<<1) + msg.pop()
+            return angle,i
 
         offset = 0
         self.rects = []
@@ -215,7 +209,7 @@ class Marker_Detector(Plugin):
             # marker_img = img[slice(*x_slice),slice(*y_slice)]
             # marker_img *= 0.1
             size = 120 # should be a multiple of marker grid
-            M = cv2.getPerspectiveTransform(r,np.array(((0.,0.),(0.,size),(size,size),(size,0.)),dtype=np.float32) )
+            M = cv2.getPerspectiveTransform(r,np.array(((0,0),(size,0),(size,size),(0,size)),dtype=np.float32) ) #bottom left,top left, top right, bottom right in image
             flat_marker_img =  cv2.warpPerspective(gray_img, M, (size,size) )#[, dst[, flags[, borderMode[, borderValue]]]])
 
             # Otsu documentation here :
@@ -224,32 +218,39 @@ class Marker_Detector(Plugin):
 
 
             # cosmetics -- getting a cleaner display of the rectangle marker
-            kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
+            kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
             cv2.erode(otsu,kernel,otsu, iterations=2)
             # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
             # cv2.dilate(otsu,kernel,otsu, iterations=1)
 
 
 
-            marker = decode(otsu, 6)
+            marker = decode(otsu, 6) # the full 6*6 marker
             if marker is not None:
                 angle,msg = marker
                 # roll points such that the marker points correspond with oriented marker
                 rot_r = np.roll(r,angle/90,axis=0)
                 # this way we get the matrix transform with rotation included
-                norm_to_marker = cv2.getPerspectiveTransform(np.array(((0.,0.),(0.,1),(1,1),(1,0.)),dtype=np.float32),rot_r)
+                norm_to_marker = cv2.getPerspectiveTransform(np.array(((1,0),(1,1),(0,1),(0,0)),dtype=np.float32),rot_r)
                 marker_to_screen = cv2.getPerspectiveTransform(rot_r,np.array(((0.,0.),(0.,1),(1,1),(1,0.)),dtype=np.float32))
-
+                #marker coord system:
+                # +-----------+
+                # |0,1     1,1|  ^
+                # |           | / \
+                # |           |  |  UP
+                # |0,0     1,0|  |
+                # +-----------+
                 self.rects.append(r)
-                img[0:flat_marker_img.shape[0],offset:flat_marker_img.shape[1]+offset,1] = np.rot90(otsu,angle/90)
+                img[0:flat_marker_img.shape[0],offset:flat_marker_img.shape[1]+offset,1] = np.rot90(otsu,-angle/90)
                 # img[0:flat_marker_img.shape[0],offset:flat_marker_img.shape[1]+offset,2] = np.rot90(flat_marker_img,angle/90)
-
                 centroid = [r.sum(axis=0)/4.]
+                bottom_left = rot_r[0]
                 center = np.array([[[.5,.5]]],dtype=np.float32)
+                center = np.array([[[0,0],[1,0],[1.5,.5],[1,1],[0,1]]],dtype=np.float32)
                 center = cv2.perspectiveTransform(center,norm_to_marker)
                 cv2.polylines(img,np.int0(center),color = (0,0,255),isClosed=True)
                 cv2.polylines(img,np.int0(centroid),color = (255,255,0),isClosed=True)
-
+                cv2.putText(img,'id: '+str(msg),tuple(np.int0(bottom_left)[0,:]),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255,100,50))
                 offset += size+10
                 if offset+size > img.shape[1]:
                     break
