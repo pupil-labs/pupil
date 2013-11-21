114d8bb0bde1e382d23bbc2763f0976305827799
Moritz Kassner <moritzkassner@gmail.com>
2013-11-21 11:13:23 +0100

    moved to 5,5 marker

--- pupil_src/capture/marker_detector.py
+++ pupil_src/capture/marker_detector.py
@@ -31,7 +31,7 @@ class Marker_Detector(Plugin):
         self.rects = []
 
         self.aperture = c_int(9)
-        self.min_marker_circumferece = 70
+        self.min_marker_perimeter = 40
 
         self.window_should_open = False
         self.window_should_close = False
@@ -135,7 +135,7 @@ class Marker_Detector(Plugin):
         aprox_contours = [cv2.approxPolyDP(c,epsilon=2.5,closed=True) for c in contained_contours]
         # any rectagle will be made of 4 segemnts in its approximation we dont need to find a marker so small that we cannot read it in the end...
         #also we want all contours to be counter clockwise oriented, we use convex hull fot this:
-        rect_cand = [cv2.convexHull(c) for c in aprox_contours if c.shape[0]==4 and cv2.arcLength(c,closed=True) > self.min_marker_circumferece]
+        rect_cand = [cv2.convexHull(c) for c in aprox_contours if c.shape[0]==4 and cv2.arcLength(c,closed=True) > self.min_marker_perimeter]
         # a covex quadrangle is not what we are looking for.
         rect_cand = [r for r in rect_cand if r.shape[0]==4]
 
@@ -159,32 +159,41 @@ class Marker_Detector(Plugin):
             # strip border to get the message
             msg = msg[1:-1,1:-1]/255
 
-
-            # B|*|*|W   ^
-            # *|*|*|*  / \
-            # *|*|*|*   |  UP
-            # W|*|*|W   |
+            # out first bit is encoded in the orientation corners of the marker:
+            #               MSB = 0                   MSB = 1
+            #               W|*|*|W   ^               B|*|*|B   ^
+            #               *|*|*|*  / \              *|*|*|*  / \
+            #               *|*|*|*   |  UP           *|*|*|*   |  UP
+            #               B|*|*|W   |               W|*|*|B   |
             # 0,0 -1,0 -1,-1, 0,-1
             # angles are counter-clockwise rotation
             corners = msg[0,0], msg[-1,0], msg[-1,-1], msg[0,-1]
-            if corners == (0,1,1,1):
-                angle = 0
-            elif corners == (1,0,1,1):
-                angle = 90
-            elif corners == (1,1,0,1):
-                angle = 180
-            elif corners == (1,1,1,0):
-                angle = 270
+
+            if sum(corners) == 3:
+                msg_int = 0
+            elif sum(corners) ==1:
+                msg_int = 1
+                corners = tuple([1-c for c in corners]) #simple inversion
             else:
-                # logger.debug("This marker does not have valid orientation: \n %s " %msg)
                 return None
+
+            #read rotation of marker by now we are guaranteed to have 3w and 1b
+            if corners == (0,1,1,1):
+                angle = 270
+            elif corners == (1,0,1,1):
+                angle = 0
+            elif corners == (1,1,0,1):
+                angle = 90
+            else:
+                angle = 180
+
             msg = np.rot90(msg,-angle/90)
-            # B|LSB| 1 |W      ^
-            # 2| 3 | 4 |5     / \
-            # 6| 7 | 8 |9      |  UP
-            # W| 10|MSB|W      |
+            # W  |LSB| 1 |W      ^
+            # 2  | 3 | 4 |5     / \
+            # 6  | 7 | 8 |9      |  UP
+            # MSB| 10| 11|W      |
             # print angle
-            # print msg.transpose() # we align the output of print with the pixels
+            # print msg.transpose() # we align the output of print to align with pixels that you see
             msg = msg.tolist()
 
             #strip orientation corners from marker
@@ -193,22 +202,21 @@ class Marker_Detector(Plugin):
             del msg[-1][0]
             del msg[-1][-1]
             #flatten list
-
             msg = [item for sublist in msg for item in sublist]
-            i = msg.pop()
             while msg:
-                i = (i<<1) + msg.pop()
-            return angle,i
+                # [0,1,0,1] -> int [MSB,bit,bit,...,LSB], note the MSB is definde above
+                msg_int = (msg_int<<1) + msg.pop()
+            return angle,msg_int
 
         offset = 0
-        self.rects = []
+        markers = []
         for r in rects:
             # cv2.polylines(img,[np.int0(r)],isClosed=True,color=(100,200,0))
             # y_slice = int(min(r[:,:,0])-1),int(max(r[:,:,0])+1)
             # x_slice = int(min(r[:,:,1])-1),int(max(r[:,:,1])+1)
             # marker_img = img[slice(*x_slice),slice(*y_slice)]
             # marker_img *= 0.1
-            size = 120 # should be a multiple of marker grid
+            size = 100 # should be a multiple of marker grid
             M = cv2.getPerspectiveTransform(r,np.array(((0,0),(size,0),(size,size),(0,size)),dtype=np.float32) ) #bottom left,top left, top right, bottom right in image
             flat_marker_img =  cv2.warpPerspective(gray_img, M, (size,size) )#[, dst[, flags[, borderMode[, borderValue]]]])
 
@@ -219,20 +227,20 @@ class Marker_Detector(Plugin):
 
             # cosmetics -- getting a cleaner display of the rectangle marker
             kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
-            cv2.erode(otsu,kernel,otsu, iterations=2)
+            cv2.erode(otsu,kernel,otsu, iterations=3)
             # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
             # cv2.dilate(otsu,kernel,otsu, iterations=1)
 
 
 
-            marker = decode(otsu, 6) # the full 6*6 marker
+            marker = decode(otsu, 5) # the full 6*6 marker
             if marker is not None:
                 angle,msg = marker
                 # roll points such that the marker points correspond with oriented marker
                 rot_r = np.roll(r,angle/90,axis=0)
                 # this way we get the matrix transform with rotation included
-                norm_to_marker = cv2.getPerspectiveTransform(np.array(((1,0),(1,1),(0,1),(0,0)),dtype=np.float32),rot_r)
-                marker_to_screen = cv2.getPerspectiveTransform(rot_r,np.array(((0.,0.),(0.,1),(1,1),(1,0.)),dtype=np.float32))
+                marker_to_screen = cv2.getPerspectiveTransform(np.array(((1,0),(1,1),(0,1),(0,0)),dtype=np.float32),rot_r)
+                screen_to_marker = cv2.getPerspectiveTransform(rot_r,np.array(((0.,0.),(0.,1),(1,1),(1,0.)),dtype=np.float32))
                 #marker coord system:
                 # +-----------+
                 # |0,1     1,1|  ^
@@ -245,13 +253,14 @@ class Marker_Detector(Plugin):
                 # img[0:flat_marker_img.shape[0],offset:flat_marker_img.shape[1]+offset,2] = np.rot90(flat_marker_img,angle/90)
                 centroid = [r.sum(axis=0)/4.]
                 bottom_left = rot_r[0]
-                center = np.array([[[.5,.5]]],dtype=np.float32)
                 center = np.array([[[0,0],[1,0],[1.5,.5],[1,1],[0,1]]],dtype=np.float32)
-                center = cv2.perspectiveTransform(center,norm_to_marker)
+                center = cv2.perspectiveTransform(center,marker_to_screen)
                 cv2.polylines(img,np.int0(center),color = (0,0,255),isClosed=True)
                 cv2.polylines(img,np.int0(centroid),color = (255,255,0),isClosed=True)
                 cv2.putText(img,'id: '+str(msg),tuple(np.int0(bottom_left)[0,:]),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255,100,50))
                 offset += size+10
+                # marker to be returned/broadcast out -- accessible to world
+                marker = {'id':msg,'pts':rot_r,'marker_to_screen':marker_to_screen,'screen_to_marker':screen_to_marker}
                 if offset+size > img.shape[1]:
                     break
 
@@ -294,45 +303,4 @@ class Marker_Detector(Plugin):
         if self._window:
             self.close_window()
         self._bar.destroy()
-
-
-# shared helper functions for detectors private to the module
-def _calibrate_camera(img_pts, obj_pts, img_size):
-    # generate pattern size
-    camera_matrix = np.zeros((3,3))
-    dist_coef = np.zeros(4)
-    rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_pts, img_pts,
-                                                    img_size, camera_matrix, dist_coef)
-    return camera_matrix, dist_coefs
-
-def _gen_pattern_grid(size=(4,11)):
-    pattern_grid = []
-    for i in xrange(size[1]):
-        for j in xrange(size[0]):
-            pattern_grid.append([(2*j)+i%2,i,0])
-    return np.asarray(pattern_grid, dtype='f4')
-
-
-def _make_grid(dim=(11,4)):
-    """
-    this function generates the structure for an assymetrical circle grid
-    centerd around 0 width=1, height scaled accordingly
-    """
-    x,y = range(dim[0]),range(dim[1])
-    p = np.array([[[s,i] for s in x] for i in y], dtype=np.float32)
-    p[:,1::2,1] += 0.5
-    p = np.reshape(p, (-1,2), 'F')
-
-    # scale height = 1
-    x_scale =  1./(np.amax(p[:,0])-np.amin(p[:,0]))
-    y_scale =  1./(np.amax(p[:,1])-np.amin(p[:,1]))
-
-    p *=x_scale,x_scale/.5
-
-    # center x,y around (0,0)
-    x_offset = (np.amax(p[:,0])-np.amin(p[:,0]))/2.
-    y_offset = (np.amax(p[:,1])-np.amin(p[:,1]))/2.
-    p -= x_offset,y_offset
-    return p
-
 
