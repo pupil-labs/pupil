630ab5c851eeaf03fc4168d66f9c00cc048f8f95
Moritz Kassner <moritzkassner@gmail.com>
2013-12-16 09:46:40 +0100

    added uvc_driver patch to ubuntu bundle. small renames.

--- deploy/finalize_bundle_ubuntu.py
+++ deploy/finalize_bundle_ubuntu.py
@@ -4,14 +4,17 @@ import shutil
 
 distribtution_dir = 'dist'
 pupil_capture_dir =  os.path.join(distribtution_dir, 'pupil_capture')
-pupil_capture_exc = os.path.join(pupil_capture_dir,'pupil_capture')
-v4l2_exc = os.path.join(pupil_capture_dir,'v4l2-ctl')
 
+
+shutil.copy('patch_uvc_driver.sh',os.path.join(distribtution_dir,'patch_uvc_driver.sh'))
+print "Copied a small script to patch uvc driver into the distribution dir"
+os.chmod(os.path.join(distribtution_dir,'patch_uvc_driver.sh'),0775)
+print "Gave that file excetion rights"
 
 shutil.copy('make_shortcut.sh',os.path.join(distribtution_dir,'make_shortcut.sh'))
-print "copied a small script that creates a shortcut for the user into distribtution_dir"
+print "Copied a small script that creates a shortcut for the user into distribtution_dir"
 os.chmod(os.path.join(distribtution_dir,'make_shortcut.sh'),0775)
-print "gave that file excetion rights"
+print "Gave that file excetion rights"
 
 print "starting version stript:"
 write_version_file.main(pupil_capture_dir)
