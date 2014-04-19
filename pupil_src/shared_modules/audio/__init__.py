1ed364f44d0c1d3e64d9cc7bf6da3a0f3bee48b8
Moritz Kassner <moritzkassner@gmail.com>
2014-04-19 10:35:35 +0200

    adding linux audio capture.

--- pupil_src/shared_modules/audio/__init__.py
+++ pupil_src/shared_modules/audio/__init__.py
@@ -14,41 +14,59 @@ del platform
 from time import sleep
 
 import subprocess as sp
+import signal
 
 # OS specific audio players via terminal
 if os_name == "Linux":
-	def beep():
-		sp.Popen(["paplay", "/usr/share/sounds/ubuntu/stereo/message.ogg"])
+    def beep():
+        sp.Popen(["paplay", "/usr/share/sounds/ubuntu/stereo/message.ogg"])
 
-	def tink():
-		sp.Popen(["paplay", "/usr/share/sounds/ubuntu/stereo/message.ogg"])
+    def tink():
+        sp.Popen(["paplay", "/usr/share/sounds/ubuntu/stereo/message.ogg"])
 
-	def say(message):
-		sp.Popen(["spd-say", message])
+    def say(message):
+        sp.Popen(["spd-say", message])
+
+
+    class audio_capture(object):
+        """docstring for audio_capture"""
+        def __init__(self, out_file):
+            super(audio_capture, self).__init__()
+            self.out_path = out_path
+            ffmpeg_bin = 'ffmpeg'
+            command = [ ffmpeg_bin,
+                    '-i', 'hw:0,0',
+                    '-f', 'alsa',
+                    out_file]
+            self.process =  sp.Popen(command)
+
+        def __del__(self):
+            self.process.send_signal(signal.SIGINT)
+
 
 elif os_name == "Darwin":
-	def beep():
-		sp.Popen(["afplay", "/System/Library/Sounds/Pop.aiff"])
+    def beep():
+        sp.Popen(["afplay", "/System/Library/Sounds/Pop.aiff"])
 
-	def tink():
-		sp.Popen(["afplay", "/System/Library/Sounds/Tink.aiff"])
+    def tink():
+        sp.Popen(["afplay", "/System/Library/Sounds/Tink.aiff"])
 
-	def say(message):
-		sp.Popen(["say", message, "-v" "Victoria"])
+    def say(message):
+        sp.Popen(["say", message, "-v" "Victoria"])
 else:
-	def beep():
-		print '\a'
+    def beep():
+        print '\a'
 
-	def tink():
-		print '\a'
+    def tink():
+        print '\a'
 
-	def say(message):
-		print '\a'
-		print message
+    def say(message):
+        print '\a'
+        print message
 
 if __name__ == '__main__':
-	beep()
-	sleep(1)
-	tink()
-	sleep(1)
-	say("Hello, I am Pupil's audio module.")
+    beep()
+    sleep(1)
+    tink()
+    sleep(1)
+    say("Hello, I am Pupil's audio module.")
