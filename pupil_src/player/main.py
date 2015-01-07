dfcd68be691b494b534088cbb4693a34a10de6e1
Will Patera <willpatera@gmail.com>
2015-01-07 11:07:38 +0700

    updating license header (happy 2015!)

--- pupil_src/player/main.py
+++ pupil_src/player/main.py
@@ -1,14 +1,12 @@
 '''
-
 (*)~----------------------------------------------------------------------------------
  Pupil - eye tracking platform
- Copyright (C) 2012-2014  Pupil Labs
+ Copyright (C) 2012-2015  Pupil Labs
 
  Distributed under the terms of the CC BY-NC-SA License.
  License details are in the file license.txt, distributed as part of this software.
 ----------------------------------------------------------------------------------~(*)
 '''
-
 
 import sys, os,platform
 from time import sleep
