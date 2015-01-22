4096c8bed4fe5464edf5c6594609287f38d08b28
Moritz Kassner <moritzkassner@gmail.com>
2015-01-22 11:09:17 +0100

    change order to avoid loading Thinkpad cameras

--- pupil_src/capture/main.py
+++ pupil_src/capture/main.py
@@ -91,7 +91,7 @@ class Global_Container(object):
 
 def main():
     # To assign camera by name: put string(s) in list
-    eye_src = ["Microsoft", "6000","Integrated Camera","USB 2.0 Camera"]
+    eye_src = ["USB 2.0 Camera","Microsoft", "6000","Integrated Camera"]
     world_src = ["Logitech Camera","(046d:081d)","C510","B525", "C525","C615","C920","C930e"]
 
     # to assign cameras directly, using integers as demonstrated below
