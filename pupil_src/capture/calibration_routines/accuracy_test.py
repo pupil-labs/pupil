dbbfe933778d1663ddadba1af56cf5440086ab3d
Moritz Kassner <moritzkassner@gmail.com>
2015-02-19 20:16:45 +0100

    fixed quick bar order

--- pupil_src/capture/calibration_routines/accuracy_test.py
+++ pupil_src/capture/calibration_routines/accuracy_test.py
@@ -164,7 +164,7 @@ class Accuracy_Test(Calibration_Plugin):
 
         self.button = ui.Thumb('active',self,setter=self.toggle,label='Calibrate',hotkey='c')
         self.button.on_color[:] = (.3,.2,1.,.9)
-        self.g_pool.quickbar.append(self.button)
+        self.g_pool.quickbar.insert(0,self.button)
 
 
     def deinit_gui(self):
