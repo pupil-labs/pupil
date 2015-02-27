52f2300a0f4535516e129f0e6cc28f44674cdca0
Michael Barz <mb@barz.de>
2015-02-27 19:47:50 +0100

    Fixations get visualized

--- pupil_src/shared_modules/fixation_detector.py
+++ pupil_src/shared_modules/fixation_detector.py
@@ -9,10 +9,17 @@
 '''
 
 import numpy as np
-
+import cv2
+import logging
 from math import atan, tan
+from methods import denormalize
 from plugin import Plugin
 from pyglui import ui
+from gl_utils.utils import make_coord_system_pixel_based, draw_gl_polyline,\
+    draw_gl_polyline_norm
+
+# logging
+logger = logging.getLogger(__name__)
 
 class Fixation_Detector(Plugin):
     """ base class for different fixation detection algorithms """
@@ -20,6 +27,7 @@ class Fixation_Detector(Plugin):
         super(Fixation_Detector, self).__init__(g_pool)
         
 class Dispersion_Fixation_Detector(Fixation_Detector):
+    
     """ fixation detection algorithm based on a dispersion threshold """
     def __init__(self, g_pool, dispersion=0.45, h_fov=78, v_fov=50, menu_conf={}):
         super(Fixation_Detector, self).__init__(g_pool)
@@ -29,6 +37,7 @@ class Dispersion_Fixation_Detector(Fixation_Detector):
         self.v_fov = v_fov
         self.scene_res = self.g_pool.capture.frame_size
         self.angle_factor = (0.5/tan(np.deg2rad(self.h_fov/2)), 0.5/tan(np.deg2rad(self.v_fov/2)))
+        self.fixation = None
         self.gaze_history = []
         
     def init_gui(self):
@@ -57,9 +66,10 @@ class Dispersion_Fixation_Detector(Fixation_Detector):
                 angular_dist = self.compute_angular_distance(prev_gaze, gaze)
                 
                 if angular_dist > self.dispersion:
-                    self.fixation = np.mean(np.array([self.gaze_history], dtype = np.float32), axis = 0)
+                    self.fixation = np.mean(np.array(self.gaze_history, dtype = np.float32), axis = 0)
                     # TODO: add fixation to events
                     self.gaze_history = []
+            self.gaze_history.append(gaze)
                 
     def compute_angular_distance(self, prev_gaze, gaze):
         # move origin to image center
@@ -74,7 +84,10 @@ class Dispersion_Fixation_Detector(Fixation_Detector):
         return np.linalg.norm([angle_prev_x - angle_x, angle_prev_y - angle_y])
     
     def gl_display(self):
-        pass
+        if self.fixation is not None:
+            abs_fixation = denormalize(self.fixation,self.g_pool.capture.frame_size, flip_y=True)
+            ellipse = cv2.ellipse2Poly((int(abs_fixation[0]), int(abs_fixation[1])),(25, 25),0,0,360,15)
+            draw_gl_polyline(ellipse,(0.,0.,.5,.75),'Polygon')
     
     def get_init_dict(self):
         return {'dispersion': self.dispersion, 'h_fov':self.h_fov, 'v_fov': self.v_fov, 'menu_conf': self.menu.configuration}
