6e3ea47d0b12b1da0340bdf2ba92d9d64f7d0587
Moritz Kassner <moritzkassner@gmail.com>
2015-03-20 09:23:42 +0100

    typo errors, bug fixes.

--- pupil_src/player/eye_video_overlay.py
+++ pupil_src/player/eye_video_overlay.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 
 def get_past_timestamp(idx,timestamps):
     """
-    recursive function to find the most recent valid timestamp in the past 
+    recursive function to find the most recent valid timestamp in the past
     """
     if idx == 0:
         # if at the beginning, we can't go back in time.
@@ -44,7 +44,7 @@ def get_past_timestamp(idx,timestamps):
 def get_future_timestamp(idx,timestamps):
     """
     recursive function to find most recent valid timestamp in the future
-    """    
+    """
     if idx == len(timestamps)-1:
         # if at the end, we can't go further into the future.
         return get_past_timestamp(idx,timestamps)
@@ -56,11 +56,11 @@ def get_future_timestamp(idx,timestamps):
 
 def get_nearest_timestamp(past_timestamp,future_timestamp,world_timestamp):
     dt_past = abs(past_timestamp-world_timestamp)
-    dt_future = abs(future_timestamp-world_timestamp) # abs prob not necessary here, but just for sanity 
+    dt_future = abs(future_timestamp-world_timestamp) # abs prob not necessary here, but just for sanity
     if dt_past < dt_future:
         return past_timestamp
-    else: 
-        return future_timestamp 
+    else:
+        return future_timestamp
 
 
 def correlate_eye_world(eye_timestamps,world_timestamps):
@@ -104,8 +104,8 @@ def correlate_eye_world(eye_timestamps,world_timestamps):
         if not candidate:
             # get most recent timestamp, either in the past or future
             e_past_ts = get_past_timestamp(idx,eye_timestamps_by_world_index)
-            e_future_ts = get_future_timestamp(idx,eye_timestamps_by_world_index)        
-            eye_frame_index.append(eye_frames_by_timestamp[get_nearest_timestamp(e_past_ts,e_future_ts,world_ts)]) 
+            e_future_ts = get_future_timestamp(idx,eye_timestamps_by_world_index)
+            eye_frame_index.append(eye_frames_by_timestamp[get_nearest_timestamp(e_past_ts,e_future_ts,world_ts)])
         else:
             # TODO - if there is a list of len > 1 - then we should check which is the temporally closest timestamp
             eye_frame_index.append(eye_frames_by_timestamp[eye_timestamps_by_world_index[idx][-1]])
@@ -126,23 +126,23 @@ class Eye_Video_Overlay(Plugin):
         # and load menu configuration of last session
         self.menu = None
         self.menu_conf = menu_conf
-        
+
         # user controls
         self.alpha = alpha
         self.mirror = mirror
 
-        self.last_world_idx = None 
+        self.last_world_idx = None
         self._frame = None
 
         #parse info.csv file
         with open(g_pool.rec_dir + "/info.csv") as info:
             meta_info = dict( ((line.strip().split('\t')) for line in info.readlines() ) )
 
-        # load eye videos and eye timestamps 
+        # load eye videos and eye timestamps
         if g_pool.rec_version < VersionFormat('0.4'):
             required_files = ['eye.avi','eye_timestamps.npy']
             eye0_video_path = os.path.join(g_pool.rec_dir,required_files[0])
-            eye0_timestamps_path = os.path.join(g_pool.rec_dir,required_files[1]) 
+            eye0_timestamps_path = os.path.join(g_pool.rec_dir,required_files[1])
         else:
             required_files = ['eye0.mkv','eye0_timestamps.npy']
             eye0_video_path = os.path.join(g_pool.rec_dir,required_files[0])
@@ -150,19 +150,20 @@ class Eye_Video_Overlay(Plugin):
             if meta_info['Eye Mode'] == 'binocular':
                 required_files += ['eye1.mkv','eye1_timestamps.npy']
                 eye1_video_path = os.path.join(g_pool.rec_dir,required_files[2])
-                eye1_timestamps_path = os.path.join(g_pool.rec_dir,required_files[3])        
+                eye1_timestamps_path = os.path.join(g_pool.rec_dir,required_files[3])
 
         # check to see if eye videos exist
         for f in required_files:
             if not os.path.isfile(os.path.join(g_pool.rec_dir,f)):
-                logger.debug("Did not find required file: ") %(f, g_pool.rec_dir)
-                self.cleanup() # early exit -- no required files
+                logger.error("Did not find required file: %s in %s"%(f, g_pool.rec_dir))
+                self.alive=False
+                return
 
         logger.debug("%s contains required eye video(s): %s."%(g_pool.rec_dir,required_files))
 
         # Initialize capture -- for now we just try with monocular
         self.cap = autoCreateCapture(eye0_video_path,timestamps=eye0_timestamps_path)
-       
+
         if isinstance(self.cap,FakeCapture):
             logger.error("could not start capture.")
             self.cleanup() # early exit -- no real eye videos
@@ -202,7 +203,7 @@ class Eye_Video_Overlay(Plugin):
 
     def update(self,frame,events):
         requested_eye_frame_idx = self.eye0_frame_index[frame.index]
-        
+
         if self.last_world_idx != frame.index and requested_eye_frame_idx != frame.index:
             # seeklogic
             if requested_eye_frame_idx == self.cap.get_frame_index()+1:
@@ -211,7 +212,7 @@ class Eye_Video_Overlay(Plugin):
             if requested_eye_frame_idx != self.cap.get_frame_index():
                # only now do I need to seek
                self.cap.seek_to_frame(requested_eye_frame_idx)
-            
+
             # reading the frame
             try:
                self._frame = self.cap.get_frame()
@@ -230,7 +231,7 @@ class Eye_Video_Overlay(Plugin):
 
     def gl_display(self):
         # removed texture method because we need to be able to see what we will export - draw directly in the array
-        # update the eye texture 
+        # update the eye texture
         # render camera image
         # if self._frame and self.show_eye:
         #     print self._frame.img.shape
@@ -258,4 +259,4 @@ class Eye_Video_Overlay(Plugin):
         """
         self.deinit_gui()
 
-        
+
