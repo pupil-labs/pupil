c9e2ca3b8799a09f1df9e7531bfe780d3fafb1bf
Michael Barz <mb@barz.de>
2015-04-22 17:45:00 +0200

    added high-speed eye cam

--- pupil_src/capture/main.py
+++ pupil_src/capture/main.py
@@ -90,7 +90,7 @@ class Global_Container(object):
 
 def main():
     # To assign camera by name: put string(s) in list
-    eye_cam_names = ["USB 2.0 Camera","Microsoft", "6000","Integrated Camera"]
+    eye_cam_names = ["USB 2.0 Camera","Microsoft", "6000","Integrated Camera","HD USB Camera"]
     world_src = ["Logitech Camera","(046d:081d)","C510","B525", "C525","C615","C920","C930e"]
     eye_src = (eye_cam_names,0),(eye_cam_names,1) #first match for eye0 and second match for eye1
 
