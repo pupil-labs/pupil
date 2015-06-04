3e1f04bf285e2c53c7640c842ee55a56ac98e977
Moritz Kassner <moritzkassner@gmail.com>
2015-06-04 11:41:00 +0200

    added python compiler to player

--- pupil_src/player/main.py
+++ pupil_src/player/main.py
@@ -29,6 +29,10 @@ else:
     # Specifiy user dirs.
     user_dir = os.path.join(pupil_base_dir,'player_settings')
     version_file = None
+    if __name__ == '__main__':
+        #compile all cython source files
+        from pyx_compiler import build_extensions
+        build_extensions()
 
 
 # create folder for user settings, tmp data
