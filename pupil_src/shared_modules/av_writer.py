ff7c1ed73b54f255a425354a9046a61cfc4d5c12
Moritz Kassner <moritzkassner@gmail.com>
2015-09-23 12:42:17 +0200

    added pyav based audio capture

--- pupil_src/shared_modules/av_writer.py
+++ pupil_src/shared_modules/av_writer.py
@@ -13,17 +13,25 @@ av_writer module uses PyAV (ffmpeg or libav backend) to write AV files.
 requires:
     -
 """
-import os,sys
+import os,sys,platform
 import av
 from av.packet import Packet
 import numpy as np
 from time import time
 from fractions import Fraction
-import subprocess as sp
 
 #logging
 import logging
 logger = logging.getLogger(__name__)
+
+
+from threading import Thread as Process
+from threading import Event
+
+# if platform.system() == 'Darwin':
+#     from billiard import Process,Event
+# else:
+#     from multiprocessing import Process,Event
 
 
 """
@@ -228,10 +236,84 @@ class JPEG_Writer(object):
 
 
 
-class JPEG_Dumper(object):
-    """simple for JPEG_Dumper"""
-    def __init__(self, file_loc):
-        super(JPEG_Dumper, self).__init__()
+def format_time(time, time_base):
+        if time is None:
+            return 'None'
+        return '%.3fs (%s or %s/%s)' % (time_base * time, time_base * time, time_base.numerator * time, time_base.denominator)
+
+
+def rec_thread(file_loc, audio_src,should_close):
+
+    #create out container
+    out_container = av.open(file_loc,'w')
+    logger.debug("Opended '%s' for writing."%file_loc)
+    out_stream =  out_container.add_stream('pcm_f32le')
+
+    #create in container
+    in_container = av.open(':%s'%audio_src,format="avfoundation")
+    in_stream = None
+    # print 'container:', in_container
+    # print '\tformat:', in_container.format
+    # print '\tduration:', float(in_container.duration) / av.time_base
+    # print '\tmetadata:'
+    # for k, v in sorted(in_container.metadata.iteritems()):
+    #     print '\t\t%s: %r' % (k, v)
+    # print
+
+    # print len(in_container.streams), 'stream(s):'
+    for i, stream in enumerate(in_container.streams):
+
+        print '\t%r' % stream
+        print '\t\ttime_base: %r' % stream.time_base
+        print '\t\trate: %r' % stream.rate
+        print '\t\tstart_time: %r' % stream.start_time
+        print '\t\tduration: %s' % format_time(stream.duration, stream.time_base)
+        print '\t\tbit_rate: %r' % stream.bit_rate
+        print '\t\tbit_rate_tolerance: %r' % stream.bit_rate_tolerance
+
+        if stream.type == 'audio':
+            print '\t\taudio:'
+            print '\t\t\tformat:', stream.format
+            print '\t\t\tchannels: %s' % stream.channels
+            in_stream = stream
+            break
+
+        # elif stream.type == 'container':
+        #     print '\t\tcontainer:'
+        #     print '\t\t\tformat:', stream.format
+        #     print '\t\t\taverage_rate: %r' % stream.average_rate
+
+        # print '\t\tmetadata:'
+        # for k, v in sorted(stream.metadata.iteritems()):
+        #     print '\t\t\t%s: %r' % (k, v)
+
+    if in_stream is None:
+        logger.error("No input audio stream found.")
+        out_container.close()
+        return
+
+    for packet in in_container.demux(in_stream):
+        # for frame in packet.decode():
+        #     packet = out_stream.encode(frame)
+        #     if packet:
+        # print '%r' %packet
+        # print '\tduration: %s' % format_time(packet.duration, packet.stream.time_base)
+        # print '\tpts: %s' % format_time(packet.pts, packet.stream.time_base)
+        # print '\tdts: %s' % format_time(packet.dts, packet.stream.time_base)
+        out_container.mux(packet)
+        if should_close.is_set():
+            break
+
+    out_container.close()
+
+
+class Audio_Capture(object):
+    """
+    PyAV based audio capture.
+    """
+
+    def __init__(self, file_loc,audio_src=0):
+        super(Audio_Capture, self).__init__()
 
         try:
             file_path,ext = file_loc.rsplit('.', 1)
@@ -239,38 +321,31 @@ class JPEG_Dumper(object):
             logger.error("'%s' is not a valid media file name."%file_loc)
             raise Exception("Error")
 
-        self.raw_path = file_path+'.raw'
-        self.out_path = file_loc
+        if ext not in ('wav'):
+            logger.error("media file container should be wav. Using a different container is not supported.")
+            raise NotImplementedError()
 
-        self.file_handle = open(self.raw_path, 'wb')
+        self.should_close = Event()
+        self.process = None
 
+        self.start(file_loc,audio_src)
 
-    def write_video_frame(self,frame):
-        self.file_handle.write(frame.jpeg_buffer.view())
+    def start(self,file_loc, audio_src):
+        self.should_close.clear()
+        self.process = Process(target=rec_thread, args=(file_loc, audio_src,self.should_close))
+        self.process.start()
 
-    def release(self):
-        self.file_handle.close()
-        cmd_bin = ffmpeg_bin()
-        if cmd_bin:
-            # ffmpeg  -f mjpeg -i world.raw -vcodec copy world.mkv
-            sp.Popen([cmd_bin+' -f mjpeg -i '+self.raw_path +' -vcodec copy '+self.out_path + '&& rm '+ self.raw_path],shell=True)
-            #this should be done programatically but requires a better video backend.
+    def stop(self):
+        self.should_close.set()
+        self.process.join()
+        self.process = None
 
+    def close(self):
+        self.stop()
 
-def ffmpeg_bin():
-    try:
-        sp.Popen('ffmpeg',stdout=open(os.devnull, 'wb'),stderr=open(os.devnull, 'wb'))
-    except OSError:
-        pass
-    else:
-        return 'ffmpeg'
-    try:
-        sp.Popen('avconv',stdout=open(os.devnull, 'wb'),stderr=open(os.devnull, 'wb'))
-    except OSError:
-        logger.error("Please install ffmpeg or libav-tools to enable pupil capture to record raw jpeg streams as a readable format.")
-        return None
-    else:
-        return 'avconv'
+    def __del__(self):
+        if self.process:
+            self.stop()
 
 # def test():
 
@@ -296,16 +371,23 @@ def ffmpeg_bin():
 #     writer.close()
 
 
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
+    # try:
+    #     from billiard import forking_enable
+    #     forking_enable(0)
+    # except ImportError:
+    #     pass
     logging.basicConfig(level=logging.DEBUG)
 
-    #mic device
+    cap = Audio_Capture('test.wav',0)
 
-    def format_time(time, time_base):
-        if time is None:
-            return 'None'
-        return '%.3fs (%s or %s/%s)' % (time_base * time, time_base * time, time_base.numerator * time, time_base.denominator)
+    import time
+    time.sleep(20)
+    cap.close()
+    #mic device
+    exit()
 
 
     container = av.open(':0',format="avfoundation")
@@ -318,6 +400,7 @@ if __name__ == '__main__':
     print
 
     print len(container.streams), 'stream(s):'
+    audio_stream = None
     for i, stream in enumerate(container.streams):
 
         print '\t%r' % stream
@@ -332,7 +415,8 @@ if __name__ == '__main__':
             print '\t\taudio:'
             print '\t\t\tformat:', stream.format
             print '\t\t\tchannels: %s' % stream.channels
-
+            audio_stream = stream
+            break
         elif stream.type == 'container':
             print '\t\tcontainer:'
             print '\t\t\tformat:', stream.format
@@ -342,20 +426,21 @@ if __name__ == '__main__':
         for k, v in sorted(stream.metadata.iteritems()):
             print '\t\t\t%s: %r' % (k, v)
 
-
+    if not audio_stream:
+        exit()
     #file contianer:
 
     out_container = av.open('test.wav','w')
     out_stream = out_container.add_stream('pcm_f32le')
     # out_stream.rate = 44100
-    for i,packet in enumerate(container.demux(container.streams[0])):
+    for i,packet in enumerate(container.demux(audio_stream)):
         # for frame in packet.decode():
         #     packet = out_stream.encode(frame)
         #     if packet:
-        # print '%r' %packet
-        # print '\tduration: %s' % format_time(packet.duration, packet.stream.time_base)
-        # print '\tpts: %s' % format_time(packet.pts, packet.stream.time_base)
-        # print '\tdts: %s' % format_time(packet.dts, packet.stream.time_base)
+        print '%r' %packet
+        print '\tduration: %s' % format_time(packet.duration, packet.stream.time_base)
+        print '\tpts: %s' % format_time(packet.pts, packet.stream.time_base)
+        print '\tdts: %s' % format_time(packet.dts, packet.stream.time_base)
         out_container.mux(packet)
         if i >1000:
             break
