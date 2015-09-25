45647535ed87c560b011a4d5106ce60e6e4df03b
Patrick Fürst <pat.fuerst@gmail.com>
2015-09-25 11:19:59 +0200

    added color image to detector_2d_pyx

--- pupil_src/capture/pupil_detectors/detector_2d.pyx
+++ pupil_src/capture/pupil_detectors/detector_2d.pyx
@@ -5,6 +5,7 @@ cimport numpy as np
 cdef extern from '<opencv2/core/types_c.h>':
 
   int CV_8UC1
+  int CV_8UC3
 
 
 cdef extern from '<opencv2/core/core.hpp>' namespace 'cv::Mat':
@@ -27,7 +28,7 @@ cdef extern from 'detect_2d.hpp':
     int test
     double test2
 
-  cdef Result detect( Mat& image, Rect_[int]& usr_roi, Mat& color_image , bint use_color_image )
+  cdef Result detect( Mat& image, Rect_[int]& usr_roi, Mat& color_image , bint visualize  )
 
 
 cdef class Detector_2D:
@@ -38,13 +39,15 @@ cdef class Detector_2D:
 
 
           cdef unsigned char[:,::1] img = frame.gray
+          cdef unsigned char[:,:,:] img_color = frame.img
           cdef Mat cv_image = Mat(height, width, CV_8UC1, <void *> &img[0,0] )
+          cdef Mat cv_image_color = Mat(height, width, CV_8UC3, <void *> &img_color[0,0,0] )
 
           x = roi.get()[0]
           y = roi.get()[1]
           width  = roi.get()[2] - roi.get()[0]
           height  = roi.get()[3] - roi.get()[1]
-          result =  detect( cv_image , Rect_[int](x,y,width,height), Mat(), False )
+          result =  detect( cv_image , Rect_[int](x,y,width,height), cv_image_color, True )
           return result
 
 
