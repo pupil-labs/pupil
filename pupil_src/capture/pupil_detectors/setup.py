91084e641219e93da22824c769530613568d29b9
Patrick Fürst <pat.fuerst@gmail.com>
2015-10-06 10:30:13 +0200

    add Eigen to include path

--- pupil_src/capture/pupil_detectors/setup.py
+++ pupil_src/capture/pupil_detectors/setup.py
@@ -27,7 +27,7 @@ extensions = [
         # configured to Andrew Xia's ubuntu installation location
         name="detector_2d",
         sources=['detector_2d.pyx','singleeyefitter/cvx.cpp','singleeyefitter/utils.cpp'], #I don't need cvx.
-        include_dirs = [ np.get_include()],
+        include_dirs = [ np.get_include() , '/usr/local/include/eigen3'],
         libraries = ['opencv_highgui','opencv_core','opencv_imgproc'],
         # library_dirs = ['/usr/local/lib'],
         extra_link_args=[], #'-WL,-R/usr/local/lib'
