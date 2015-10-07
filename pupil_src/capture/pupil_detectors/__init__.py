67872a28515869bcddcb788c9f395622cdb85e1d
Patrick Fürst <pat.fuerst@gmail.com>
2015-10-07 16:55:10 +0200

    expose detector_2D

--- pupil_src/capture/pupil_detectors/__init__.py
+++ pupil_src/capture/pupil_detectors/__init__.py
@@ -8,19 +8,6 @@
 ----------------------------------------------------------------------------------~(*)
 '''
 
-# make shared modules available across pupil_src
-if __name__ == '__main__':
-    from sys import path as syspath
-    from os import path as ospath
-    loc = ospath.abspath(__file__).rsplit('pupil_src', 1)
-    syspath.append(ospath.join(loc[0], 'pupil_src', 'shared_modules'))
-    del syspath, ospath
-
-import cv2
-import numpy as np
-from methods import *
-import logging
-logger = logging.getLogger(__name__)
-
 from canny_detector import Canny_Detector
+from detector_2d import Detector_2D
 
