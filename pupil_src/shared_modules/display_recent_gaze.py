38bdcd6fcd3d07c4fbdbd1edf826cc79aeaa0b6f
MichaelBarz <mb@barz.de>
2015-10-15 17:26:08 +0200

    Added signal smoothing for gaze visualization (OneEureFilter)

--- pupil_src/shared_modules/display_recent_gaze.py
+++ pupil_src/shared_modules/display_recent_gaze.py
@@ -10,6 +10,8 @@
 
 from plugin import Plugin
 from pyglui.cygl.utils import draw_points_norm,RGBA
+from one_euro_filter import OneEuroFilter
+from pyglui import ui
 
 class Display_Recent_Gaze(Plugin):
     """
@@ -17,17 +19,44 @@ class Display_Recent_Gaze(Plugin):
     recent gaze position on the screen
     """
 
-    def __init__(self, g_pool):
+    def __init__(self, g_pool, filter_active):
         super(Display_Recent_Gaze, self).__init__(g_pool)
         self.order = .8
         self.pupil_display_list = []
+        self.filter_active = filter_active
+        
+        config = {
+            'freq': 30,        # Hz
+            'mincutoff': 1, # lower values decrease jitter
+            'beta': 5,    # higher values decrease lag
+            'dcutoff': 1.0     # this one should be ok
+        }
+        self._filter_x = OneEuroFilter(**config)
+        self._filter_y = OneEuroFilter(**config)
+        
+    def filter(self, norm_pos):
+        if self.filter_active:
+            return self._filter_x(norm_pos[0]), self._filter_y(norm_pos[1])
+        else:
+            return norm_pos
 
     def update(self,frame,events):
         for pt in events.get('gaze_positions',[]):
-            self.pupil_display_list.append(pt['norm_pos'])
+            self.pupil_display_list.append(self.filter(pt['norm_pos']))
 
         self.pupil_display_list[:-3] = []
-
+        
+    def init_gui(self):
+        self.filter_switch = ui.Switch('filter_active',self,on_val=True,off_val=False,label='Smooth gaze visualization')
+        self.g_pool.sidebar[0].insert(-1,self.filter_switch)
+        
+    def deinit_gui(self):
+        if self.filter_switch:
+            self.g_pool.sidebar[0].remove(self.filter_switch)
+            self.filter_switch = None
+            
+    def cleanup(self):
+        self.deinit_gui()
 
     def gl_display(self):
         draw_points_norm(self.pupil_display_list,
@@ -35,4 +64,4 @@ class Display_Recent_Gaze(Plugin):
                         color=RGBA(1.,.2,.4,.6))
 
     def get_init_dict(self):
-        return {}
+        return {'filter_active':True}
