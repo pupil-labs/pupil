d4daba59781af70516173b4417cb1e5c97d9c187
Moritz Kassner <moritzkassner@gmail.com>
2015-11-12 15:35:21 +0100

    show undistorted map in camera intrinsics estimation to validate existing and new calibration.

--- pupil_src/capture/main.py
+++ pupil_src/capture/main.py
@@ -90,8 +90,8 @@ class Global_Container(object):
 def main():
 
     # To assign camera by name: put string(s) in list
-    world_src = ["Pupil Cam1 ID2","Logitech Camera","(046d:081d)","C510","B525", "C525","C615","C920","C930e"]
-    eye0 = ["Pupil Cam1 ID0","HD-6000","Integrated Camera","HD USB Camera","USB 2.0 Camera"]
+    world_src = ["Integrated Camera","Pupil Cam1 ID2","Logitech Camera","(046d:081d)","C510","B525", "C525","C615","C920","C930e"]
+    eye0 = ["Pupil Cam1 ID0","HD-6000","HD USB Camera","USB 2.0 Camera"]
     eye1 = ["Pupil Cam1 ID1","HD-6000","Integrated Camera"]
     eye_src = eye0, eye1
 
