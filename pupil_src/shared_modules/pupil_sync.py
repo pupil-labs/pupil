548d3421bd4f33f62d21b11fba2f2593b1027c75
Moritz Kassner <moritzkassner@gmail.com>
2015-11-23 10:56:20 +0100

    added continuous self adjusting time syncing to pupil sync.

--- pupil_src/shared_modules/pupil_sync.py
+++ pupil_src/shared_modules/pupil_sync.py
@@ -10,54 +10,28 @@
 
 
 from plugin import Plugin
-
 from pyglui import ui
 import zmq
-from pyre import Pyre
-from pyre import zhelper
-import uuid
+from pyre import Pyre, zhelper
+from uuid import UUID
 from time import sleep
+from threading import Timer
 import logging
 logger = logging.getLogger(__name__)
 
+from network_time_sync import Clock_Sync_Master,Clock_Sync_Follower
 
 start_rec = "START_REC:"
 stop_rec = "STOP_REC:"
 start_cal = "START_CAL"
 stop_cal = "STOP_CAL"
-sync_time_init = "SYNC_INIT:"
-sync_time_request = "SYNC_REQ:"
-sync_time_reply = "SYNC_RPL:"
+sync_time_master_announce = "SYNC_TIME_MASTER:"
 user_event = "USREVENT:"
+msg_delimeter  = '::'
 
 
 # Pipe signals:
 exit_thread = "EXIT_THREAD".encode('utf_8')
-init_master_sync = "INIT_SYNC".encode('utf_8')
-
-'''
-Time synchonization scheme:
-
-the initializing node is called master, others just nodes.
-the new timebase is the one the master currenlty runs on:
-the master then starts whispering to each node:
-for node in other_nodes:
-    send time req to node
-    now the node takes over command:
-    node send local time  (t0) to master
-    master returns with massage of own time stamp (t1)
-    Upon receipt by master, node take time t2 then: latency: t2-t0 target time = t1+latency/2
-    node sets timebase to target time.
-    for x in range(5)
-        measure t0,t1,t2, latency, offset = local_time -target_time
-    calulate mean, variance of latency
-        take avg offset of measurements that are not outliers
-    apply offset to local time
-
-'''
-
-
-
 
 class Pupil_Sync(Plugin):
     """Synchonize behaviour of Pupil captures
@@ -72,19 +46,67 @@ class Pupil_Sync(Plugin):
         self.menu = None
         self.group_menu = None
 
+
+        #variables for the time sync logic
+        self.sync_node = None
+        self.last_master_announce = self.get_monotonic_time()
+
+        #constants for the time sync logic
+        self.time_sync_announce_interval = 5
+        self.time_sync_wait_interval_short = self.time_sync_announce_interval * 3
+        self.time_sync_wait_interval_long = self.time_sync_announce_interval * 4
+
+
         self.context = zmq.Context()
         self.thread_pipe = zhelper.zthread_fork(self.context, self.thread_loop)
 
 
 
-        #variables for the time sync logic
-        self.sync_master = None
-        self.sync_data = []
-        self.sync_to_collect = 0
-        self.sync_nodes = []
-        self.timeout = None
 
+    def clock_master_worthiness(self):
+        '''
+        How worthy am I to be the clock master?
+        A measure 0 (unworthy) to 1 (destined)
+        tie-breaking is does via bigger uuid.int
 
+        At current this is done via
+        '''
+        if self.g_pool.timebase.value == 0:
+            return 0.
+        else:
+            return 0.5
+
+    ###time sync fns these are used by the time sync node to get and adjust time
+    def get_monotonic_time(self):
+        #return time not influced by outside clocks.
+        return self.g_pool.capture.get_now()
+
+    def get_time(self):
+        return self.g_pool.capture.get_timestamp()
+
+    def set_time(self,offset):
+        self.g_pool.timebase.value +=offset
+
+    def jump_time(self,offset):
+        ok_to_change = True
+        for p in self.g_pool.plugins:
+            if p.class_name == 'Recorder':
+                if p.running:
+                    ok_to_change = False
+                    logger.error("Request to change timebase during recording ignored. Turn off recording first.")
+                    break
+        if ok_to_change:
+            self.set_time(offset)
+            logger.info("Pupil Sync has adjusted the clock by %ss"%offset)
+            return True
+        else:
+            return False
+
+    def sync_status_info(self):
+        if self.sync_node is None:
+            return 'Waiting for time sync msg.'
+        else:
+            return str(self.sync_node)
 
     def init_gui(self):
         help_str = "Synchonize behaviour of Pupil captures across the local network."
@@ -93,9 +115,10 @@ class Pupil_Sync(Plugin):
         self.menu.append(ui.Info_Text(help_str))
         self.menu.append(ui.Text_Input('name',self,setter=self.set_name,label='Name'))
         self.menu.append(ui.Text_Input('group',self,setter=self.set_group,label='Group'))
-        help_str = "Before starting a recording. Make sure to sync the timebase of all Pupils to one master Pupil by clicking the bottom below. This will apply this Pupil's timebase to all of its group."
+        help_str = "All pupil nodes of one group share a Master clock."
         self.menu.append(ui.Info_Text(help_str))
-        self.menu.append(ui.Button('sync time for all Pupils',self.set_sync))
+        self.menu.append(ui.Text_Input('sync status',getter=self.sync_status_info,setter=lambda _: _))
+        # self.menu[-1].read_only = True
         self.group_menu = ui.Growing_Menu('Other Pupils')
         self.menu.append(self.group_menu)
         self.g_pool.sidebar.append(self.menu)
@@ -104,8 +127,8 @@ class Pupil_Sync(Plugin):
     def update_gui(self):
         if self.group_menu:
             self.group_menu.elements[:] = []
-            for uid in self.group_members.keys():
-                self.group_menu.append(ui.Info_Text("%s"%self.group_members[uid]))
+            for uuid in self.group_members.keys():
+                self.group_menu.append(ui.Info_Text("%s"%self.group_members[uuid]))
 
     def set_name(self,new_name):
         self.name = new_name
@@ -125,24 +148,6 @@ class Pupil_Sync(Plugin):
         self.update_gui()
         self.thread_pipe = zhelper.zthread_fork(self.context, self.thread_loop)
 
-    def set_sync(self):
-        self.sync_master = self
-        self.sync_nodes = self.group_members.keys()
-        self.thread_pipe.send(init_master_sync)
-
-    def set_timebase(self,new_time):
-        self.g_pool.timebase.value = float(new_time)
-        logger.debug("New timebase set to %s all timestamps will count from here now."%self.g_pool.timebase.value)
-
-
-    def ok_to_set_timebase(self):
-        ok_to_change = True
-        for p in self.g_pool.plugins:
-            if p.class_name == 'Recorder':
-                if p.running:
-                    ok_to_change = False
-                    logger.warning("Request to change timebase during recording ignored. Turn off recording first.")
-        return ok_to_change
 
     def close(self):
         self.alive = False
@@ -152,24 +157,63 @@ class Pupil_Sync(Plugin):
             self.g_pool.sidebar.remove(self.menu)
             self.menu = None
 
-
     def thread_loop(self,context,pipe):
         n = Pyre(self.name)
         n.join(self.group)
         n.start()
         poller = zmq.Poller()
         poller.register(pipe, zmq.POLLIN)
-        logger.debug(n.socket())
         poller.register(n.socket(), zmq.POLLIN)
+
+
+        front,back = zhelper.zcreate_pipe(context)
+        poller.register(back, zmq.POLLIN)
+        def wake_up():
+            #on app close this timer calls a closed socket. We simply catch it here.
+            try:
+                front.send('wake_up')
+            except Exception as e:
+                logger.error(e)
+
+        t = Timer(self.time_sync_announce_interval, wake_up)
+        t.daemon = True
+        t.start()
 
         while(True):
             try:
                 #this should not fail but it does sometimes. We need to clean this out.
                 # I think we are not treating sockets correclty as they are not thread-save.
-                items = dict(poller.poll(self.timeout))
+                items = dict(poller.poll())
             except zmq.ZMQError:
                 logger.warning('Socket fail.')
-            # print(n.socket(), items)
+                continue
+
+            if back in items and items[back] == zmq.POLLIN:
+                back.recv()
+                #timeout events are used for pupil sync.
+                #annouce masterhood every interval time:
+                if isinstance(self.sync_node,Clock_Sync_Master):
+                    n.shouts(self.group, sync_time_master_announce+"%s"%self.clock_master_worthiness()+msg_delimeter+'%s'%self.sync_node.port)
+
+                # synced slave: see if we should become master if we dont hear annoncement within time.
+                elif isinstance(self.sync_node,Clock_Sync_Follower) and not self.sync_node.offset_remains:
+                    if self.get_monotonic_time()-self.last_master_announce > self.time_sync_wait_interval_short:
+                        self.sync_node.terminate()
+                        self.sync_node = Clock_Sync_Master(time_fn=self.get_time)
+                        n.shouts(self.group, sync_time_master_announce+"%s"%self.clock_master_worthiness()+msg_delimeter+'%s'%self.sync_node.port)
+
+                # unsynced slave or none should wait longer but eventually take over
+                elif self.get_monotonic_time()-self.last_master_announce > self.time_sync_wait_interval_long:
+                    if self.sync_node:
+                        self.sync_node.terminate()
+                    self.sync_node = Clock_Sync_Master(time_fn=self.get_time)
+                    n.shouts(self.group, sync_time_master_announce+"%s"%self.clock_master_worthiness()+msg_delimeter+'%s'%self.sync_node.port)
+
+                t = Timer(self.time_sync_announce_interval, wake_up)
+                t.daemon = True
+                t.start()
+
+
             if pipe in items and items[pipe] == zmq.POLLIN:
                 message = pipe.recv()
                 # message to quit
@@ -185,61 +229,86 @@ class Pupil_Sync(Plugin):
                 msg_type = cmds.pop(0)
                 msg_type = msg_type.decode('utf-8')
                 if msg_type == "SHOUT":
-                    uid,name,group,msg = cmds
+                    uuid,name,group,msg = cmds
                     logger.debug("'%s' shouts '%s'."%(name,msg))
-                    self.handle_msg(name,msg)
+                    self.handle_msg(uuid,name,msg,n)
 
                 elif msg_type == "WHISPER":
-                    uid,name,msg = cmds
+                    uuid,name,msg = cmds
                     logger.debug("'%s/' whispers '%s'."%(name,msg))
-                    self.handle_msg_whisper(uid,name,msg,n)
+                    self.handle_msg_whisper(uuid,name,msg,n)
 
                 elif msg_type == "JOIN":
-                    uid,name,group = cmds
+                    uuid,name,group = cmds
                     if group == self.group:
-                        self.group_members[uid] = name
+                        self.group_members[uuid] = name
                         self.update_gui()
 
                 elif msg_type == "EXIT":
-                    uid,name = cmds
+                    uuid,name = cmds
                     try:
-                        del self.group_members[uid]
+                        del self.group_members[uuid]
                     except KeyError:
                         pass
                     else:
                         self.update_gui()
 
                 # elif msg_type == "LEAVE":
-                #     uid,name,group = cmds
+                #     uuid,name,group = cmds
                 # elif msg_type == "ENTER":
-                #     uid,name,headers,ip = cmds
-                #     logger.warning((uid,'name',headers,ip))
-
-            elif not items:
-                #timeout events are used for pupil sync.
-                if self.sync_master is self:
-                    if self.sync_nodes:
-                        node_uid = self.sync_nodes.pop(0)
-                        logger.info("Synchonizing node %s"%self.group_members[node_uid])
-                        n.whispers(uuid.UUID(bytes=node_uid),sync_time_init)
-                    else:
-                        self.timeout = None
-                        self.sync_master = None
-                        logger.info("All other Pupil nodes are sycronized.")
-                elif self.sync_master:
-                    t0 = self.g_pool.capture.get_timestamp()
-                    n.whispers(uuid.UUID(bytes=self.sync_master),sync_time_request+'%s'%t0)
-
+                #     uuid,name,headers,ip = cmds
+                #     logger.warning((uuid,'name',headers,ip))
             else:
                 pass
 
         logger.debug('thread_loop closing.')
+
         self.thread_pipe = None
         n.stop()
 
 
-    def handle_msg(self,name,msg):
-        if start_rec in msg :
+    def handle_msg(self,uuid,name,msg,n):
+
+        #Clock Sync master announce logic
+        if sync_time_master_announce in msg:
+
+            self.last_master_announce = self.get_monotonic_time()
+
+            worthiness,port = msg.replace(sync_time_master_announce,'').split(msg_delimeter)
+            foreign_master_worthiness = float(worthiness)
+            foreign_master_port = int(port)
+            forein_master_uuid = UUID(bytes=uuid)
+            foreign_master_address = n.peer_address(forein_master_uuid)
+            foreign_master_ip = foreign_master_address.split('//')[-1].split(':')[0]  # tcp://10.0.1.68:59149
+
+            if isinstance(self.sync_node,Clock_Sync_Master):
+                # who should yield?
+                if not self.was_adjusted() and foreign_master_adjusted:
+                    should_yield = True
+                elif self.nerver_adjusted() and (not foreign_master_adjusted):
+                    should_yield = False
+                else:
+                    if n.uuid().int > forein_master_uuid.int:
+                        self.should_yield = False
+                    else:
+                        self.should_yield = True
+
+                if should_yield:
+                    logger.warning("Yield Clock_Sync_Master to %s@%s"%(name,new_master))
+                    self.sync_node.stop()
+                    self.sync_node = Clock_Sync_Follower(foreign_master_ip,port=foreign_master_port,interval=10,time_fn=self.get_time,jump_fn=self.jump_time,slew_fn=self.set_time)
+                else:
+                    logger.warning("Dominate as Clock_Sync_Master")
+                    n.shouts(self.group, sync_time_master_announce+"%s"%self.clock_master_worthiness()+msg_delimeter+'%s'%self.sync_node.port)
+
+            elif isinstance(self.sync_node,Clock_Sync_Follower):
+                self.sync_node.host = foreign_master_ip
+                self.sync_node.port = foreign_master_port
+            else:
+                self.sync_node = Clock_Sync_Follower(foreign_master_ip,port=foreign_master_port,interval=10,time_fn=self.get_time,jump_fn=self.jump_time,slew_fn=self.set_time)
+                logger.debug("Clock synced with %s"%foreign_master_ip)
+
+        elif start_rec in msg :
             session_name = msg.replace(start_rec,'')
             self.notify_all({'subject':'rec_should_start','session_name':session_name,'network_propagate':False})
         elif stop_rec in msg:
@@ -254,45 +323,7 @@ class Pupil_Sync(Plugin):
             self.notify_all({'subject':'remote_user_event','user_event_name':user_event_name,'timestamp':float(timestamp),'network_propagate':False,'sender':name,'received_timestamp':self.g_pool.capture.get_timestamp()})
 
     def handle_msg_whisper(self,peer,name,msg,node):
-
-        #when acting as master during sync all we need to do is reply to requests.
-        if sync_time_request in msg:
-            t0 = msg.replace(sync_time_request,'')
-            node.whispers(uuid.UUID(bytes=peer),sync_time_reply+t0+':'+"%s"%self.g_pool.capture.get_timestamp())
-
-
-        #as node during sync we need to note reply times measure and set our timebase.
-        elif sync_time_reply in msg:
-            t2 = self.g_pool.capture.get_timestamp()
-            t0_t1 = msg.replace(sync_time_reply,'')
-            t0,t1 = (float(t) for t in t0_t1.split(':'))
-
-            logger.info("Round trip sync latency %sms "%(t2-t0))
-
-            if self.sync_to_collect == 5:
-                #first run, we do a coarse adjustment
-                new_time = t1 + ((t2-t0)/2.)
-                if self.ok_to_set_timebase:
-                    self.set_timebase(new_time)
-
-            elif self.sync_to_collect > 0:
-                #collect 4 more samples
-                self.sync_data.append((t0,t1,t2))
-
-
-            elif self.sync_to_collect == 0:
-                self.timeout = None
-
-            self.sync_to_collect -= 1
-
-
-        elif sync_time_init in msg:
-            self.sync_master = peer
-            self.sync_data = []
-            self.sync_to_collect = 5
-             #from now on zmq.poller will timeout when the lines are clear and we use this to send sync messages.
-            self.timeout = 200
-
+        logger.warning('%s %s %s %s'%(peer,name,msg,node))
 
 
     def on_notify(self,notification):
@@ -318,6 +349,8 @@ class Pupil_Sync(Plugin):
         """gets called when the plugin get terminated.
            This happens either volunatily or forced.
         """
+        if self.sync_node:
+            self.sync_node.stop()
         self.deinit_gui()
         self.thread_pipe.send(exit_thread)
         while self.thread_pipe:
