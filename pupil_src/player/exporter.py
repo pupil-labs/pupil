39bf2447b162e1b0e450d9644b0313eff95ff6d0
Moritz Kassner <moritzkassner@gmail.com>
2016-01-18 18:11:48 +0100

    removed gaze mapper for player exporter.

--- pupil_src/player/exporter.py
+++ pupil_src/player/exporter.py
@@ -42,7 +42,6 @@ from vis_watermark import Vis_Watermark
 from scan_path import Scan_Path
 from manual_gaze_correction import Manual_Gaze_Correction
 from eye_video_overlay import Eye_Video_Overlay
-from calibration_routines.gaze_mappers import Dummy_Gaze_Mapper,Simple_Gaze_Mapper,Volumetric_Gaze_Mapper,Bilateral_Gaze_Mapper
 from fixation_detector import Dispersion_Duration_Fixation_Detector
 
 
@@ -50,8 +49,6 @@ available_plugins = Vis_Circle,Vis_Cross, Vis_Polyline, \
                     Vis_Light_Points, Vis_Watermark, \
                     Scan_Path, \
                     Manual_Gaze_Correction,Eye_Video_Overlay, \
-                    Dummy_Gaze_Mapper,Simple_Gaze_Mapper, \
-                    Volumetric_Gaze_Mapper,Bilateral_Gaze_Mapper, \
                     Dispersion_Duration_Fixation_Detector
 name_by_index = [p.__name__ for p in available_plugins]
 index_by_name = dict(zip(name_by_index,range(len(name_by_index))))
