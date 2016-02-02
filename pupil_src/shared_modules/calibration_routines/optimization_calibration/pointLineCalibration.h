bdee117895389455aad33c363b3b4fe9161b7bdb
Patrick Fürst <pat.fuerst@gmail.com>
2016-02-02 15:44:51 +0100

    use quaternion for rotation

--- pupil_src/shared_modules/calibration_routines/optimization_calibration/pointLineCalibration.h
+++ pupil_src/shared_modules/calibration_routines/optimization_calibration/pointLineCalibration.h
@@ -22,34 +22,41 @@ struct TransformationError {
 
     template <typename T>
     bool operator()(
-        const T* const R_t,  // Rotation denoted by angle axis
+        const T* const R,  // Rotation denoted by quaternion
+        const T* const t,
         // followed with translation
         T* residuals) const
     {
 
 
-        Eigen::Matrix<T, 3, 1> translation;
-        translation[0] = R_t[3];
-        translation[1] = R_t[4];
-        translation[2] = R_t[5];
         // Compute coordinates with current transformation matrix: x = RX + t.
-        Eigen::Matrix<T, 3, 1> p1;
-        Eigen::Matrix<T, 3, 1> p2;
+
 
         T g1[3] = {T(gazePoint1[0]), T(gazePoint1[1]), T(gazePoint1[2])};
         T g2[3] = {T(gazePoint2[0]), T(gazePoint2[1]), T(gazePoint2[2])};
 
-        ceres::AngleAxisRotatePoint(R_t, g1 , p1.data());
-        ceres::AngleAxisRotatePoint(R_t, g2, p2.data());
+        T p1[3];
+        T p2[3];
+        ceres::QuaternionRotatePoint(R, g1, p1 );
+        ceres::QuaternionRotatePoint(R, g2, p2 );
 
-        p1 += translation;
-        p2 += translation;
+        p1[0] += t[0];
+        p1[1] += t[1];
+        p1[2] += t[2];
+        p2[0] += t[0];
+        p2[1] += t[1];
+        p2[2] += t[2];
 
         Eigen::Matrix<T, 3, 1> refP;
         refP << T(referencePoint[0]) , T(referencePoint[1]) , T(referencePoint[2]);
 
+        Eigen::Matrix<T, 3, 1> ep1;
+        ep1 << T(p1[0]) , T(p1[1]) , T(p1[2]);
+
+        Eigen::Matrix<T, 3, 1> ep2;
+        ep2 << T(p2[0]) , T(p2[1]) , T(p2[2]);
         // now calculate the distance between the observed point and the nearest point on the line
-        residuals[0] = (refP - p1).cross(refP - p2).norm() / (p2 - p1).norm();
+        residuals[0] = (refP - ep1).cross(refP - ep2).norm() / (ep2 - ep1).norm();
         return true;
     }
 
@@ -65,31 +72,27 @@ Eigen::Matrix4d pointLineCalibration(Vector3 spherePosition, const std::vector<V
 
 
     Problem problem;
-    double transformation[6] = {0.0 , -1.5 , 0.0 , -30.0 , -30.0  , -20.0 };
+
+    double angle_axis[3] = {0.0,0,0.0};
+    double rotation[4];
+    ceres::AngleAxisToQuaternion(  angle_axis ,rotation );
+    double translation[3] = { 0.0 , 0.0  , 0.0 };
 
 
-    // Parameterization used to restrict camera motion for modal solvers.
-    ceres::SubsetParameterization* constant_transform_parameterization = NULL;
-    std::vector<int> constant_translation;
-    // First three elements are rotation, last three are translation.
-    constant_translation.push_back(3);
-    constant_translation.push_back(4);
-    constant_translation.push_back(5);
-    constant_transform_parameterization =
-        new ceres::SubsetParameterization(6, constant_translation);
-
+    ceres::LocalParameterization* quaternion_parameterization = new ceres::QuaternionParameterization;
 
     int i = 0;
 
     for (auto& p : refPoints) {
         auto g = gazeDirections.at(i);
         i++;
-        CostFunction* cost = new AutoDiffCostFunction<TransformationError , 1, 6 >(new TransformationError(p , g , spherePosition));
-        problem.AddResidualBlock(cost, nullptr, &transformation[0]);
+        CostFunction* cost = new AutoDiffCostFunction<TransformationError , 1, 4, 3 >(new TransformationError(p , g , spherePosition));
+        problem.AddResidualBlock(cost, nullptr, &rotation[0],  &translation[0]);
     }
 
-    problem.SetParameterization(&transformation[0],
-                                constant_transform_parameterization);
+    problem.SetParameterBlockConstant(&translation[0]);
+
+    problem.SetParameterization(&rotation[0], quaternion_parameterization);
 
 
     // Build and solve the problem.
@@ -100,31 +103,42 @@ Eigen::Matrix4d pointLineCalibration(Vector3 spherePosition, const std::vector<V
     options.function_tolerance = 1e-10;
     options.gradient_tolerance = 1e-10;
     // options.minimizer_type = ceres::TRUST_REGION;
-    options.minimizer_progress_to_stdout = true;
+    //options.minimizer_progress_to_stdout = true;
     options.check_gradients = true;
     Solver::Summary summary;
     Solve(options, &problem, &summary);
     // // Recover r from m.
 
-    // std::cout << summary.BriefReport() << "\n";
-    std::cout << summary.FullReport() << "\n";
-
-    for (int i = 0; i < 6; i++) {
-        std::cout << "," << transformation[i] << std::endl;
+     std::cout << summary.BriefReport() << "\n";
+    //std::cout << summary.FullReport() << "\n";
+   for (int i = 0; i < 4; i++) {
+        std::cout << "," << rotation[i] << std::endl;
+    }
+    for (int i = 0; i < 3; i++) {
+        std::cout << "," << translation[i] << std::endl;
     }
 
-    Eigen::Matrix3d m;
-    m = Eigen::AngleAxisd(transformation[0], Vector3::UnitX())
-        * Eigen::AngleAxisd(transformation[1], Vector3::UnitY())
-        * Eigen::AngleAxisd(transformation[2], Vector3::UnitZ());
+
+    Eigen::Matrix<double, 3, 3, Eigen::RowMajor> r;
+    Eigen::Matrix<double, 3, 3> rc;
+    ceres::QuaternionToRotation( rotation , r.data() );
+    std::cout << "r: " << r << std::endl;
+    std::cout << "det: " << r.determinant() << std::endl;
+    rc = Eigen::Map<Eigen::Matrix<double,3,3,Eigen::RowMajor> >(r.data());
+
+
+
 
     Eigen::Matrix4d t = Eigen::Matrix4d::Identity();
-    t.block<3, 3>(0, 0) = m;
-    t(0, 3) = transformation[3];
-    t(1, 3) = transformation[4];
-    t(2, 3) = transformation[5];
-
+    t.block<3,3>(0,0) = rc;
+    t(0, 3) = translation[0];
+    t(1, 3) = translation[1];
+    t(2, 3) = translation[2];
     std::cout << "transformation: "  << t  << std::endl;
+
+    // Eigen::Matrix4d tt = Eigen::Map<Eigen::Matrix<double,4,4,Eigen::RowMajor> >(t.data());
+    // std::cout << "transformation: "  << tt  << std::endl;
+
     return t;
 }
 
