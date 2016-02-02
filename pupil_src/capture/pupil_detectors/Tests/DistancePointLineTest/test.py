40938008b6c0244d2ab7712d108a65b5d0db22b8
Patrick Fürst <pat.fuerst@gmail.com>
2016-02-02 11:13:18 +0100

    add test for point line distance

---
+++ pupil_src/capture/pupil_detectors/Tests/DistancePointLineTest/test.py
@@ -0,0 +1,46 @@
+
+
+import numpy as np
+
+
+def distance_point_line( ref_point, line ):
+
+    p1 = line[0]
+    p2 = line[1]
+
+    distance = np.linalg.norm( np.cross((ref_point-p1),(ref_point-p2)) )  / np.linalg.norm((p2 -p1));
+    return distance
+
+
+if __name__ == '__main__':
+
+
+    line_p1 = np.array([0.0,0.0,0.0 ])
+    line_p2 = np.array([0.0,0.0,1.0 ])
+
+    #find nearest point on line for ref point
+    ref_point = np.array([0.0,0.5,1.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == 0.5
+
+    ref_point = np.array([0.0,-0.5,1.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == 0.5
+
+    ref_point = np.array([0.5,0.5,1.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == np.sqrt(0.5)
+
+    ref_point = np.array([-0.5,0.5,1.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == np.sqrt(0.5)
+
+    line_p2 = np.array([0.0,0.0,-1.0 ])
+    ref_point = np.array([-0.5,0.5,1.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == np.sqrt(0.5)
+
+    line_p2 = np.array([0.0,0.0,1.0 ])
+    ref_point = np.array([ 0.0,0.0,0.0])
+    distance =  distance_point_line( ref_point , (line_p1, line_p2) )
+    assert distance == 0.0
