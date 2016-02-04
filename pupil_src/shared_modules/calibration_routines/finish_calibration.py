070674352b644b2140585e826b33816d949a7aa7
Moritz Kassner <moritzkassner@gmail.com>
2016-02-04 20:48:00 +0100

    record notifications

--- pupil_src/shared_modules/calibration_routines/finish_calibration.py
+++ pupil_src/shared_modules/calibration_routines/finish_calibration.py
@@ -18,7 +18,7 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
         pass
     else:
         logger.error(not_enough_data_error_msg)
-        g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp()})
+        g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
         return
 
     camera_intrinsics = load_camera_calibration(g_pool)
@@ -65,7 +65,7 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
                 ref_3d = cal_pt_cloud[:,2]
             except:
                 logger.error(not_enough_data_error_msg)
-                g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp()})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
 
                 return
 
@@ -127,7 +127,7 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
                 ref_3d = cal_pt_cloud[:,1]
             except:
                 logger.error(not_enough_data_error_msg)
-                g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp()})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
                 return
 
             best_distance = 1000
@@ -163,7 +163,7 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
 
         else:
             logger.error(not_enough_data_error_msg)
-            g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp()})
+            g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
             return
     elif mode == '2d':
         if matched_binocular_data:
@@ -184,11 +184,11 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
             g_pool.plugins.add(Simple_Gaze_Mapper,args={'params':params})
         else:
             logger.error(not_enough_data_error_msg)
-            g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp()})
+            g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
 
             return
 
     user_calibration_data = {'pupil_list':pupil_list,'ref_list':ref_list,'calibration_method':method}
     save_object(user_calibration_data,os.path.join(g_pool.user_dir, "user_calibration_data"))
-    g_pool.active_calibration_plugin.notify_all({'subject':'calibration_successful','method':method,'timestamp':g_pool.capture.get_timestamp(),})
+    g_pool.active_calibration_plugin.notify_all({'subject':'calibration_successful','method':method,'timestamp':g_pool.capture.get_timestamp(),'record':True})
 
