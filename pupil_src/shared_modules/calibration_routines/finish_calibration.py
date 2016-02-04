773f1736581158b48fb48e06a16a4ee539205c53
Moritz Kassner <moritzkassner@gmail.com>
2016-02-04 20:51:00 +0100

    removed whitespace.

--- pupil_src/shared_modules/calibration_routines/finish_calibration.py
+++ pupil_src/shared_modules/calibration_routines/finish_calibration.py
@@ -66,7 +66,6 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
             except:
                 logger.error(not_enough_data_error_msg)
                 g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
-
                 return
 
             best_distance = 1000
@@ -158,13 +157,13 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
             avg_distance, dist_var = calibrate.calculate_residual_3D_Points( ref_3d, gaze_3d, eye_to_world_matrix )
             print 'best calibration average distance: ' , avg_distance
             # print 'best calibration distance variance: ' , dist_var
-
             g_pool.plugins.add(Vector_Gaze_Mapper,args={'eye_to_world_matrix':eye_to_world_matrix , 'camera_intrinsics': camera_intrinsics , 'cal_ref_points_3d': cal_pt_cloud[:,1].tolist(), 'cal_gaze_points_3d': cal_pt_cloud[:,0].tolist()})
 
         else:
             logger.error(not_enough_data_error_msg)
             g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
             return
+
     elif mode == '2d':
         if matched_binocular_data:
             method = 'binocular polynomial regression'
@@ -185,7 +184,6 @@ def finish_calibration(g_pool,pupil_list,ref_list,calibration_distance_3d = 500,
         else:
             logger.error(not_enough_data_error_msg)
             g_pool.active_calibration_plugin.notify_all({'subject':'calibration_failed','reason':not_enough_data_error_msg,'timestamp':self.g_pool.capture.get_timestamp(),'record':True})
-
             return
 
     user_calibration_data = {'pupil_list':pupil_list,'ref_list':ref_list,'calibration_method':method}
