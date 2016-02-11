b09606a207338ec4ed425f31692354820258d323
Patrick Fürst <pat.fuerst@gmail.com>
2016-02-11 17:21:11 +0100

    add path of shared_cpp/include to include_dir

--- pupil_src/capture/pupil_detectors/setup.py
+++ pupil_src/capture/pupil_detectors/setup.py
@@ -31,11 +31,13 @@ for dirpath, dirnames, filenames in os.walk("singleeyefitter"):
     for filename in [f for f in filenames if f.endswith(".h")]:
         dependencies.append( os.path.join(dirpath, filename) )
 
+shared_cpp_include_path = '../../shared_cpp/include'
+
 extensions = [
     Extension(
         name="detector_2d",
         sources=['detector_2d.pyx','singleeyefitter/ImageProcessing/cvx.cpp','singleeyefitter/utils.cpp','singleeyefitter/detectorUtils.cpp' ],
-        include_dirs = [ np.get_include() , '/usr/local/include/eigen3','/usr/include/eigen3'],
+        include_dirs = [ np.get_include() , '/usr/local/include/eigen3','/usr/include/eigen3', shared_cpp_include_path],
         libraries = ['opencv_highgui','opencv_core','opencv_imgproc' , 'boost_python'],
         #library_dirs = ['/usr/local/lib'],
         extra_link_args=[], #'-WL,-R/usr/local/lib'
@@ -45,7 +47,7 @@ extensions = [
      Extension(
         name="detector_3d",
         sources=['detector_3d.pyx','singleeyefitter/ImageProcessing/cvx.cpp','singleeyefitter/utils.cpp','singleeyefitter/detectorUtils.cpp', 'singleeyefitter/EyeModelFitter.cpp','singleeyefitter/EyeModel.cpp'],
-        include_dirs = [ np.get_include() , '/usr/local/include/eigen3','/usr/include/eigen3'],
+        include_dirs = [ np.get_include() , '/usr/local/include/eigen3','/usr/include/eigen3', shared_cpp_include_path],
         libraries = ['opencv_highgui','opencv_core','opencv_imgproc', 'opencv_video', 'ceres', 'boost_python' ],
         # library_dirs = ['/usr/local/lib'],
         extra_link_args=[], #'-WL,-R/usr/local/lib'
