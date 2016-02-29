0a1ef378c5fb78a5ba708314ce6e2c79f5c01228
Patrick Fürst <pat.fuerst@gmail.com>
2016-02-29 14:30:06 +0100

    inherit from Visualizer

--- pupil_src/capture/pupil_detectors/visualizer_3d.py
+++ pupil_src/capture/pupil_detectors/visualizer_3d.py
@@ -1,28 +1,12 @@
 
-import logging
-from glfw import *
+from visualizer import Visualizer
 from OpenGL.GL import *
-from OpenGL.GLU import gluOrtho2D
-
-# create logger for the context of this function
-logger = logging.getLogger(__name__)
-from pyglui import ui
-
-from pyglui.cygl.utils import *
+from pyglui.cygl import utils as glutils
+from pyglui.cygl.utils import RGBA
 from gl_utils.trackball import Trackball
-from pyglui.pyfontstash import fontstash as fs
-from pyglui.ui import get_opensans_font_path
 import numpy as np
 import math
-import cv2
-import random
 
-
-
-def convert_fov(fov,width):
-	fov = fov* math.pi/180
-	focal_length = (width/2)/np.tan(fov/2)
-	return focal_length
 
 def get_perpendicular_vector(v):
     """ Finds an arbitrary perpendicular vector to *v*."""
@@ -50,25 +34,17 @@ def get_perpendicular_vector(v):
     #     c = -(x + y)/z
     return np.array([1, 1, -1.0 * (v[0] + v[1]) / v[2]])
 
-class Visualizer(object):
-	def __init__(self,focal_length, name = "Debug Visualizer", run_independently = False):
-       # super(Visualizer, self).__init__()
+class Eye_Visualizer(Visualizer):
+	def __init__(self,g_pool , focal_length ):
+		super(Eye_Visualizer, self).__init__(g_pool , "Debug Visualizer", False)
+
 		self.focal_length = focal_length
 		self.image_width = 640 # right values are assigned in update
 		self.image_height = 480
-		# transformation matrices
-		self.anthromorphic_matrix = self.get_anthropomorphic_matrix()
-
-		self.name = name
-		self.window_size = (640,480)
-		self._window = None
-		self.input = None
-		self.run_independently = run_independently
 
 		camera_fov = math.degrees(2.0 * math.atan( self.image_height / (2.0 * self.focal_length)))
 		self.trackball = Trackball(camera_fov)
 
-		self.sphere = None
 	############## MATRIX FUNCTIONS ##############################
 
 	def get_anthropomorphic_matrix(self):
@@ -134,97 +110,6 @@ class Visualizer(object):
 
 	############## DRAWING FUNCTIONS ##############################
 
-	def draw_frustum(self ):
-
-		W = self.image_width/2.0
-		H = self.image_height/2.0
-		Z = self.focal_length
-		glPushMatrix()
-		# draw it
-		glLineWidth(1)
-		glColor4f( 1, 0.5, 0, 0.5 )
-		glBegin( GL_LINE_LOOP )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( -W, H, Z )
-		glVertex3f( W, H, Z )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( W, H, Z )
-		glVertex3f( W, -H, Z )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( W, -H, Z )
-		glVertex3f( -W, -H, Z )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( -W, -H, Z )
-		glVertex3f( -W, H, Z )
-		glEnd( )
-		glPopMatrix()
-
-	def draw_coordinate_system(self,l=1):
-		# Draw x-axis line. RED
-		glLineWidth(2)
-		glColor3f( 1, 0, 0 )
-		glBegin( GL_LINES )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( l, 0, 0 )
-		glEnd( )
-
-		# Draw y-axis line. GREEN.
-		glColor3f( 0, 1, 0 )
-		glBegin( GL_LINES )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( 0, l, 0 )
-		glEnd( )
-
-		# Draw z-axis line. BLUE
-		glColor3f( 0, 0, 1 )
-		glBegin( GL_LINES )
-		glVertex3f( 0, 0, 0 )
-		glVertex3f( 0, 0, l )
-		glEnd( )
-
-	def draw_sphere(self,sphere_position, sphere_radius,contours = 45, color =RGBA(.2,.5,0.5,.5) ):
-
-		glPushMatrix()
-		glTranslatef(sphere_position[0],sphere_position[1],sphere_position[2])
-		glScale(sphere_radius,sphere_radius,sphere_radius)
-		self.sphere.draw(color, primitive_type = GL_LINE_STRIP)
-		glPopMatrix()
-
-	def draw_circle(self, circle_center, circle_normal, circle_radius, color=RGBA(1.1,0.2,.8), num_segments = 20):
-		vertices = []
-		vertices.append( (0,0,0) )  # circle center
-
-		#create circle vertices in the xy plane
-		for i in np.linspace(0.0, 2.0*math.pi , num_segments ):
-			x = math.sin(i)
-			y = math.cos(i)
-			z = 0
-			vertices.append((x,y,z))
-
-		glPushMatrix()
-		glMatrixMode(GL_MODELVIEW )
-		glLoadMatrixf(self.get_pupil_transformation_matrix(circle_normal,circle_center, circle_radius))
-		draw_polyline((vertices),color=color, line_type = GL_TRIANGLE_FAN) # circle
-		draw_polyline( [ (0,0,0), (0,0, 4) ] ,color=RGBA(0,0,0), line_type = GL_LINES) #normal
-		glPopMatrix()
-
-
-
-
-	def draw_contours(self, contours, thickness = 1, color = RGBA(0.,0.,0.,0.5) ):
-		glPushMatrix()
-		glLoadMatrixf(self.get_anthropomorphic_matrix())
-		for contour in contours:
-			draw_polyline(contour, thickness, color = color )
-		glPopMatrix()
-
-	def draw_contour(self, contour, thickness = 1, color = RGBA(0.,0.,0.,0.5) ):
-		glPushMatrix()
-		glLoadMatrixf(self.get_anthropomorphic_matrix())
-		draw_polyline(contour,thickness, color)
-		glPopMatrix()
-
-
 	def draw_debug_info(self, result  ):
 		models = result['models']
 		eye = models[0]['sphere'];
@@ -257,93 +142,33 @@ class Visualizer(object):
 
 		self.glfont.pop_state()
 
+	def draw_circle(self, circle_center, circle_normal, circle_radius, color=RGBA(1.1,0.2,.8), num_segments = 20):
+		vertices = []
+		vertices.append( (0,0,0) )  # circle center
 
-	########## Setup functions I don't really understand ############
+		#create circle vertices in the xy plane
+		for i in np.linspace(0.0, 2.0*math.pi , num_segments ):
+		    x = math.sin(i)
+		    y = math.cos(i)
+		    z = 0
+		    vertices.append((x,y,z))
 
-	def basic_gl_setup(self):
-		glEnable(GL_POINT_SPRITE )
-		glEnable(GL_VERTEX_PROGRAM_POINT_SIZE) # overwrite pointsize
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-		glEnable(GL_BLEND)
-		glClearColor(.8,.8,.8,1.)
-		glEnable(GL_LINE_SMOOTH)
-		# glEnable(GL_POINT_SMOOTH)
-		glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
-		glEnable(GL_LINE_SMOOTH)
-		glEnable(GL_POLYGON_SMOOTH)
-		glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST)
+		glPushMatrix()
+		glMatrixMode(GL_MODELVIEW )
+		glLoadMatrixf(self.get_pupil_transformation_matrix(circle_normal,circle_center, circle_radius))
+		glutils.draw_polyline((vertices),color=color, line_type = GL_TRIANGLE_FAN) # circle
+		glutils.draw_polyline( [ (0,0,0), (0,0, 4) ] ,color=RGBA(0,0,0), line_type = GL_LINES) #normal
+		glPopMatrix()
 
-	def adjust_gl_view(self,w,h):
-		"""
-		adjust view onto our scene.
-		"""
-		glViewport(0, 0, w, h)
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		glOrtho(0, w, h, 0, -1, 1)
-		glMatrixMode(GL_MODELVIEW)
-		glLoadIdentity()
-
-	def clear_gl_screen(self):
-		glClearColor(.9,.9,0.9,1.)
-		glClear(GL_COLOR_BUFFER_BIT)
-
-	########### Open, update, close #####################
-
-	def open_window(self):
-		if not self._window:
-			self.input = {'button':None, 'mouse':(0,0)}
-
-			# get glfw started
-			if self.run_independently:
-				glfwInit()
-			window = glfwGetCurrentContext()
-			self._window = glfwCreateWindow(self.window_size[0], self.window_size[1], self.name, None, window)
-			glfwMakeContextCurrent(self._window)
-
-			if not self._window:
-				exit()
-
-			glfwSetWindowPos(self._window,0,0)
-			# Register callbacks window
-			glfwSetFramebufferSizeCallback(self._window,self.on_resize)
-			glfwSetWindowIconifyCallback(self._window,self.on_iconify)
-			glfwSetKeyCallback(self._window,self.on_key)
-			glfwSetCharCallback(self._window,self.on_char)
-			glfwSetMouseButtonCallback(self._window,self.on_button)
-			glfwSetCursorPosCallback(self._window,self.on_pos)
-			glfwSetScrollCallback(self._window,self.on_scroll)
-
-			# get glfw started
-			if self.run_independently:
-				init()
-			self.basic_gl_setup()
-
-			self.sphere = Sphere(20)
-
-
-			self.glfont = fs.Context()
-			self.glfont.add_font('opensans',get_opensans_font_path())
-			self.glfont.set_size(22)
-			self.glfont.set_color_float((0.2,0.5,0.9,1.0))
-			self.on_resize(self._window,*glfwGetFramebufferSize(self._window))
-			glfwMakeContextCurrent(window)
-
-			# self.gui = ui.UI()
-
-	def update_window(self, g_pool, result  ):
+	def update_window(self, g_pool, result ):
 
 		if not result:
 			return
 
-		if glfwWindowShouldClose(self._window):
-			self.close_window()
+		if not self.window:
 			return
 
-		if self._window != None:
-			glfwMakeContextCurrent(self._window)
-		else:
-			return
+		self.begin_update_window()
 
 		self.image_width , self.image_height = g_pool.capture.frame_size
 
@@ -360,7 +185,7 @@ class Visualizer(object):
 		g_pool.image_tex.draw( quad=((0,self.image_height),(self.image_width,self.image_height),(self.image_width,0),(0,0)) ,alpha=0.5)
 
 		glLoadMatrixf(self.get_adjusted_pixel_space_matrix(15))
-		self.draw_frustum()
+		self.draw_frustum( self.image_width, self.image_height, self.focal_length )
 
 		glLoadMatrixf(self.get_anthropomorphic_matrix())
 		model_count = 0;
@@ -378,7 +203,7 @@ class Visualizer(object):
 			if model_count == 0:
 				# self.draw_sphere(initial_sphere[0],initial_sphere[1], color = sphere_color )
 				self.draw_sphere(sphere[0],sphere[1],  color = initial_sphere_color )
-				draw_points(bin_positions, 3 , RGBA(0.6,0.0,0.6,0.5) )
+				glutils.draw_points(bin_positions, 3 , RGBA(0.6,0.0,0.6,0.5) )
 
 			else:
 				#self.draw_sphere(initial_sphere[0],initial_sphere[1], color = alternative_sphere_color )
@@ -390,7 +215,7 @@ class Visualizer(object):
 		self.draw_circle( latest_circle[0], latest_circle[1], latest_circle[2], RGBA(0.0,1.0,1.0,0.4))
 		# self.draw_circle( predicted_circle[0], predicted_circle[1], predicted_circle[2], RGBA(1.0,0.0,0.0,0.4))
 
-		draw_points(edges, 2 , RGBA(1.0,0.0,0.6,0.5) )
+		glutils.draw_points(edges, 2 , RGBA(1.0,0.0,0.6,0.5) )
 
 		glLoadMatrixf(self.get_anthropomorphic_matrix())
 		self.draw_coordinate_system(4)
@@ -399,26 +224,15 @@ class Visualizer(object):
 
 		self.draw_debug_info(result)
 
-		glfwSwapBuffers(self._window)
-		glfwPollEvents()
+		self.end_update_window()
+
 		return True
 
-	def close_window(self):
-		if self._window:
-			glfwDestroyWindow(self._window)
-			self._window = None
 
 	############ window callbacks #################
 	def on_resize(self,window,w, h):
-		h = max(h,1)
-		w = max(w,1)
+		Visualizer.on_resize(self,window,w, h)
 		self.trackball.set_window_size(w,h)
-
-		self.window_size = (w,h)
-		active_window = glfwGetCurrentContext()
-		glfwMakeContextCurrent(window)
-		self.adjust_gl_view(w,h)
-		glfwMakeContextCurrent(active_window)
 
 	def on_char(self,window,char):
 		if char == ord('r'):
@@ -426,29 +240,6 @@ class Visualizer(object):
 			self.trackball.pitch = 0
 			self.trackball.roll = 0
 
-	def on_button(self,window,button, action, mods):
-		# self.gui.update_button(button,action,mods)
-		if action == GLFW_PRESS:
-			self.input['button'] = button
-			self.input['mouse'] = glfwGetCursorPos(window)
-		if action == GLFW_RELEASE:
-			self.input['button'] = None
-
-	def on_pos(self,window,x, y):
-		hdpi_factor = float(glfwGetFramebufferSize(window)[0]/glfwGetWindowSize(window)[0])
-		x,y = x*hdpi_factor,y*hdpi_factor
-		# self.gui.update_mouse(x,y)
-		if self.input['button']==GLFW_MOUSE_BUTTON_RIGHT:
-			old_x,old_y = self.input['mouse']
-			self.trackball.drag_to(x-old_x,y-old_y)
-			self.input['mouse'] = x,y
-		if self.input['button']==GLFW_MOUSE_BUTTON_LEFT:
-			old_x,old_y = self.input['mouse']
-			self.trackball.pan_to(x-old_x,y-old_y)
-			self.input['mouse'] = x,y
 
 	def on_scroll(self,window,x,y):
 		self.trackball.zoom_to(y)
-
-	def on_iconify(self,window,iconified): pass
-	def on_key(self,window, key, scancode, action, mods): pass
