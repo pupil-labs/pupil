45273b0588e3a69990599343f8516125785a0287
Patrick Fürst <pat.fuerst@gmail.com>
2016-02-29 14:30:39 +0100

    handle context

--- pupil_src/shared_modules/calibration_routines/visualizer_calibration.py
+++ pupil_src/shared_modules/calibration_routines/visualizer_calibration.py
@@ -10,9 +10,8 @@ import math
 
 class Calibration_Visualizer(Visualizer):
 	def __init__(self, g_pool, world_camera_intrinsics , cal_ref_points_3d, eye_camera_to_world_matrix0 , cal_gaze_points0_3d, eye_camera_to_world_matrix1 = np.eye(4) , cal_gaze_points1_3d = [] ):
-		super(Calibration_Visualizer, self).__init__( "Calibration Visualizer",  False)
+		super(Calibration_Visualizer, self).__init__( g_pool, "Calibration Visualizer",  False)
 
-		self.g_pool = g_pool
 		self.image_width = 640 # right values are assigned in update
 		self.focal_length = 620
 		self.image_height = 480
@@ -44,102 +43,105 @@ class Calibration_Visualizer(Visualizer):
 
 	def update_window(self, g_pool , gaze_points0 , sphere0 , gaze_points1 = [] , sphere1 = None, intersection_points = []  ):
 
-			self.begin_update_window() #sets context
+		if not self.window:
+			return
 
-			self.clear_gl_screen()
-			self.trackball.push()
+		self.begin_update_window() #sets context
 
-			glMatrixMode( GL_MODELVIEW )
+		self.clear_gl_screen()
+		self.trackball.push()
 
-			# draw things in world camera coordinate system
+		glMatrixMode( GL_MODELVIEW )
+
+		# draw things in world camera coordinate system
+		glPushMatrix()
+		glLoadIdentity()
+
+		calibration_points_line_color = RGBA(0.5,0.5,0.5,0.1);
+		error_line_color = RGBA(1.0,0.0,0.0,0.5)
+
+		self.draw_coordinate_system(200)
+		if self.world_camera_width != 0:
+			self.draw_frustum( self.world_camera_width/ 10.0 , self.world_camera_height/ 10.0 , self.world_camera_focal / 10.0)
+
+		for p in self.cal_ref_points_3d:
+			glutils.draw_polyline( [ (0,0,0), p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
+		#calibration points
+		glutils.draw_points( self.cal_ref_points_3d , 4 , RGBA( 0, 1, 1, 1 ) )
+
+
+		glPopMatrix()
+
+		if sphere0:
+			# eye camera
 			glPushMatrix()
-			glLoadIdentity()
+			glLoadMatrixf( self.eye_camera_to_world_matrix0.T )
 
-			calibration_points_line_color = RGBA(0.5,0.5,0.5,0.1);
-			error_line_color = RGBA(1.0,0.0,0.0,0.5)
-
-			self.draw_coordinate_system(200)
-			if self.world_camera_width != 0:
-				self.draw_frustum( self.world_camera_width/ 10.0 , self.world_camera_height/ 10.0 , self.world_camera_focal / 10.0)
-
-			for p in self.cal_ref_points_3d:
-				glutils.draw_polyline( [ (0,0,0), p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
-			#calibration points
-			glutils.draw_points( self.cal_ref_points_3d , 4 , RGBA( 0, 1, 1, 1 ) )
-
-
+			self.draw_coordinate_system(60)
+			self.draw_frustum( self.image_width / 10.0, self.image_height / 10.0, self.focal_length /10.)
 			glPopMatrix()
 
-			if sphere0:
-				# eye camera
-				glPushMatrix()
-				glLoadMatrixf( self.eye_camera_to_world_matrix0.T )
+			#everything else is in world coordinates
 
-				self.draw_coordinate_system(60)
-				self.draw_frustum( self.image_width / 10.0, self.image_height / 10.0, self.focal_length /10.)
-				glPopMatrix()
+			#eye
+			sphere_center0 = list(sphere0['center'])
+			sphere_radius0 = sphere0['radius']
+			self.draw_sphere(sphere_center0,sphere_radius0,  color = RGBA(1,1,0,1))
 
-				#everything else is in world coordinates
+			#gazelines
+			for p in self.cal_gaze_points0_3d:
+				glutils.draw_polyline( [ sphere_center0, p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
 
-				#eye
-				sphere_center0 = list(sphere0['center'])
-				sphere_radius0 = sphere0['radius']
-				self.draw_sphere(sphere_center0,sphere_radius0,  color = RGBA(1,1,0,1))
+			#calibration points
+			glutils.draw_points( self.cal_gaze_points0_3d , 4 , RGBA( 1, 0, 1, 1 ) )
 
-				#gazelines
-				for p in self.cal_gaze_points0_3d:
-					glutils.draw_polyline( [ sphere_center0, p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
+			#current gaze points
+			glutils.draw_points( gaze_points0 , 2 , RGBA( 1, 0, 0, 1 ) )
+			for p in gaze_points0:
+				glutils.draw_polyline( [sphere_center0, p]  , 1 , RGBA(0,0,0,1), line_type = GL_LINES)
 
-				#calibration points
-				glutils.draw_points( self.cal_gaze_points0_3d , 4 , RGBA( 1, 0, 1, 1 ) )
+			#draw error lines form eye gaze points to  ref points
+			distance = 0.0
+			for(cal_gaze_point,ref_point) in zip(self.cal_gaze_points0_3d, self.cal_ref_points_3d):
+				glutils.draw_polyline( [ cal_gaze_point, ref_point]  , 1 , error_line_color, line_type = GL_LINES)
 
-				#current gaze points
-				glutils.draw_points( gaze_points0 , 2 , RGBA( 1, 0, 0, 1 ) )
-				for p in gaze_points0:
-					glutils.draw_polyline( [sphere_center0, p]  , 1 , RGBA(0,0,0,1), line_type = GL_LINES)
+		#second eye
+		if sphere1:
+			# eye camera
+			glPushMatrix()
+			glLoadMatrixf( self.eye_camera_to_world_matrix1.T )
 
-				#draw error lines form eye gaze points to  ref points
-				distance = 0.0
-				for(cal_gaze_point,ref_point) in zip(self.cal_gaze_points0_3d, self.cal_ref_points_3d):
-					glutils.draw_polyline( [ cal_gaze_point, ref_point]  , 1 , error_line_color, line_type = GL_LINES)
+			self.draw_coordinate_system(60)
+			self.draw_frustum( self.image_width / 10.0, self.image_height / 10.0, self.focal_length /10.)
+			glPopMatrix()
 
-			#second eye
-			if sphere1:
-				# eye camera
-				glPushMatrix()
-				glLoadMatrixf( self.eye_camera_to_world_matrix1.T )
+			#everything else is in world coordinates
 
-				self.draw_coordinate_system(60)
-				self.draw_frustum( self.image_width / 10.0, self.image_height / 10.0, self.focal_length /10.)
-				glPopMatrix()
+			#eye
+			sphere_center1 = list(sphere1['center'])
+			sphere_radius1 = sphere1['radius']
+			self.draw_sphere(sphere_center1,sphere_radius1,  color = RGBA(1,1,0,1))
 
-				#everything else is in world coordinates
+			#gazelines
+			for p in self.cal_gaze_points1_3d:
+				glutils.draw_polyline( [ sphere_center1, p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
 
-				#eye
-				sphere_center1 = list(sphere1['center'])
-				sphere_radius1 = sphere1['radius']
-				self.draw_sphere(sphere_center1,sphere_radius1,  color = RGBA(1,1,0,1))
+			#calibration points
+			glutils.draw_points( self.cal_gaze_points1_3d , 4 , RGBA( 1, 0, 1, 1 ) )
 
-				#gazelines
-				for p in self.cal_gaze_points1_3d:
-					glutils.draw_polyline( [ sphere_center1, p]  , 1 , calibration_points_line_color, line_type = GL_LINES)
+			#current gaze points
+			glutils.draw_points( gaze_points1 , 2 , RGBA( 1, 0, 0, 1 ) )
+			for p in gaze_points1:
+				glutils.draw_polyline( [sphere_center1, p]  , 1 , RGBA(0,0,0,1), line_type = GL_LINES)
 
-				#calibration points
-				glutils.draw_points( self.cal_gaze_points1_3d , 4 , RGBA( 1, 0, 1, 1 ) )
+			#draw error lines form eye gaze points to  ref points
+			distance = 0.0
+			for(cal_gaze_point,ref_point) in zip(self.cal_gaze_points1_3d, self.cal_ref_points_3d):
+				glutils.draw_polyline( [ cal_gaze_point, ref_point]  , 1 , error_line_color, line_type = GL_LINES)
 
-				#current gaze points
-				glutils.draw_points( gaze_points1 , 2 , RGBA( 1, 0, 0, 1 ) )
-				for p in gaze_points1:
-					glutils.draw_polyline( [sphere_center1, p]  , 1 , RGBA(0,0,0,1), line_type = GL_LINES)
+		self.trackball.pop()
 
-				#draw error lines form eye gaze points to  ref points
-				distance = 0.0
-				for(cal_gaze_point,ref_point) in zip(self.cal_gaze_points1_3d, self.cal_ref_points_3d):
-					glutils.draw_polyline( [ cal_gaze_point, ref_point]  , 1 , error_line_color, line_type = GL_LINES)
-
-			self.trackball.pop()
-
-			self.end_update_window() #swap buffers, handle context
+		self.end_update_window() #swap buffers, handle context
 
 
 
