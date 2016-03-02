827cd5cc7f682b2364e342a012545bcf3ab9bdb4
Patrick Fürst <pat.fuerst@gmail.com>
2016-03-02 11:24:59 +0100

    create our own math module. including intersection and transformation helper

---
+++ pupil_src/shared_modules/math_helper/transformations.py
@@ -0,0 +1,246 @@
+# From http://www.lfd.uci.edu/~gohlke/code/transformations.py.html
+# Copyright (c) 2006-2015, Christoph Gohlke
+# Copyright (c) 2006-2015, The Regents of the University of California
+# Produced at the Laboratory for Fluorescence Dynamics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of the copyright holders nor the names of any
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+import sys
+import numpy as np
+
+def quat2mat(quaternion):
+
+    """Return homogeneous rotation matrix from quaternion.
+    """
+    q = quaternion
+    n = np.dot(q, q)
+    if n < np.finfo(np.float64).eps:
+        return np.identity(4)
+    q *= np.sqrt(2.0 / n)
+    q = np.outer(q, q)
+    return np.array([
+        [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
+        [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
+        [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
+        [                0.0,                 0.0,                 0.0, 1.0]])
+
+
+
+def mat2quat(matrix , isprecise=False):
+    """Return quaternion from rotation matrix.
+
+    If isprecise is True, the input matrix is assumed to be a precise rotation
+    matrix and a faster algorithm is used.
+
+    >>> q = quaternion_from_matrix(numpy.identity(4), True)
+    >>> numpy.allclose(q, [1, 0, 0, 0])
+    True
+    >>> q = quaternion_from_matrix(numpy.diag([1, -1, -1, 1]))
+    >>> numpy.allclose(q, [0, 1, 0, 0]) or numpy.allclose(q, [0, -1, 0, 0])
+    True
+    >>> R = rotation_matrix(0.123, (1, 2, 3))
+    >>> q = quaternion_from_matrix(R, True)
+    >>> numpy.allclose(q, [0.9981095, 0.0164262, 0.0328524, 0.0492786])
+    True
+    >>> R = [[-0.545, 0.797, 0.260, 0], [0.733, 0.603, -0.313, 0],
+    ...      [-0.407, 0.021, -0.913, 0], [0, 0, 0, 1]]
+    >>> q = quaternion_from_matrix(R)
+    >>> numpy.allclose(q, [0.19069, 0.43736, 0.87485, -0.083611])
+    True
+    >>> R = [[0.395, 0.362, 0.843, 0], [-0.626, 0.796, -0.056, 0],
+    ...      [-0.677, -0.498, 0.529, 0], [0, 0, 0, 1]]
+    >>> q = quaternion_from_matrix(R)
+    >>> numpy.allclose(q, [0.82336615, -0.13610694, 0.46344705, -0.29792603])
+    True
+    >>> R = random_rotation_matrix()
+    >>> q = quaternion_from_matrix(R)
+    >>> is_same_transform(R, quaternion_matrix(q))
+    True
+    >>> R = euler_matrix(0.0, 0.0, numpy.pi/2.0)
+    >>> numpy.allclose(quaternion_from_matrix(R, isprecise=False),
+    ...                quaternion_from_matrix(R, isprecise=True))
+    True
+
+    """
+    M = numpy.array(matrix, dtype=numpy.float64, copy=False)[:4, :4]
+    if isprecise:
+        q = numpy.empty((4, ))
+        t = numpy.trace(M)
+        if t > M[3, 3]:
+            q[0] = t
+            q[3] = M[1, 0] - M[0, 1]
+            q[2] = M[0, 2] - M[2, 0]
+            q[1] = M[2, 1] - M[1, 2]
+        else:
+            i, j, k = 1, 2, 3
+            if M[1, 1] > M[0, 0]:
+                i, j, k = 2, 3, 1
+            if M[2, 2] > M[i, i]:
+                i, j, k = 3, 1, 2
+            t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+            q[i] = t
+            q[j] = M[i, j] + M[j, i]
+            q[k] = M[k, i] + M[i, k]
+            q[3] = M[k, j] - M[j, k]
+        q *= 0.5 / math.sqrt(t * M[3, 3])
+    else:
+        m00 = M[0, 0]
+        m01 = M[0, 1]
+        m02 = M[0, 2]
+        m10 = M[1, 0]
+        m11 = M[1, 1]
+        m12 = M[1, 2]
+        m20 = M[2, 0]
+        m21 = M[2, 1]
+        m22 = M[2, 2]
+        # symmetric matrix K
+        K = numpy.array([[m00-m11-m22, 0.0,         0.0,         0.0],
+                         [m01+m10,     m11-m00-m22, 0.0,         0.0],
+                         [m02+m20,     m12+m21,     m22-m00-m11, 0.0],
+                         [m21-m12,     m02-m20,     m10-m01,     m00+m11+m22]])
+        K /= 3.0
+        # quaternion is eigenvector of K that corresponds to largest eigenvalue
+        w, V = numpy.linalg.eigh(K)
+        q = V[[3, 0, 1, 2], numpy.argmax(w)]
+    if q[0] < 0.0:
+        numpy.negative(q, q)
+    return q
+
+
+
+#  From https://github.com/nipy/nibabel/
+#  The MIT License
+
+#  Copyright (c) 2009-2014 Matthew Brett <matthew.brett@gmail.com>
+#  Copyright (c) 2010-2013 Stephan Gerhard <git@unidesign.ch>
+#  Copyright (c) 2006-2014 Michael Hanke <michael.hanke@gmail.com>
+#  Copyright (c) 2011 Christian Haselgrove <christian.haselgrove@umassmed.edu>
+#  Copyright (c) 2010-2011 Jarrod Millman <jarrod.millman@gmail.com>
+#  Copyright (c) 2011-2014 Yaroslav Halchenko <debian@onerussian.com>
+
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+
+
+def quat2angle_axis(quat, identity_thresh=None):
+    ''' Convert quaternion to rotation of angle around axis
+    Parameters
+    ----------
+    quat : 4 element sequence
+       w, x, y, z forming quaternion
+    identity_thresh : None or scalar, optional
+       threshold below which the norm of the vector part of the
+       quaternion (x, y, z) is deemed to be 0, leading to the identity
+       rotation.  None (the default) leads to a threshold estimated
+       based on the precision of the input.
+    Returns
+    -------
+    theta : scalar
+       angle of rotation
+    vector : array shape (3,)
+       axis around which rotation occurs
+    Examples
+    --------
+    >>> theta, vec = quat2angle_axis([0, 1, 0, 0])
+    >>> np.allclose(theta, np.pi)
+    True
+    >>> vec
+    array([ 1.,  0.,  0.])
+    If this is an identity rotation, we return a zero angle and an
+    arbitrary vector
+    >>> quat2angle_axis([1, 0, 0, 0])
+    (0.0, array([ 1.,  0.,  0.]))
+    Notes
+    -----
+    A quaternion for which x, y, z are all equal to 0, is an identity
+    rotation.  In this case we return a 0 angle and an  arbitrary
+    vector, here [1, 0, 0]
+    '''
+    w, x, y, z = quat
+    vec = np.asarray([x, y, z])
+    if identity_thresh is None:
+        try:
+            identity_thresh = np.finfo(vec.dtype).eps * 3
+        except ValueError:  # integer type
+            identity_thresh = np.finfo(np.float64).eps * 3
+    n = np.sqrt(x*x + y*y + z*z)
+    if n < identity_thresh:
+        # if vec is nearly 0,0,0, this is an identity rotation
+        return 0.0, np.array([1.0, 0, 0])
+    return 2 * np.acos(w), vec / n
+
+def angle_axis2quat(theta, vector, is_normalized=False):
+    ''' Quaternion for rotation of angle `theta` around `vector`
+    Parameters
+    ----------
+    theta : scalar
+       angle of rotation
+    vector : 3 element sequence
+       vector specifying axis for rotation.
+    is_normalized : bool, optional
+       True if vector is already normalized (has norm of 1).  Default
+       False
+    Returns
+    -------
+    quat : 4 element sequence of symbols
+       quaternion giving specified rotation
+    Examples
+    --------
+    >>> q = angle_axis2quat(np.pi, [1, 0, 0])
+    >>> np.allclose(q, [0, 1, 0,  0])
+    True
+    Notes
+    -----
+    Formula from http://mathworld.wolfram.com/EulerParameters.html
+    '''
+    vector = np.array(vector)
+    if not is_normalized:
+        # Cannot divide in-place because input vector may be integer type,
+        # whereas output will be float type; this may raise an error in
+        # versions of numpy > 1.6.1
+        vector = vector / np.sqrt(np.dot(vector, vector))
+    t2 = theta / 2.0
+    st2 = np.sin(t2)
+    return np.concatenate(([np.cos(t2)],
+                           vector * st2))
+
