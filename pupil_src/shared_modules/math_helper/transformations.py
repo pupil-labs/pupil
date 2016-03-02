db54179d3f8a71ec1d1bcfd3f4040077bac7d7df
Patrick Fürst <pat.fuerst@gmail.com>
2016-03-02 11:57:47 +0100

    add general mat2quat

--- pupil_src/shared_modules/math_helper/transformations.py
+++ pupil_src/shared_modules/math_helper/transformations.py
@@ -48,87 +48,47 @@ def quat2mat(quaternion):
         [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2]]])
 
 
+def unit_vector(data, axis=None, out=None):
+    """Return ndarray normalized by length, i.e. Euclidean norm, along axis.
 
-def mat2quat(matrix , isprecise=False):
-    """Return quaternion from rotation matrix.
-
-    If isprecise is True, the input matrix is assumed to be a precise rotation
-    matrix and a faster algorithm is used.
-
-    >>> q = quaternion_from_matrix(numpy.identity(4), True)
-    >>> numpy.allclose(q, [1, 0, 0, 0])
+    >>> v0 = np.random.random(3)
+    >>> v1 = unit_vector(v0)
+    >>> np.allclose(v1, v0 / np.linalg.norm(v0))
     True
-    >>> q = quaternion_from_matrix(numpy.diag([1, -1, -1, 1]))
-    >>> numpy.allclose(q, [0, 1, 0, 0]) or numpy.allclose(q, [0, -1, 0, 0])
+    >>> v0 = np.random.rand(5, 4, 3)
+    >>> v1 = unit_vector(v0, axis=-1)
+    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=2)), 2)
+    >>> np.allclose(v1, v2)
     True
-    >>> R = rotation_matrix(0.123, (1, 2, 3))
-    >>> q = quaternion_from_matrix(R, True)
-    >>> numpy.allclose(q, [0.9981095, 0.0164262, 0.0328524, 0.0492786])
+    >>> v1 = unit_vector(v0, axis=1)
+    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=1)), 1)
+    >>> np.allclose(v1, v2)
     True
-    >>> R = [[-0.545, 0.797, 0.260, 0], [0.733, 0.603, -0.313, 0],
-    ...      [-0.407, 0.021, -0.913, 0], [0, 0, 0, 1]]
-    >>> q = quaternion_from_matrix(R)
-    >>> numpy.allclose(q, [0.19069, 0.43736, 0.87485, -0.083611])
+    >>> v1 = np.empty((5, 4, 3))
+    >>> unit_vector(v0, axis=1, out=v1)
+    >>> np.allclose(v1, v2)
     True
-    >>> R = [[0.395, 0.362, 0.843, 0], [-0.626, 0.796, -0.056, 0],
-    ...      [-0.677, -0.498, 0.529, 0], [0, 0, 0, 1]]
-    >>> q = quaternion_from_matrix(R)
-    >>> numpy.allclose(q, [0.82336615, -0.13610694, 0.46344705, -0.29792603])
-    True
-    >>> R = random_rotation_matrix()
-    >>> q = quaternion_from_matrix(R)
-    >>> is_same_transform(R, quaternion_matrix(q))
-    True
-    >>> R = euler_matrix(0.0, 0.0, numpy.pi/2.0)
-    >>> numpy.allclose(quaternion_from_matrix(R, isprecise=False),
-    ...                quaternion_from_matrix(R, isprecise=True))
-    True
+    >>> list(unit_vector([]))
+    []
+    >>> list(unit_vector([1]))
+    [1.0]
 
     """
-    M = numpy.array(matrix, dtype=numpy.float64, copy=False)[:4, :4]
-    if isprecise:
-        q = numpy.empty((4, ))
-        t = numpy.trace(M)
-        if t > M[3, 3]:
-            q[0] = t
-            q[3] = M[1, 0] - M[0, 1]
-            q[2] = M[0, 2] - M[2, 0]
-            q[1] = M[2, 1] - M[1, 2]
-        else:
-            i, j, k = 1, 2, 3
-            if M[1, 1] > M[0, 0]:
-                i, j, k = 2, 3, 1
-            if M[2, 2] > M[i, i]:
-                i, j, k = 3, 1, 2
-            t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
-            q[i] = t
-            q[j] = M[i, j] + M[j, i]
-            q[k] = M[k, i] + M[i, k]
-            q[3] = M[k, j] - M[j, k]
-        q *= 0.5 / math.sqrt(t * M[3, 3])
+    if out is None:
+        if data.ndim == 1:
+            data /= np.sqrt(np.dot(data, data))
+            return data
     else:
-        m00 = M[0, 0]
-        m01 = M[0, 1]
-        m02 = M[0, 2]
-        m10 = M[1, 0]
-        m11 = M[1, 1]
-        m12 = M[1, 2]
-        m20 = M[2, 0]
-        m21 = M[2, 1]
-        m22 = M[2, 2]
-        # symmetric matrix K
-        K = numpy.array([[m00-m11-m22, 0.0,         0.0,         0.0],
-                         [m01+m10,     m11-m00-m22, 0.0,         0.0],
-                         [m02+m20,     m12+m21,     m22-m00-m11, 0.0],
-                         [m21-m12,     m02-m20,     m10-m01,     m00+m11+m22]])
-        K /= 3.0
-        # quaternion is eigenvector of K that corresponds to largest eigenvalue
-        w, V = numpy.linalg.eigh(K)
-        q = V[[3, 0, 1, 2], numpy.argmax(w)]
-    if q[0] < 0.0:
-        numpy.negative(q, q)
-    return q
-
+        if out is not data:
+            out[:] = np.array(data, copy=False)
+        data = out
+    length = np.atleast_1d(np.sum(data*data, axis))
+    np.sqrt(length, length)
+    if axis is not None:
+        length = np.expand_dims(length, axis)
+    data /= length
+    if out is None:
+        return data
 
 
 #  From https://github.com/nipy/nibabel/
@@ -159,6 +119,62 @@ def mat2quat(matrix , isprecise=False):
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
+def mat2quat(M):
+    ''' Calculate quaternion corresponding to given rotation matrix
+    Parameters
+    ----------
+    M : array-like
+      3x3 rotation matrix
+    Returns
+    -------
+    q : (4,) array
+      closest quaternion to input matrix, having positive q[0]
+    Notes
+    -----
+    Method claimed to be robust to numerical errors in M
+    Constructs quaternion by calculating maximum eigenvector for matrix
+    K (constructed from input `M`).  Although this is not tested, a
+    maximum eigenvalue of 1 corresponds to a valid rotation.
+    A quaternion q*-1 corresponds to the same rotation as q; thus the
+    sign of the reconstructed quaternion is arbitrary, and we return
+    quaternions with positive w (q[0]).
+    References
+    ----------
+    * https://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
+    * Bar-Itzhack, Itzhack Y. (2000), "New method for extracting the
+      quaternion from a rotation matrix", AIAA Journal of Guidance,
+      Control and Dynamics 23(6):1085-1087 (Engineering Note), ISSN
+      0731-5090
+    Examples
+    --------
+    >>> import numpy as np
+    >>> q = mat2quat(np.eye(3)) # Identity rotation
+    >>> np.allclose(q, [1, 0, 0, 0])
+    True
+    >>> q = mat2quat(np.diag([1, -1, -1]))
+    >>> np.allclose(q, [0, 1, 0, 0]) # 180 degree rotn around axis 0
+    True
+    '''
+    # Qyx refers to the contribution of the y input vector component to
+    # the x output vector component.  Qyx is therefore the same as
+    # M[0,1].  The notation is from the Wikipedia article.
+    Qxx, Qyx, Qzx, Qxy, Qyy, Qzy, Qxz, Qyz, Qzz = M.flat
+    # Fill only lower half of symmetric matrix
+    K = np.array([
+        [Qxx - Qyy - Qzz, 0, 0, 0],
+        [Qyx + Qxy, Qyy - Qxx - Qzz, 0, 0],
+        [Qzx + Qxz, Qzy + Qyz, Qzz - Qxx - Qyy, 0],
+        [Qyz - Qzy, Qzx - Qxz, Qxy - Qyx, Qxx + Qyy + Qzz]]
+    ) / 3.0
+    # Use Hermitian eigenvectors, values for speed
+    vals, vecs = np.linalg.eigh(K)
+    # Select largest eigenvector, reorder to w,x,y,z quaternion
+    q = vecs[[3, 0, 1, 2], np.argmax(vals)]
+    # Prefer quaternion with positive w
+    # (q * -1 corresponds to same rotation as q)
+    if q[0] < 0:
+        q *= -1
+    return q
 
 
 def quat2angle_axis(quat, identity_thresh=None):
