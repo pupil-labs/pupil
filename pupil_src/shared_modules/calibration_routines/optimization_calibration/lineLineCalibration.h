f36f651a067c70b7eb21fef7ace0f8569c51c60f
Patrick Fürst <pat.fuerst@gmail.com>
2016-03-03 16:05:32 +0100

    add weight to optimization

--- pupil_src/shared_modules/calibration_routines/optimization_calibration/lineLineCalibration.h
+++ pupil_src/shared_modules/calibration_routines/optimization_calibration/lineLineCalibration.h
@@ -44,15 +44,44 @@ struct CoplanarityError {
 
         Eigen::Matrix<T, 3, 1> gazeD = {T(gazeDirection[0]), T(gazeDirection[1]), T(gazeDirection[2])};
         Eigen::Matrix<T, 3, 1> refD = {T(refDirection[0]) , T(refDirection[1]) , T(refDirection[2])};
-        Eigen::Matrix<T, 3, 1> t = {T(translation[0]) , T(translation[1]) , T(translation[2])};
+        Eigen::Matrix<T, 3, 1> b = {T(translation[0]) , T(translation[1]) , T(translation[2])};
 
         Eigen::Matrix<T, 3, 1> gazeWorld;
+        //T inv[4] = {rotation[0], -rotation[1],-rotation[2],-rotation[3]};
         ceres::QuaternionRotatePoint( rotation , gazeD.data(), gazeWorld.data() );
         //TODO add weighting factors to the residual , better approximation
         //coplanarity constraint  x1.T * E * x2 = 0
-        auto res = refD.transpose() * ( t.cross(gazeWorld));
+        auto res = refD.transpose() * ( b.cross(gazeWorld));
 
-        residuals[0] = res[0]* res[0];
+
+
+        if (useWeight)
+        {
+                       // weighting factor:
+            const T generalVariance = T(0.1);
+            const Eigen::Matrix<T, 3, 1> distanceVector = gazeWorld.cross(refD);
+            const T distance = distanceVector.norm();
+            const T distanceSquared = distance*distance;
+
+            const T worldCameraVariance = T(20.0);
+            const T eyeVariance = T(20.0);
+
+            const T nom  = distanceSquared * generalVariance;
+
+            const T c = b.cross(gazeD).dot(distanceVector);
+            const T d = b.cross(gazeWorld).dot(distanceVector);
+
+            const T denom = c * c * worldCameraVariance + d * d * eyeVariance;
+
+            const T weight = nom / denom;
+            //std::cout << "weight" << weight << std::endl;
+
+            residuals[0] = weight * res[0]* res[0];
+        }else{
+             residuals[0] =  res[0]* res[0];
+
+        }
+
         return true;
 
 
@@ -60,12 +89,13 @@ struct CoplanarityError {
 
     const Vector3 gazeDirection;
     const Vector3 refDirection;
+    const bool useWeight;
 };
 
 
 
 bool lineLineCalibration(const std::vector<Vector3>& refDirections, const std::vector<Vector3>& gazeDirections ,
-    double (&orientation)[4], double (&translation)[3] , double& avgDistance, bool fixTranslation = false )
+    double (&orientation)[4], double (&translation)[3] , double& avgDistance, bool fixTranslation = false, bool useWeight = true )
 {
 
     // don't use Constructor 'Quaternion (const Scalar *data)' because the internal layout for coefficients is different from the one we use.
@@ -98,7 +128,7 @@ bool lineLineCalibration(const std::vector<Vector3>& refDirections, const std::v
 
         if( valid ){
 
-            CostFunction* cost = new AutoDiffCostFunction<CoplanarityError , 1, 4, 3 >(new CoplanarityError(ref, gaze ));
+            CostFunction* cost = new AutoDiffCostFunction<CoplanarityError , 1, 4, 3 >(new CoplanarityError(ref, gaze, useWeight ));
             // TODO use a loss function, to handle gaze point outliers
             problem.AddResidualBlock(cost, nullptr, orientation,  translation );
         }else{
@@ -125,7 +155,7 @@ bool lineLineCalibration(const std::vector<Vector3>& refDirections, const std::v
     // Build and solve the problem.
     Solver::Options options;
     options.max_num_iterations = 1000;
-    //options.linear_solver_type = ceres::DENSE_QR;
+    options.linear_solver_type = ceres::DENSE_QR;
 
     options.parameter_tolerance = 1e-15;
     options.function_tolerance = 1e-16;
@@ -139,7 +169,7 @@ bool lineLineCalibration(const std::vector<Vector3>& refDirections, const std::v
     Solve(options, &problem, &summary);
 
     // std::cout << summary.BriefReport() << "\n";
-    //std::cout << summary.FullReport() << "\n";
+    std::cout << summary.FullReport() << "\n";
 
     if( summary.termination_type != ceres::TerminationType::CONVERGENCE  ){
         std::cout << "Termination Error: " << ceres::TerminationTypeToString(summary.termination_type) << std::endl;
@@ -151,76 +181,100 @@ bool lineLineCalibration(const std::vector<Vector3>& refDirections, const std::v
     translation[1] *= n;
     translation[2] *= n;
 
-    // using singleeyefitter::Line3;
-    // // check for possible ambiguity
-    // //intersection points need to lie in positive z
+    using singleeyefitter::Line3;
+    // check for possible ambiguity
+    //intersection points need to lie in positive z
 
-    // auto checkResult = [ &gazeDirections, &refDirections ]( Eigen::Quaterniond& orientation , Vector3 translation, double& avgDistance ){
+    auto checkResult = [ &gazeDirections, &refDirections ]( Eigen::Quaterniond& orientation , Vector3 translation, double& avgDistance ){
 
-    //     int validCount = 0;
-    //     avgDistance = 0.0;
-    //     for(int i=0; i<refDirections.size(); i++) {
+        int validCount = 0;
+        avgDistance = 0.0;
+        for(int i=0; i<refDirections.size(); i++) {
 
-    //         auto gaze = gazeDirections.at(i);
-    //         auto ref = refDirections.at(i);
+            auto gaze = gazeDirections.at(i);
+            auto ref = refDirections.at(i);
 
-    //         gaze.normalize(); //just to be sure
-    //         ref.normalize(); //just to be sure
+            gaze.normalize(); //just to be sure
+            ref.normalize(); //just to be sure
 
-    //         Vector3 gazeWorld = orientation * gaze;
+            Vector3 gazeWorld = orientation * gaze;
 
-    //         Line3 refLine = { Vector3(0,0,0) , ref  };
-    //         Line3 gazeLine = { translation , gazeWorld  };
+            Line3 refLine = { Vector3(0,0,0) , ref  };
+            Line3 gazeLine = { translation , gazeWorld  };
 
-    //         auto closestPoints = closest_points_on_line( refLine , gazeLine );
+            auto closestPoints = closest_points_on_line( refLine , gazeLine );
 
-    //         if( closestPoints.first.z() > 0.0 && closestPoints.second.z() > 0.0 )
-    //             validCount++;
+            if( closestPoints.first.z() > 0.0 && closestPoints.second.z() > 0.0 )
+                validCount++;
 
-    //         avgDistance += euclidean_distance( closestPoints.first, closestPoints.second );
-    //     }
-    //     avgDistance /= refDirections.size();
+            avgDistance += euclidean_distance( closestPoints.first, closestPoints.second );
+        }
+        avgDistance /= refDirections.size();
 
-    //     return validCount > refDirections.size() * 0.7;
-    // };
+        return validCount;
+    };
 
 
-    // Eigen::Quaterniond q1(orientation[0],orientation[1],orientation[2],orientation[3]); // don't mapp orientation
-    // Vector3 t1 =  Vector3(translation[0],translation[1], translation[2]);
-    // Eigen::Quaterniond q2  = q1.conjugate();
-    // Vector3 t2 =  -t1;
+    Eigen::Quaterniond q1(orientation[0],orientation[1],orientation[2],orientation[3]); // don't mapp orientation
+    Vector3 t1 =  Vector3(translation[0],translation[1], translation[2]);
+    Eigen::Quaterniond q2  = q1.conjugate();
+    Vector3 t2 =  -t1;
 
-    // if(checkResult(q1,t1,avgDistance)){
-    //     std::cout << "result one" <<std::endl;
-    //     return true;
-    // }
-    // if(checkResult(q1,t2,avgDistance)){
-    //     std::cout << "result two" <<std::endl;
-    //     translation[0] *= -1.0;
-    //     translation[1] *= -1.0;
-    //     translation[2] *= -1.0;
-    //     return true;
-    // }
-    // if(checkResult(q2,t1,avgDistance)){
-    //     std::cout << "result three" <<std::endl;
+    int s1 = checkResult(q1,t1,avgDistance);
+    int s2 = checkResult(q1,t2,avgDistance);
+    int s3 = checkResult(q2,t1,avgDistance);
+    int s4 = checkResult(q2,t2,avgDistance);
 
-    //     orientation[1] *= -1.0;
-    //     orientation[2] *= -1.0;
-    //     orientation[3] *= -1.0;
-    //     return true;
-    // }
-    // if(checkResult(q2,t2,avgDistance)){
-    //     std::cout << "result four" <<std::endl;
+    std::cout << "s1: " << s1 << std::endl;
+    std::cout << "s2: " << s2 << std::endl;
+    std::cout << "s3: " << s3 << std::endl;
+    std::cout << "s4: " << s4 << std::endl;
 
-    //     orientation[1] *= -1.0;
-    //     orientation[2] *= -1.0;
-    //     orientation[3] *= -1.0;
+    std::vector<int> v = {s1,s2,s3,s4};
+    int maxIndex = -1;
+    int maxValue = 0;
+    int i = 0;
+    for( auto s : v ){
 
-    //     translation[0] *= -1.0;
-    //     translation[1] *= -1.0;
-    //     translation[2] *= -1.0;
-    //     return true;
-    // }
+        if(s > maxValue){
+            maxValue = s;
+            maxIndex = i;
+        }
+        i++;
+    }
+
+
+    switch(maxIndex){
+
+        case 0:
+            std::cout << "result one" <<std::endl;
+            break;
+        case 1:
+
+            std::cout << "result two" <<std::endl;
+            translation[0] *= -1.0;
+            translation[1] *= -1.0;
+            translation[2] *= -1.0;
+            break;
+
+        case 2:
+            std::cout << "result three" <<std::endl;
+
+            orientation[1] *= -1.0;
+            orientation[2] *= -1.0;
+            orientation[3] *= -1.0;
+            break;
+        case 3:
+            std::cout << "result four" <<std::endl;
+
+            orientation[1] *= -1.0;
+            orientation[2] *= -1.0;
+            orientation[3] *= -1.0;
+
+            translation[0] *= -1.0;
+            translation[1] *= -1.0;
+            translation[2] *= -1.0;
+    }
 
     return true;
 
