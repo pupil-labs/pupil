7c626d87b97f1cefce77765af5dce4f6fcce6b9a
Moritz Kassner <moritzkassner@gmail.com>
2016-06-28 16:26:59 +0200

    turning off parallel compilation as suggested in #405 .

--- pupil_src/shared_modules/calibration_routines/optimization_calibration/setup.py
+++ pupil_src/shared_modules/calibration_routines/optimization_calibration/setup.py
@@ -8,23 +8,23 @@
 ----------------------------------------------------------------------------------~(*)
 '''
 
-# monkey-patch for parallel compilation
-def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
-    # those lines are copied from distutils.ccompiler.CCompiler directly
-    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
-    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
-    # parallel code
-    N=4 # number of parallel compilations
-    import multiprocessing.pool
-    def _single_compile(obj):
-        try: src, ext = build[obj]
-        except KeyError: return
-        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
-    # convert to list, imap is evaluated on-demand
-    list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
-    return objects
-import distutils.ccompiler
-distutils.ccompiler.CCompiler.compile=parallelCCompile
+# # monkey-patch for parallel compilation
+# def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+#     # those lines are copied from distutils.ccompiler.CCompiler directly
+#     macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+#     cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+#     # parallel code
+#     N=4 # number of parallel compilations
+#     import multiprocessing.pool
+#     def _single_compile(obj):
+#         try: src, ext = build[obj]
+#         except KeyError: return
+#         self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+#     # convert to list, imap is evaluated on-demand
+#     list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
+#     return objects
+# import distutils.ccompiler
+# distutils.ccompiler.CCompiler.compile=parallelCCompile
 
 from distutils.core import setup
 from distutils.extension import Extension
