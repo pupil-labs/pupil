3eb2b8a887040ba6cd537d97238f0c4e5d1b19ac
PolynomialDivision <vincent@systemli.org>
2016-07-18 14:08:22 +0200

    Fix notify_all
    There is no self.notify_all in finish_calibration.
    So use g_pool.active_calibration_plugin.notify_all instead.

--- pupil_src/shared_modules/calibration_routines/finish_calibration.py
+++ pupil_src/shared_modules/calibration_routines/finish_calibration.py
@@ -296,17 +296,17 @@ def finish_calibration(g_pool,pupil_list,ref_list):
 
             map_fn,inliers,params = calibrate.calibrate_2d_polynomial(cal_pt_cloud_binocular,g_pool.capture.frame_size,binocular=True)
             if not inliers.any():
-                self.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
                 return
 
             map_fn,inliers,params_eye0 = calibrate.calibrate_2d_polynomial(cal_pt_cloud0,g_pool.capture.frame_size,binocular=False)
             if not inliers.any():
-                self.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
                 return
 
             map_fn,inliers,params_eye1 = calibrate.calibrate_2d_polynomial(cal_pt_cloud1,g_pool.capture.frame_size,binocular=False)
             if not inliers.any():
-                self.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
                 return
 
             g_pool.plugins.add(Binocular_Gaze_Mapper,args={'params':params, 'params_eye0':params_eye0, 'params_eye1':params_eye1})
@@ -317,7 +317,7 @@ def finish_calibration(g_pool,pupil_list,ref_list):
             cal_pt_cloud = calibrate.preprocess_2d_data_monocular(matched_monocular_data)
             map_fn,inliers,params = calibrate.calibrate_2d_polynomial(cal_pt_cloud,g_pool.capture.frame_size,binocular=False)
             if not inliers.any():
-                self.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
+                g_pool.active_calibration_plugin.notify_all({'subject':'calibration.failed','reason':solver_failed_to_converge_error_msg,'timestamp':g_pool.capture.get_timestamp(),'record':True,'network_propagate':True})
                 return
 
             g_pool.plugins.add(Monocular_Gaze_Mapper,args={'params':params})
