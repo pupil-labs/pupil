88714b377decbd0e42d75d609cb4869a4f724f28
Moritz Kassner <moritzkassner@gmail.com>
2017-01-02 15:06:26 +0100

    updating licence headers. Happy 2017!

--- pupil_src/shared_modules/calibration_routines/optimization_calibration/calibration_methods.pyx
+++ pupil_src/shared_modules/calibration_routines/optimization_calibration/calibration_methods.pyx
@@ -1,11 +1,12 @@
 '''
-(*)~----------------------------------------------------------------------------------
- Pupil - eye tracking platform
- Copyright (C) 2012-2016  Pupil Labs
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
 
- Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
- License details are in the file license.txt, distributed as part of this software.
-----------------------------------------------------------------------------------~(*)
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
 '''
 
 from libcpp.vector cimport vector
--- pupil_src/shared_modules/zmq_tools.py
+++ pupil_src/shared_modules/zmq_tools.py
@@ -1,4 +1,14 @@
 '''
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+'''
+'''
 (*)~----------------------------------------------------------------------------------
  Pupil - eye tracking platform
  Copyright (C) 2012-2016  Pupil Labs
