1dafde1485dd85e8fe4f134dae84f9f6b1168698
Moritz Kassner <moritzkassner@gmail.com>
2017-01-02 14:28:44 +0100

    refactor licence header a bit. Added missing files.

--- update_license_header.py
+++ update_license_header.py
@@ -1,11 +1,12 @@
 '''
-(*)~----------------------------------------------------------------------------------
- Pupil - eye tracking platform
- Copyright (C) 2012-2016  Pupil Labs
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
 
- Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
- License details are in the file license.txt, distributed as part of this software.
-----------------------------------------------------------------------------------~(*)
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
 '''
 import fnmatch
 import os
@@ -22,9 +23,6 @@ See COPYING and COPYING.LESSER for license details.
 ---------------------------------------------------------------------------~(*)\
 """
 
-# find out the cwd and change to the top level Pupil folder
-cwd = os.getcwd()
-pupil_dir = os.path.join(cwd,'pupil_src')
 
 pattern = re.compile('(\'{3}|[/][*])\n\([*]\)~(.+?)~\([*]\)\n(\'{3}|[*][/])', re.DOTALL|re.MULTILINE)
 
@@ -32,56 +30,57 @@ pattern = re.compile('(\'{3}|[/][*])\n\([*]\)~(.+?)~\([*]\)\n(\'{3}|[*][/])', re
 # choose directories to exclude from search
 includes = ['*.py', '*.c']
 excludes = ['.git', '*.md', 'src_video', 'recordings', 'License', 'shader.py', 'vertex_buffer.py',
- 			 'gprof2dot.py','git_version.py','transformations.py','libuvcc*', '*.pstats', '*.png',
- 			 '*.svg', '*.ico', '*.sh', '*.icns', '*.spec', '.gitignore','glfw.py', 'version_utils.py']
+             'gprof2dot.py','git_version.py','transformations.py','libuvcc*', '*.pstats', '*.png',
+             '*.svg', '*.ico', '*.sh', '*.icns', '*.spec', '.gitignore','glfw.py', 'version_utils.py']
 
 # transform glob patterns to regular expressions
 includes = r'|'.join([fnmatch.translate(x) for x in includes])
 excludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
 
 def get_files(start_dir, includes, excludes):
-	# use os.walk to recursively dig down into the Pupil directory
-	match_files = []
-	for root, dirs, files in os.walk(start_dir):
-		if not re.search(excludes, root):
-			files = [f for f in files if re.search(includes, f) and not re.search(excludes, f)]
-			files = [os.path.join(root, f) for f in files]
-			match_files += files
-	return match_files
+    # use os.walk to recursively dig down into the Pupil directory
+    match_files = []
+    for root, dirs, files in os.walk(start_dir):
+        if not re.search(excludes, root):
+            files = [f for f in files if re.search(includes, f) and not re.search(excludes, f)]
+            files = [os.path.join(root, f) for f in files]
+            match_files += files
+    return match_files
 
 def write_header(file_name, license_txt):
-	# find and replace license header
-	# or add new header if not existing
-	c_comment = ['/*\n', '\n*/\n']
-	py_comment = ["'''\n","\n'''\n"]
-	file_type = os.path.splitext(file_name)[-1]
+    # find and replace license header
+    # or add new header if not existing
+    c_comment = ['/*\n', '\n*/\n']
+    py_comment = ["'''\n","\n'''\n"]
+    file_type = os.path.splitext(file_name)[-1]
 
-	if file_type == '.py':
-		license_txt = py_comment[0] + license_txt + py_comment[1]
-	if file_type == '.c':
-		license_txt = c_comment[0] + license_txt + c_comment[1]
+    if file_type == '.py':
+        license_txt = py_comment[0] + license_txt + py_comment[1]
+    if file_type == '.c':
+        license_txt = c_comment[0] + license_txt + c_comment[1]
 
-	with file(file_name, 'r') as original:
-		data = original.read()
+    with file(file_name, 'r') as original:
+        data = original.read()
 
-	with file(file_name, 'w') as modified:
-		if re.findall(pattern, data):
-			# if header already exists, then update, but dont add the last newline.
-			modified.write(re.sub(pattern, license_txt[:-1], data))
-			modified.close()
-		else:
-			# else write the license header
-			modified.write(license_txt + data)
-			modified.close()
-
-def update_header():
-	# Add a license/docstring header to selected files
-	match_files = get_files(pupil_dir, includes, excludes)
-	print match_files
-
-	for f in match_files:
-		write_header(f, license_txt)
+    with file(file_name, 'w') as modified:
+        if re.findall(pattern, data):
+            # if header already exists, then update, but dont add the last newline.
+            modified.write(re.sub(pattern, license_txt[:-1], data))
+            modified.close()
+        else:
+            # else write the license header
+            modified.write(license_txt + data)
+            modified.close()
 
 if __name__ == '__main__':
-	# run update_header() to add headers to find files
-	update_header()
+
+    # find out the cwd and change to the top level Pupil folder
+    cwd = os.getcwd()
+    pupil_dir = cwd
+
+    # Add a license/docstring header to selected files
+    match_files = get_files(pupil_dir, includes, excludes)
+    print len(match_files)
+
+    for f in match_files:
+        write_header(f, license_txt)
