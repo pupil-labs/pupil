f44d0bff5d7eccbeaff8e6f7a8a20cd6ba7c77f6
Moritz Kassner <moritzkassner@gmail.com>
2017-01-02 15:09:42 +0100

    removed broken headers.

--- pupil_src/shared_modules/zmq_tools.py
+++ pupil_src/shared_modules/zmq_tools.py
@@ -8,15 +8,9 @@ Lesser General Public License (LGPL v3.0).
 See COPYING and COPYING.LESSER for license details.
 ---------------------------------------------------------------------------~(*)
 '''
+
+
 '''
-(*)~----------------------------------------------------------------------------------
- Pupil - eye tracking platform
- Copyright (C) 2012-2016  Pupil Labs
-
- Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
- License details are in the file license.txt, distributed as part of this software.
-----------------------------------------------------------------------------------~(*)
-
 This file contains convenience classes for communication with
 the Pupil IPC Backbone.
 '''
