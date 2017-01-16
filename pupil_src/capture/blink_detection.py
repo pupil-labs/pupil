397ebaf6589d28491ac370dc3e551cc320ee6721
Marc Tonsen <mt@pupil-labs.com>
2017-01-16 16:44:54 +0100

    Merge with papr's prior commit

--- pupil_src/capture/blink_detection.py
+++ pupil_src/capture/blink_detection.py
@@ -1,21 +1,21 @@
 '''
-(*)~---------------------------------------------------------------------------
-Pupil - eye tracking platform
-Copyright (C) 2012-2017  Pupil Labs
+(*)~----------------------------------------------------------------------------------
+ Pupil - eye tracking platform
+ Copyright (C) 2012-2016  Pupil Labs
 
-Distributed under the terms of the GNU
-Lesser General Public License (LGPL v3.0).
-See COPYING and COPYING.LESSER for license details.
----------------------------------------------------------------------------~(*)
+ Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
+ License details are in the file license.txt, distributed as part of this software.
+----------------------------------------------------------------------------------~(*)
 '''
 
 from plugin import Plugin
-from pyglui.cygl.utils import draw_points_norm, RGBA
+from pyglui.cygl.utils import draw_points_norm,RGBA
 from collections import deque
+from itertools import islice
 import numpy as np
+import math
 import logging
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
 
 
 class Blink_Detection(Plugin):
@@ -27,75 +27,94 @@ class Blink_Detection(Plugin):
     def __init__(self, g_pool):
         super(Blink_Detection, self).__init__(g_pool)
         self.order = .8
-        self.history_length = 21  # Should be odd
-        self.confidence_histories = (deque(maxlen=self.history_length), deque(maxlen=self.history_length))
-        self.timestamp_histories = (deque(maxlen=self.history_length), deque(maxlen=self.history_length))
+        self.history_length_per_fps = 0.2
+
+        # The maximum length of the history needs to be set a priori. If we are assuming a maximum framerat of 120 FPS
+        # and a genorous maximum on-set duratio of a blink of 0.5 seconds, 60 frames of history should always be enough
+        self.confidence_histories = (deque(maxlen=60), deque(maxlen=60))
+        self.timestamp_histories = (deque(maxlen=60), deque(maxlen=60))
         self.eyes_are_alive = g_pool.eyes_are_alive
 
         self.is_blink = False
-
-    @property
-    def history_length(self):
-        return self._history_length
-
-    @history_length.setter
-    def history_length(self, hist_len):
-        self._history_length = hist_len
-        self.blink_filter = np.ones(hist_len)
-        self.blink_filter[hist_len//2:] = -1
 
     def update(self, frame=None, events={}):
         # backwards compatibility
         self.recent_events(events)
 
     def recent_events(self, events={}):
-        # Update history
-        for pt in events.get('pupil_positions', []):
-            self.confidence_histories[pt['id']].append(pt['confidence'])
-            self.timestamp_histories[pt['id']].append(pt['timestamp'])
+
+        # Process all pupil_positions events
+        for pt in events.get('pupil_positions',[]):
+            # Update history
+            self.confidence_histories[pt['id']].appendleft(pt['confidence'])
+            self.timestamp_histories[pt['id']].appendleft(pt['timestamp'])
+
+            # Wait for at least 5 frames of history to compute the current framerate with
+            if len(self.timestamp_histories[pt['id']]) < 60:
+                continue
+            else:
+                fps = 60.0 / (self.timestamp_histories[pt['id']][0] -  self.timestamp_histories[pt['id']][59])
+                # fps = 120
+                self.history_length = int(self.history_length_per_fps * fps)
+
+            # Build filter based on current history length
+            filter = np.asarray([-1 for i in range(int(math.floor(self.history_length / 2.0)))] + [1 for i in range(
+                int(math.ceil(self.history_length / 2.0)))])
+            filter = np.ones(self.hist_len)
+            filter[self.hist_len // 2:] = -1
 
             # Compute activations if history is sufficient
-            if self.eyes_are_alive[0] and len(self.confidence_histories[0]) < self.history_length:
-                continue
-            else:
-                act0 = np.dot(self.confidence_histories[0], self.blink_filter)
+            if self.eyes_are_alive[0].value:
+                if len(self.confidence_histories[0]) >= self.history_length:
+                    slice = np.asarray(deque(islice(self.confidence_histories[0], 0, self.history_length)))
+                    # Normalize deviations in the overall magnitude and legth of the used filter
+                    slice = (slice - slice.min()) / (slice.max() - slice.min())
+                    act0 = np.dot(slice, filter) / self.history_length
+                else:
+                    continue
 
-            if self.eyes_are_alive[1] and len(self.confidence_histories[1]) < self.history_length:
-                continue
-            else:
-                act1 = np.dot(self.confidence_histories[1], self.blink_filter)
+            if self.eyes_are_alive[1].value:
+                if len(self.confidence_histories[1]) >= self.history_length:
+                    slice = np.asarray(deque(islice(self.confidence_histories[1], 0, self.history_length)))
+                    # Normalize deviations in the overall magnitude and legth of the used filter
+                    slice = (slice - slice.min()) / (slice.max() - slice.min())
+                    act1 = np.dot(slice, filter) / self.history_length
+                else:
+                    continue
 
-            if self.eyes_are_alive[0] and self.eyes_are_alive[1]:
+            # Combine activations if we are binocular
+            if self.eyes_are_alive[0].value and self.eyes_are_alive[1].value:
                 act = np.maximum(act0, act1)
-            elif self.eyes_are_alive[0]:
+            elif self.eyes_are_alive[0].value:
                 act = act0
-            else:
+            elif self.eyes_are_alive[1].value:
                 act = act1
+            else:
+                return
 
-            if not self.is_blink and act > 7:
-                logger.debug("Blink")
+            # Judge if activation is sufficient for the on-set of a blink
+            if not self.is_blink and act > 0.4:
+                logger.error("Blink")
                 self.is_blink = True
-            if self.is_blink and act < -2:
+
+            # If we also want to measure the off-set of a blink we could do it like this
+            if self.is_blink and act < -0.1:
                 self.is_blink = False
 
-            logger.debug("{} {}".format(self.is_blink, act))
+            print str(self.is_blink) + " " + str(act)
 
+            # Add info to events
             blink_entry = {
                 'topic': 'blink',
                 'activation': act,
-                'timestamp': self.timestamp_histories[pt['id']][10],
-                'is_blink': int(self.is_blink)
+                'timestamp': self.timestamp_histories[pt['id']][int(math.ceil(self.history_length / 2.0))],
+                'is_blink': self.is_blink
             }
 
-            if 'blinks' not in events:
+            if not 'blinks' in events:
                 events['blinks'] = []
             events['blinks'].append(blink_entry)
 
-    def gl_display(self):
-        if not self.is_blink:
-            draw_points_norm([(0.5, 0.5)],
-                             size=35,
-                             color=RGBA(1., .2, .4, 1.))
 
     def get_init_dict(self):
         return {}
