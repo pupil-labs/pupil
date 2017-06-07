9d21205eb797e837477eca36d330c0e1148f7b78
Moritz Kassner <moritzkassner@gmail.com>
2017-06-07 11:29:02 +0200

    visual feedback of calibration marker

--- pupil_src/player/gaze_producers.py
+++ pupil_src/player/gaze_producers.py
@@ -354,6 +354,9 @@ class Offline_Calibration(Gaze_Producer_Base):
         self.persistent.save()
 
     def gl_display(self):
+        ref_points_norm = [r['norm_pos'] for r in self.ref_positions_by_frame[self.g_pool.capture.get_frame_index()] if r]
+        draw_points_norm(ref_points_norm,color=RGBA(0,.5,0.5,.7))
+
         padding = 30.
         max_ts = len(self.g_pool.timestamps)
 
