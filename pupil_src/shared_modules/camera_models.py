759be316706ce0357fd77f1081a4d3dbc827da9c
marc-tonsen <mt@pupil-labs.com>
2017-08-23 10:07:11 +0200

    Added copyright notice

--- pupil_src/shared_modules/camera_models.py
+++ pupil_src/shared_modules/camera_models.py
@@ -1,3 +1,14 @@
+'''
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+'''
+
 import numpy as np
 import cv2
 import os
