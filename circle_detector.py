bd4a4959d48ba04e74e74abfe36f1e983ed6ee88
Ching <31063965+ChingT@users.noreply.github.com>
2017-09-29 17:23:11 +0200

    Circle marker improvements
    To refactor the changes into a new function called find_pupil_circle_marker(img, find_v2_marker=True,find_v3_marker=True)

---
+++ circle_detector.py
@@ -0,0 +1,154 @@
+'''
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+'''
+
+import numpy as np
+import cv2
+from methods import dist_pts_ellipse, normalize
+
+
+def find_concetric_circles(gray_img,min_ring_count=3, visual_debug=False):
+    # get threshold image used to get crisp-clean edges using blur to remove small features
+    edges = cv2.adaptiveThreshold(cv2.blur(gray_img,(3,3)), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 7)
+    _, contours, hierarchy = cv2.findContours(edges,
+                                    mode=cv2.RETR_TREE,
+                                    method=cv2.CHAIN_APPROX_NONE,offset=(0,0)) #TC89_KCOS
+    if visual_debug is not False:
+        cv2.drawContours(visual_debug,contours,-1,(200,0,0))
+    if contours is None or hierarchy is None:
+        return []
+    clusters = get_nested_clusters(contours,hierarchy[0],min_nested_count=min_ring_count)
+    concentric_cirlce_clusters = []
+
+    #speed up code by caching computed ellipses
+    ellipses = {}
+
+    # for each cluster fit ellipses and cull members that dont have good ellipse fit
+    for cluster in clusters:
+        if visual_debug is not False:
+            cv2.drawContours(visual_debug, [contours[i] for i in cluster],-1, (0,0,255))
+        candidate_ellipses = []
+        for i in cluster:
+            c = contours[i]
+            if len(c)>5:
+                if not i in ellipses:
+                    e = cv2.fitEllipse(c)
+                    fit = max(dist_pts_ellipse(e,c))
+                    ellipses[i] = e,fit
+                else:
+                    e,fit = ellipses[i]
+                a,b = e[1][0]/2.,e[1][1]/2.
+                if fit<max(2,max(e[1])/20):
+                    candidate_ellipses.append(e)
+                    if visual_debug is not False:
+                        cv2.ellipse(visual_debug, e, (0,255,0),1)
+
+        if candidate_ellipses:
+            cluster_center = np.mean(np.array([e[0] for e in candidate_ellipses]),axis=0)
+            candidate_ellipses = [e for e in candidate_ellipses if np.linalg.norm(e[0]-cluster_center)<max(3,min(e[1])/20) ]
+            if len(candidate_ellipses) >= min_ring_count:
+                concentric_cirlce_clusters.append(candidate_ellipses)
+                if visual_debug is not False:
+                    cv2.ellipse(visual_debug, candidate_ellipses[-1], (0,255,255),4)
+
+    #return clusters sorted by size of outmost cirlce biggest first.
+    return sorted(concentric_cirlce_clusters,key=lambda e:-max(e[-1][1]))
+
+def find_pupil_circle_marker(gray_img, find_v2_marker=True,find_v3_marker=True):
+    cirlce_clusters = find_concetric_circles(gray_img, min_ring_count=3)
+    img_size = gray_img.shape[::-1]
+
+    for i in range(len(cirlce_clusters)):
+        detected = False
+        Largest_ellipse = cirlce_clusters[i][-1]
+        col_slice = int(Largest_ellipse[0][0] - Largest_ellipse[1][0]/2), int(Largest_ellipse[0][0] + Largest_ellipse[1][0]/2)
+        row_slice = int(Largest_ellipse[0][1] - Largest_ellipse[1][1]/2), int(Largest_ellipse[0][1] + Largest_ellipse[1][1]/2)
+        marker_gray = gray_img[slice(*row_slice), slice(*col_slice)]
+
+        # To avoid index out of bounds with size 0
+        if not min(marker_gray.shape):
+            continue
+
+        avg = cv2.mean(marker_gray)[0]
+        if min(marker_gray[0, 0], marker_gray[-1, -1]) < avg:
+            continue
+
+        if find_v2_marker:
+            if len(cirlce_clusters[i]) >= 7:
+                ring_ratio = cirlce_clusters[i][-1][1][0] / cirlce_clusters[i][-3][1][0]
+                if ring_ratio > 1.1:
+                    detected = True
+            elif len(cirlce_clusters[i]) >= 4:
+                ring_ratio = cirlce_clusters[i][-1][1][0] / cirlce_clusters[i][-2][1][0]
+                if ring_ratio > 1.25:
+                    detected = True
+            if detected:
+                img_pos = cirlce_clusters[i][0][0]
+                norm_pos = normalize(img_pos, img_size, flip_y=True)
+                return {'ellipses': cirlce_clusters[i], 'version': 2, 'img_pos': img_pos, 'norm_pos':norm_pos}
+
+        if find_v3_marker:
+            if len(cirlce_clusters[i]) >= 5:
+                ring_ratio = cirlce_clusters[i][4][1][0] / cirlce_clusters[i][2][1][0]
+                if abs(1.5 - ring_ratio) < 0.2:
+                    detected = True
+            else:
+                ring_ratio = cirlce_clusters[i][-1][1][0] / cirlce_clusters[i][1][1][0]
+                if ring_ratio > 1.55:
+                    detected = True
+            if detected:
+                img_pos = cirlce_clusters[i][0][0]
+                norm_pos = normalize(img_pos, img_size, flip_y=True)
+                return {'ellipses': cirlce_clusters[i], 'version': 3, 'img_pos': img_pos, 'norm_pos':norm_pos}
+
+
+def add_parents(child,graph,family):
+    family.append(child)
+    parent = graph[child,-1]
+    if parent !=-1:
+        family = add_parents(parent,graph,family)
+    return family
+
+
+def get_nested_clusters(contours,hierarchy,min_nested_count):
+    clusters = {}
+    # nesting of countours where many children are grouping in a sigle parent happens a lot (your screen with stuff in it. A page with text...)
+    # we create a cluster for each of these children.
+    # to reduce CPU load we onle keep the biggest cluster for clusters where the innermost parent is the same.
+    for i in np.where(hierarchy[:,2]==-1)[0]: #contours with no children
+        if len(contours[i])>=5: #we assume that valid markers innermost contour is longer than 5px
+            cluster = add_parents(i,hierarchy,[])
+            # is this cluster bigger that the current contender in the innermost parent group if if already exsists?
+            if min_nested_count < len(cluster) > len(clusters.get(cluster[1],[])):
+                clusters[cluster[1]] = cluster
+    return clusters.values()
+
+
+if __name__ == '__main__':
+    def bench():
+        import cv2
+        cap = cv2.VideoCapture(0)
+        cap.set(3,1280)
+        cap.set(4,720)
+        for x in range(100):
+            sts,img = cap.read()
+            # img = cv2.imread('/Users/mkassner/Desktop/manual_calibration_marker-01.png')
+            gray  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            print(len(find_concetric_circles(gray,visual_debug=img)))
+            cv2.imshow('img',img)
+            cv2.waitKey(1)
+            # return
+
+
+    import cProfile,subprocess,os
+    cProfile.runctx("bench()",{},locals(),"world.pstats")
+    loc = os.path.abspath(__file__).rsplit('pupil_src', 1)
+    gprof2dot_loc = os.path.join(loc[0], 'pupil_src', 'shared_modules','gprof2dot.py')
+    subprocess.call("python "+gprof2dot_loc+" -f pstats world.pstats | dot -Tpng -o world_cpu_time.png", shell=True)
+    print("created  time graph for  process. Please check out the png next to this file")
---
+++ manual_marker_calibration.py
@@ -0,0 +1,283 @@
+'''
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2017  Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+'''
+
+import os
+import cv2
+import numpy as np
+from methods import normalize,denormalize
+from pyglui.cygl.utils import draw_points_norm,draw_polyline,RGBA
+from OpenGL.GL import GL_POLYGON
+from circle_detector import find_concetric_circles,find_pupil_circle_marker
+from . finish_calibration import finish_calibration
+from file_methods import load_object
+
+import audio
+
+from pyglui import ui
+from . calibration_plugin_base import Calibration_Plugin
+#logging
+import logging
+logger = logging.getLogger(__name__)
+
+class Manual_Marker_Calibration(Calibration_Plugin):
+    """Detector looks for a white ring on a black background.
+        Using at least 9 positions/points within the FOV
+        Ref detector will direct one to good positions with audio cues
+        Calibration only collects data at the good positions
+
+        Steps:
+            Adaptive threshold to obtain robust edge-based image of marker
+            Find contours and filter into 2 level list using RETR_CCOMP
+            Fit ellipses
+    """
+    def __init__(self, g_pool):
+        super().__init__(g_pool)
+        self.detected = False
+        self.pos = None
+        self.smooth_pos = 0.,0.
+        self.smooth_vel = 0.
+        self.sample_site = (-2,-2)
+        self.counter = 0
+        self.counter_max = 30
+        self.world_size = None
+
+        self.stop_marker_found = False
+        self.auto_stop = 0
+        self.auto_stop_max = 30
+
+        self.menu = None
+
+        self.markers = None
+
+    def init_gui(self):
+        super().init_gui()
+        self.info = ui.Info_Text("Calibrate gaze parameters using a handheld marker.")
+        self.g_pool.calibration_menu.append(self.info)
+
+        self.menu = ui.Growing_Menu('Controls')
+        self.g_pool.calibration_menu.append(self.menu)
+
+    def deinit_gui(self):
+        if self.menu:
+            self.g_pool.calibration_menu.remove(self.menu)
+            self.g_pool.calibration_menu.remove(self.info)
+            self.menu = None
+        super().deinit_gui()
+
+    def start(self):
+        super().start()
+        audio.say("Starting {}".format(self.mode_pretty))
+        logger.info("Starting {}".format(self.mode_pretty))
+        self.active = True
+        self.ref_list = []
+        self.pupil_list = []
+
+    def stop(self):
+        audio.say("Stopping  {}".format(self.mode_pretty))
+        logger.info('Stopping  {}'.format(self.mode_pretty))
+        self.screen_marker_state = 0
+        self.active = False
+        # self.close_window()
+        self.button.status_text = ''
+        if self.mode == 'calibration':
+            finish_calibration(self.g_pool, self.pupil_list, self.ref_list)
+        elif self.mode == 'accuracy_test':
+            self.finish_accuracy_test(self.pupil_list, self.ref_list)
+        super().stop()
+
+    def on_notify(self, notification):
+        '''
+        Reacts to notifications:
+           ``calibration.should_start``: Starts the calibration procedure
+           ``calibration.should_stop``: Stops the calibration procedure
+
+        Emits notifications:
+            ``calibration.started``: Calibration procedure started
+            ``calibration.stopped``: Calibration procedure stopped
+            ``calibration.marker_found``: Steady marker found
+            ``calibration.marker_moved_too_quickly``: Marker moved too quickly
+            ``calibration.marker_sample_completed``: Enough data points sampled
+
+        '''
+        super().on_notify(notification)
+
+    def recent_events(self, events):
+        """
+        gets called once every frame.
+        reference positon need to be published to shared_pos
+        if no reference was found, publish 0,0
+        """
+        frame = events.get('frame')
+        if self.active and frame:
+            recent_pupil_positions = events['pupil_positions']
+
+            gray_img = frame.gray
+
+            if self.world_size is None:
+                self.world_size = frame.width,frame.height
+
+            self.markers = find_pupil_circle_marker(gray_img, find_v2_marker=True, find_v3_marker=True)
+            self.detected = False
+            self.pos = None  # indicate that no reference is detected
+
+            if self.markers:
+                self.detected = True
+                marker_pos = self.markers['img_pos']  # first marker innermost ellipse, pos
+                self.pos = self.markers['norm_pos']
+
+                # center dark or white?
+                second_ellipse = self.markers['ellipses'][1]
+                col_slice = int(marker_pos[0]-second_ellipse[1][0]/2),int(marker_pos[0]+second_ellipse[1][0]/2)
+                row_slice = int(marker_pos[1]-second_ellipse[1][1]/2),int(marker_pos[1]+second_ellipse[1][1]/2)
+                marker_gray = gray_img[slice(*row_slice),slice(*col_slice)]
+
+                # To avoid index out of bounds with size 0
+                if not min(marker_gray.shape):
+                    return
+
+                avg = cv2.mean(marker_gray)[0]
+                center = marker_gray[int(second_ellipse[1][1])//2, int(second_ellipse[1][0])//2]
+                rel_shade = center-avg
+
+                # auto_stop logic
+                if rel_shade > 30:
+                    # bright marker center found
+                    self.auto_stop +=1
+                    self.stop_marker_found = True
+                else:
+                    self.auto_stop = 0
+                    self.stop_marker_found = False
+
+            #tracking logic
+            if self.detected and not self.stop_marker_found:
+                # calculate smoothed manhattan velocity
+                smoother = 0.3
+                smooth_pos = np.array(self.smooth_pos)
+                pos = np.array(self.pos)
+                new_smooth_pos = smooth_pos + smoother*(pos-smooth_pos)
+                smooth_vel_vec = new_smooth_pos - smooth_pos
+                smooth_pos = new_smooth_pos
+                self.smooth_pos = list(smooth_pos)
+                #manhattan distance for velocity
+                new_vel = abs(smooth_vel_vec[0])+abs(smooth_vel_vec[1])
+                self.smooth_vel = self.smooth_vel + smoother*(new_vel-self.smooth_vel)
+
+                #distance to last sampled site
+                sample_ref_dist = smooth_pos-np.array(self.sample_site)
+                sample_ref_dist = abs(sample_ref_dist[0])+abs(sample_ref_dist[1])
+
+                # start counter if ref is resting in place and not at last sample site
+                if self.counter <= 0:
+
+                    if self.smooth_vel < 0.01 and sample_ref_dist > 0.1:
+                        self.sample_site = self.smooth_pos
+                        audio.beep()
+                        logger.debug("Steady marker found. Starting to sample {} datapoints".format(self.counter_max))
+                        self.notify_all({'subject':'calibration.marker_found','timestamp':self.g_pool.get_timestamp(),'record':True})
+                        self.counter = self.counter_max
+
+                if self.counter > 0:
+                    if self.smooth_vel > 0.01:
+                        audio.tink()
+                        logger.warning("Marker moved too quickly: Aborted sample. Sampled {} datapoints. Looking for steady marker again.".format(self.counter_max-self.counter))
+                        self.notify_all({'subject':'calibration.marker_moved_too_quickly','timestamp':self.g_pool.get_timestamp(),'record':True})
+                        self.counter = 0
+                    else:
+                        self.counter -= 1
+                        ref = {}
+                        ref["norm_pos"] = self.pos
+                        ref["screen_pos"] = marker_pos
+                        ref["timestamp"] = frame.timestamp
+                        self.ref_list.append(ref)
+                        if events.get('fixations', []):
+                            self.counter -= 5
+                        if self.counter <= 0:
+                            #last sample before counter done and moving on
+                            audio.tink()
+                            logger.debug("Sampled {} datapoints. Stopping to sample. Looking for steady marker again.".format(self.counter_max))
+                            self.notify_all({'subject':'calibration.marker_sample_completed','timestamp':self.g_pool.get_timestamp(),'record':True})
+
+
+            #always save pupil positions
+            for p_pt in recent_pupil_positions:
+                if p_pt['confidence'] > self.pupil_confidence_threshold:
+                    self.pupil_list.append(p_pt)
+
+            if self.counter:
+                if self.detected:
+                    self.button.status_text = 'Sampling Gaze Data'
+                else:
+                    self.button.status_text = 'Marker Lost'
+            else:
+                self.button.status_text = 'Looking for Marker'
+
+
+
+            #stop if autostop condition is satisfied:
+            if self.auto_stop >=self.auto_stop_max:
+                self.auto_stop = 0
+                self.stop()
+
+
+        else:
+            pass
+
+
+    def get_init_dict(self):
+        return {}
+
+    def gl_display(self):
+        """
+        use gl calls to render
+        at least:
+            the published position of the reference
+        better:
+            show the detected postion even if not published
+        """
+
+        if self.active:
+            draw_points_norm([self.smooth_pos],size=15,color=RGBA(1.,1.,0.,.5))
+
+        if self.active and self.detected:
+            e = self.markers['ellipses'][-1]
+            pts = cv2.ellipse2Poly( (int(e[0][0]),int(e[0][1])),
+                                (int(e[1][0]/2),int(e[1][1]/2)),
+                                int(e[-1]),0,360,15)
+            draw_polyline(pts,color=RGBA(0.,1.,0,1.))
+
+            if self.counter:
+                # lets draw an indicator on the count
+                # cv2 requires integer arguments
+                pts = cv2.ellipse2Poly( (int(e[0][0]),int(e[0][1])),
+                                    (int(e[1][0]/2),int(e[1][1]/2)),
+                                    int(e[-1]),0,360,360//self.counter_max)
+                indicator = [e[0]] + pts[self.counter:].tolist()[::-1] + [e[0]]
+                draw_polyline(indicator,color=RGBA(0.1,.5,.7,.8),line_type=GL_POLYGON)
+
+            if self.auto_stop:
+                # lets draw an indicator on the autostop count
+                # cv2 requires integer arguments
+                pts = cv2.ellipse2Poly( (int(e[0][0]),int(e[0][1])),
+                                    (int(e[1][0]/2),int(e[1][1]/2)),
+                                    int(e[-1]),0,360,360//self.auto_stop_max)
+                indicator = [e[0]] + pts[self.auto_stop:].tolist() + [e[0]]
+                draw_polyline(indicator,color=RGBA(8.,0.1,0.1,.8),line_type=GL_POLYGON)
+        else:
+            pass
+
+    def cleanup(self):
+        """gets called when the plugin get terminated.
+        This happens either voluntarily or forced.
+        if you have an atb bar or glfw window destroy it here.
+        """
+        if self.active:
+            self.stop()
+        self.deinit_gui()
