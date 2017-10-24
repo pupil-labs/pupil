aa505986c214e726f5ae6744db086adf92b6455f
Pablo Prietz <pablo@prietz.org>
2017-10-24 20:03:26 +0200

    Leave one cpu open for other calculations than video transcoding

--- pupil_src/shared_modules/av_writer.py
+++ pupil_src/shared_modules/av_writer.py
@@ -117,7 +117,7 @@ class AV_Writer(object):
         self.video_stream = self.container.add_stream(video_stream['codec'], 1/self.time_base)
         self.video_stream.bit_rate = video_stream['bit_rate']
         self.video_stream.bit_rate_tolerance = video_stream['bit_rate']/20
-        self.video_stream.thread_count = mp.cpu_count()
+        self.video_stream.thread_count = max(1, mp.cpu_count() - 1)
         # self.video_stream.pix_fmt = "yuv420p"
         self.configured = False
         self.start_time = None
