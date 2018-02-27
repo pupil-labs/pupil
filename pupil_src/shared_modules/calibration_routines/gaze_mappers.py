991633586b1decf840b71a11ac9aee7f3faf00b8
Pablo Prietz <pablo@prietz.org>
2018-02-27 15:27:24 +0100

    Gaze topics: Switch format from 0/1/2 to 0./1./01.

--- pupil_src/shared_modules/calibration_routines/gaze_mappers.py
+++ pupil_src/shared_modules/calibration_routines/gaze_mappers.py
@@ -134,7 +134,7 @@ class Dummy_Gaze_Mapper(Monocular_Gaze_Mapper_Base, Gaze_Mapping_Plugin):
         super().__init__(g_pool)
 
     def _map_monocular(self, p):
-        return {'topic': 'gaze.2d.{}'.format(p['id']),
+        return {'topic': 'gaze.2d.{}.'.format(p['id']),
                 'norm_pos': p['norm_pos'],
                 'confidence': p['confidence'],
                 'timestamp': p['timestamp'],
@@ -158,7 +158,7 @@ class Monocular_Gaze_Mapper(Monocular_Gaze_Mapper_Base, Gaze_Mapping_Plugin):
 
     def _map_monocular(self, p):
         gaze_point = self.map_fn(p['norm_pos'])
-        return {'topic': 'gaze.2d.{}'.format(p['id']),
+        return {'topic': 'gaze.2d.{}.'.format(p['id']),
                 'norm_pos': gaze_point,
                 'confidence': p['confidence'],
                 'id': p['id'],
@@ -180,7 +180,7 @@ class Dual_Monocular_Gaze_Mapper(Monocular_Gaze_Mapper_Base, Gaze_Mapping_Plugin
 
     def _map_monocular(self, p):
         gaze_point = self.map_fns[p['id']](p['norm_pos'])
-        return {'topic': 'gaze.2d.{}'.format(p['id']),
+        return {'topic': 'gaze.2d.{}.'.format(p['id']),
                 'norm_pos': gaze_point,
                 'confidence': p['confidence'],
                 'id': p['id'],
@@ -221,7 +221,7 @@ class Binocular_Gaze_Mapper(Binocular_Gaze_Mapper_Base, Gaze_Mapping_Plugin):
                           (gaze_point_eye0[1] + gaze_point_eye1[1])/2.)
         confidence = (p0['confidence'] + p1['confidence'])/2.
         ts = (p0['timestamp'] + p1['timestamp'])/2.
-        return {'topic': 'gaze.2d.2',
+        return {'topic': 'gaze.2d.01.',
                 'norm_pos': gaze_point,
                 'confidence': confidence,
                 'timestamp': ts,
@@ -229,7 +229,7 @@ class Binocular_Gaze_Mapper(Binocular_Gaze_Mapper_Base, Gaze_Mapping_Plugin):
 
     def _map_monocular(self, p):
         gaze_point = self.map_fn_fallback[p['id']](p['norm_pos'])
-        return {'topic': 'gaze.2d.2',
+        return {'topic': 'gaze.2d.{}.'.format(p['id']),
                 'norm_pos': gaze_point,
                 'confidence': p['confidence'],
                 'timestamp': p['timestamp'],
@@ -294,7 +294,7 @@ class Vector_Gaze_Mapper(Monocular_Gaze_Mapper_Base,Gaze_Mapping_Plugin):
         gaze_3d = self.toWorld(gaze_point)
         normal_3d = np.dot( self.rotation_matrix, np.array( p['circle_3d']['normal'] ) )
 
-        g = {   'topic': 'gaze.2d.2',
+        g = {   'topic': 'gaze.3d.{}.'.format(p['id']),
                 'norm_pos': image_point,
                 'eye_center_3d': eye_center.tolist(),
                 'gaze_normal_3d': normal_3d.tolist(),
@@ -407,7 +407,7 @@ class Binocular_Vector_Gaze_Mapper(Binocular_Gaze_Mapper_Base,Gaze_Mapping_Plugi
 
         normal_3d = self.rotation_matricies[p_id] @ np.array(p['circle_3d']['normal'])
 
-        g = {'topic': 'gaze.3d.2',
+        g = {'topic': 'gaze.3d.{}.'.format(p_id),
              'eye_centers_3d': {p['id']: eye_center.tolist()},
              'gaze_normals_3d': {p['id']: normal_3d.tolist()},
              'gaze_point_3d': gaze_3d.tolist(),
@@ -490,7 +490,7 @@ class Binocular_Vector_Gaze_Mapper(Binocular_Gaze_Mapper_Base,Gaze_Mapping_Plugi
 
         confidence = min(p0['confidence'],p1['confidence'])
         ts = (p0['timestamp'] + p1['timestamp'])/2.
-        g = {'topic': 'gaze.3d.2',
+        g = {'topic': 'gaze.3d.01.',
              'eye_centers_3d': {0: s0_center.tolist(), 1: s1_center.tolist()},
              'gaze_normals_3d': {0: s0_normal.tolist(), 1: s1_normal.tolist()},
              'gaze_point_3d': nearest_intersection_point.tolist(),
