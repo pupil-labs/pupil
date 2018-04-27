519208a885a405b3070ac9cf29f4afec9e483113
Moritz Kassner <moritzkassner@gmail.com>
2018-04-27 08:43:27 +0200

    adding logic for macos.

--- deployment/bundle.sh
+++ deployment/bundle.sh
@@ -2,7 +2,15 @@
 
 # create release dir from latest tag
 current_tag=$(git describe --tags | awk -F"-" '{print $1"."$2}')
-release_dir=$(echo "pupil_${current_tag}_linux_x64")
+current_tag=${current_tag%?} #strip trailing .
+
+if [ "$(uname)" == "Darwin" ]; then
+    release_dir=$(echo "pupil_${current_tag}_macos_x64")
+    ext=dmg
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    release_dir=$(echo "pupil_${current_tag}_linux_x64")
+    ext=deb
+fi
 echo "release_dir:  ${release_dir}"
 mkdir ${release_dir}
 
@@ -20,19 +28,19 @@ python3 ../pupil_src/shared_modules/calibration_routines/optimization_calibratio
 printf "\n##########\nBundling Pupil Capture\n##########\n\n"
 cd deploy_capture
 ./bundle.sh
-mv *.deb ../$release_dir
+mv *.$ext ../$release_dir
 
 # bundle Pupil Service
 printf "\n##########\nBundling Pupil Service\n##########\n\n"
 cd ../deploy_service
 ./bundle.sh
-mv *.deb ../$release_dir
+mv *.$ext ../$release_dir
 
 # bundle Pupil Player
 printf "\n##########\nBundling Pupil Player\n##########\n\n"
 cd ../deploy_player
 ./bundle.sh
-mv *.deb ../$release_dir
+mv *.$ext ../$release_dir
 
 cd ..
 printf "\n##########\nzipping release\n##########\n\n"
