pupil-labs/pupil

commit 06d9d795c08771cfa390f535496d49ec9d5c2959

cmaera_models.unprojectPoints: Return 3d points
Fixes #1125

pupil_src/shared_modules/reference_surface.py
@@ -157,10 +157,10 @@ class Reference_Surface(object):
         if not all_verts:
             return
         all_verts = np.array(all_verts,dtype=np.float32)
-        all_verts.shape = (-1,1,2) # [vert,vert,vert,vert,vert...] with vert = [[r,c]]
         # all_verts_undistorted_normalized centered in img center flipped in y and range [-1,1]
-        all_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(all_verts, use_distortion=self.use_distortion)
-        hull = cv2.convexHull(all_verts_undistorted_normalized.astype(np.float32),clockwise=False)
+        all_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(all_verts, use_distortion=self.use_distortion)[:, :2]
+        all_verts_undistorted_normalized.shape = -1, 1, 2
+        hull = cv2.convexHull(all_verts_undistorted_normalized.astype(np.float32), clockwise=False)
 
         #simplify until we have excatly 4 verts
         if hull.shape[0]>4:
@@ -322,7 +322,8 @@ class Reference_Surface(object):
             # our camera lens creates distortions we want to get a good 2d estimate despite that so we:
             # compute the homography transform from marker into the undistored normalized image space
             # (the line below is the same as what you find in methods.undistort_unproject_pts, except that we ommit the z corrd as it is always one.)
-            xy_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(xy.reshape(-1,2), use_distortion=self.use_distortion)
+            xy_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(xy, use_distortion=self.use_distortion)[:, :2]
+            xy_undistorted_normalized.shape = -1, 1, 2
 
             m_to_undistored_norm_space,mask = cv2.findHomography(uv,xy_undistorted_normalized, method=cv2.RANSAC,ransacReprojThreshold=100)
             if not mask.all():
@@ -510,12 +511,9 @@ class Reference_Surface(object):
         if res['detected']:
             support_marker = Support_Marker(marker['id'])
             marker_verts = np.array(marker['verts'])
-            marker_verts.shape = (-1,1,2)
-            if self.use_distortion:
-                marker_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(marker_verts)
-            else:
-                marker_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(marker_verts)
-            marker_uv_coords =  cv2.perspectiveTransform(marker_verts_undistorted_normalized,res['m_from_undistored_norm_space'])
+            marker_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(marker_verts, use_distortion=self.use_distortion)[:, :2]
+            marker_verts_undistorted_normalized.shape = -1, 1, 2
+            marker_uv_coords = cv2.perspectiveTransform(marker_verts_undistorted_normalized,res['m_from_undistored_norm_space'])
             support_marker.load_uv_coords(marker_uv_coords)
             self.markers[marker['id']] = support_marker
 

pupil_src/shared_modules/accuracy_visualizer.py
@@ -170,14 +170,7 @@ class Accuracy_Visualizer(Plugin):
         # offset (distance) (in degrees of visual angle)
         # between fixations locations and the corresponding
         # locations of the fixation targets.
-        undistorted = intrinsics.unprojectPoints(locations)
-        undistorted.shape = -1, 2
-        # append column with z=1
-        # using idea from https://stackoverflow.com/questions/8486294/how-to-add-an-extra-column-to-an-numpy-array
-        undistorted_3d = np.ones((undistorted.shape[0], 3))  # shape: 2n x 3
-        undistorted_3d[:, :-1] = undistorted
-        # normalize vectors:
-        undistorted_3d /= np.linalg.norm(undistorted_3d, axis=1)[:, np.newaxis]
+        undistorted_3d = intrinsics.unprojectPoints(locations, normalize=True)
 
         # Cosine distance of A and B: (A @ B) / (||A|| * ||B||)
         # No need to calculate norms, since A and B are normalized in our case.

pupil_src/shared_modules/calibration_routines/calibrate.py
@@ -348,12 +348,7 @@ def preprocess_3d_data(matched_data, g_pool):
             # projected point uv to normal ray vector of camera
             ref = data_point['ref']['screen_pos']
             ref_vector = np.array(ref).reshape(-1,1,2)
-            ref_vector = g_pool.capture.intrinsics.unprojectPoints(ref_vector)
-            ref_vector = cv2.convertPointsToHomogeneous(np.float32(ref_vector))
-            ref_vector.shape = (-1, 3)
-            ref_vector = ref_vector.tolist()[0]
-
-            ref_vector = ref_vector / np.linalg.norm(ref_vector)
+            ref_vector = g_pool.capture.intrinsics.unprojectPoints(ref_vector, normalize=True)
             # assuming a fixed (assumed) distance we get a 3d point in world camera 3d coords.
             ref_processed.append(ref_vector)
 

pupil_src/shared_modules/camera_models.py
@@ -160,17 +160,17 @@ class Fisheye_Dist_Camera(object):
 
         return undistorted_img
 
-    def unprojectPoints(self, dist_pts, use_distortion=True):
+    def unprojectPoints(self, pts_2d, use_distortion=True, normalize=False):
         """
         Undistorts points according to the camera model.
         cv2.fisheye.undistortPoints does *NOT* perform the same unprojection step the original cv2.unprojectPoints does.
         Thus we implement this function ourselves.
-        :param dist_pts: Distorted points. Can be a list of points or a single point.
-        :return: Array of undistorted points with the same shape as the input
+        :param pts_2d: Distorted points. Can be a list of points or a single point.
+        :return: Array of unprojected 3d points
         """
-        input_shape = dist_pts.shape
+        input_shape = pts_2d.shape
 
-        dist_pts = dist_pts.reshape((-1, 2))
+        pts_2d = pts_2d.reshape((-1, 2))
         eps = np.finfo(np.float32).eps
 
         f = np.array((self.K[0, 0], self.K[1, 1])).reshape(1, 2)
@@ -180,7 +180,7 @@ class Fisheye_Dist_Camera(object):
         else:
             k = np.asarray([1. / 3., 2. / 15., 17. / 315., 62. / 2835.], dtype=np.float32)
 
-        pi = dist_pts.astype(np.float32)
+        pi = pts_2d.astype(np.float32)
         pw = (pi - c) / f
 
         theta_d = np.linalg.norm(pw, ord=2, axis=1)
@@ -194,10 +194,15 @@ class Fisheye_Dist_Camera(object):
 
         scale = np.tan(theta) / (theta_d + eps)
 
-        pu = pw * scale.reshape(-1, 1)
+        pts_2d_undist = pw * scale.reshape(-1, 1)
 
-        pu.shape = input_shape
-        return pu
+        pts_3d = cv2.convertPointsToHomogeneous(pts_2d_undist)
+        pts_3d.shape = -1, 3
+
+        if normalize:
+            pts_3d /= np.linalg.norm(pts_3d, axis=1)
+
+        return pts_3d
 
     def projectPoints(self, object_points, rvec=None, tvec=None, use_distortion=True):
         """
@@ -295,42 +300,45 @@ class Radial_Dist_Camera(object):
         undist_img = cv2.undistort(img, self.K, self.D)
         return undist_img
 
-    def unprojectPoints(self, dist_pts, use_distortion=True):
+    def unprojectPoints(self, pts_2d, use_distortion=True, normalize=False):
         """
         Undistorts points according to the camera model.
-        :param dist_pts: Distorted points. Can be a list of points or a single point.
-        :return: Array of undistorted points with the same shape as the input
+        :param pts_2d: 2d points. Can be a list of points or a single point.
+        :return: Array of unprojected 3d points
         """
-        dist_pts = np.array(dist_pts)
-        input_shape = dist_pts.shape
+        pts_2d = np.array(pts_2d, dtype=np.float32)
+        input_shape = pts_2d.shape
 
         # If given a single point expand to a 1-point list
-        if len(dist_pts.shape) == 1:
-            dist_pts = dist_pts.reshape((1, 2))
+        if len(pts_2d.shape) == 1:
+            pts_2d = pts_2d.reshape((1, 2))
 
         # Delete any posibly wrong 3rd dimension
-        if dist_pts.ndim == 3:
-            dist_pts = dist_pts.reshape((-1, 2))
+        if pts_2d.ndim == 3:
+            pts_2d = pts_2d.reshape((-1, 2))
 
         # Add third dimension the way cv2 wants it
-        if dist_pts.ndim == 2:
-            dist_pts = dist_pts.reshape((-1, 1, 2))
+        if pts_2d.ndim == 2:
+            pts_2d = pts_2d.reshape((-1, 1, 2))
 
         if use_distortion:
             _D = self.D
         else:
             _D = np.asarray([[0.,0.,0.,0.,0.]])
 
-        undist_pts = cv2.undistortPoints(
-            dist_pts.astype(np.float32),
+        pts_2d_undist = cv2.undistortPoints(
+            pts_2d,
             self.K,
             _D,
         )
 
-        # Restore whatever shape we had in the beginning
-        undist_pts.shape = input_shape
+        pts_3d = cv2.convertPointsToHomogeneous(pts_2d_undist)
+        pts_3d.shape = -1, 3
 
-        return undist_pts
+        if normalize:
+            pts_3d /= np.linalg.norm(pts_3d, axis=1)
+
+        return pts_3d
 
     def projectPoints(self, object_points, rvec=None, tvec=None, use_distortion=True):
         """

pupil_src/shared_modules/fixation_detector.py
@@ -117,9 +117,7 @@ def gaze_dispersion(capture, gaze_subset, use_pupil=True):
         locations[:, 1] = (1. - locations[:, 1]) * height
 
         # undistort onto 3d plane
-        undistorted = capture.intrinsics.unprojectPoints(locations)
-        vectors = np.ones((undistorted.shape[0], 3), dtype=undistorted.dtype)
-        vectors[:, :-1] = undistorted
+        vectors = capture.intrinsics.unprojectPoints(locations)
 
     dist = vector_dispersion(vectors)
     return dist, method, base_data

pupil_src/shared_modules/imotions_exporter.py
@@ -204,9 +204,7 @@ class iMotions_Exporter(Analysis_Plugin_Base):
                             pupil_dia[p['id']] = p['diameter_3d']
 
                         pixel_pos = denormalize(g['norm_pos'], self.g_pool.capture.frame_size, flip_y=True)
-                        undistorted2d = self.g_pool.capture.intrinsics.unprojectPoints(pixel_pos)
-                        undistorted3d = np.ones(3)
-                        undistorted3d[:2] = undistorted2d
+                        undistorted3d = self.g_pool.capture.intrinsics.unprojectPoints(pixel_pos)
                         undistorted2d = self.g_pool.capture.intrinsics.projectPoints(undistorted3d, use_distortion=False)
 
                         data = (g['timestamp'],

commit 7ce62c8f53c9c975bec94c79d1e225a630131252

fixed regression caused by 06d9d795c08771cfa390f535496d49ec9d5c2959

pupil_src/shared_modules/reference_surface.py
@@ -158,8 +158,10 @@ class Reference_Surface(object):
         if not all_verts:
             return
         all_verts = np.array(all_verts,dtype=np.float32)
-        # all_verts_undistorted_normalized centered in img center flipped in y and range [-1,1]
-        all_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(all_verts, use_distortion=self.use_distortion)[:, :2]
+        all_verts.shape = (-1,1,2) # [vert,vert,vert,vert,vert...] with vert = [[r,c]]
+
+
+        all_verts_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(all_verts, use_distortion=self.use_distortion)[:, :2] # we ommit the z corrd as it is 1.
         all_verts_undistorted_normalized.shape = -1, 1, 2
         hull = cv2.convexHull(all_verts_undistorted_normalized.astype(np.float32), clockwise=False)
 
@@ -318,11 +320,11 @@ class Reference_Surface(object):
             detected = True
             xy = np.array( [marker_by_id[i]['verts'] for i in overlap] )
             uv = np.array( [self.markers[i].uv_coords for i in overlap] )
-            uv.shape=(-1,1,2)
-
+            uv.shape=(-1,1,2) # [vert,vert,vert,vert,vert...] with vert = [[r,c]]
+            xy.shape=(-1,1,2) # [vert,vert,vert,vert,vert...] with vert = [[r,c]]
             # our camera lens creates distortions we want to get a good 2d estimate despite that so we:
             # compute the homography transform from marker into the undistored normalized image space
-            # (the line below is the same as what you find in methods.undistort_unproject_pts, except that we ommit the z corrd as it is always one.)
+            # we ommit the z corrd as it is always one.
             xy_undistorted_normalized = self.g_pool.capture.intrinsics.unprojectPoints(xy, use_distortion=self.use_distortion)[:, :2]
             xy_undistorted_normalized.shape = -1, 1, 2
 