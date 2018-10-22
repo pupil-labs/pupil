e90f2fbd40d2da92926b0d3d036224619e2011a9
Marc Tonsen <mt@pupil-labs.com>
2018-10-22 15:54:33 +0200

    Revert background logger changes handled in a separate PR

--- pupil_src/shared_modules/background_helper.py
+++ pupil_src/shared_modules/background_helper.py
@@ -1,4 +1,4 @@
-'''
+"""
 (*)~---------------------------------------------------------------------------
 Pupil - eye tracking platform
 Copyright (C) 2012-2018 Pupil Labs
@@ -7,12 +7,14 @@ Distributed under the terms of the GNU
 Lesser General Public License (LGPL v3.0).
 See COPYING and COPYING.LESSER for license details.
 ---------------------------------------------------------------------------~(*)
-'''
+"""
 
 from ctypes import c_bool
 import multiprocessing as mp
+
 # mp = mp.get_context('fork')
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,7 +23,8 @@ class EarlyCancellationError(Exception):
 
 
 class Task_Proxy(object):
-    '''Future like object that runs a given generator in the background and returns is able to return the results incrementally'''
+    """Future like object that runs a given generator in the background and returns is able to return the results incrementally"""
+
     def __init__(self, name, generator, args=(), kwargs={}):
         super().__init__()
 
@@ -32,42 +35,35 @@ class Task_Proxy(object):
         pipe_recv, pipe_send = mp.Pipe(False)
         wrapper_args = [pipe_send, self._should_terminate_flag, generator]
         wrapper_args.extend(args)
-        self.process = mp.Process(target=self._wrapper, name=name, args=wrapper_args, kwargs=kwargs)
+        self.process = mp.Process(
+            target=self._wrapper, name=name, args=wrapper_args, kwargs=kwargs
+        )
         self.process.daemon = True
         self.process.start()
         self.pipe = pipe_recv
 
     def _wrapper(self, pipe, _should_terminate_flag, generator, *args, **kwargs):
-        '''Executed in background, pipes generator results to foreground'''
-        self._fix_background_logger()
-        logger.debug('Entering _wrapper')
-
+        """Executed in background, pipes generator results to foreground"""
+        logger.debug("Entering _wrapper")
         try:
             for datum in generator(*args, **kwargs):
                 if _should_terminate_flag.value:
-                    raise EarlyCancellationError('Task was cancelled')
+                    raise EarlyCancellationError("Task was cancelled")
                 pipe.send(datum)
         except Exception as e:
             pipe.send(e)
             if not isinstance(e, EarlyCancellationError):
                 import traceback
+
                 print(traceback.format_exc())
         else:
             pipe.send(StopIteration())
         finally:
             pipe.close()
-            logger.debug('Exiting _wrapper')
-
-    def _fix_background_logger(self):
-        del logger.root.handlers[0]
-        ch = logging.StreamHandler()
-        ch.setFormatter(logging.Formatter(
-            '%(processName)s - [%(levelname)s] %(name)s: %(message)s'))
-        ch.setLevel(logger.root.level)
-        logger.addHandler(ch)
+            logger.debug("Exiting _wrapper")
 
     def fetch(self):
-        '''Fetches progress and available results from background'''
+        """Fetches progress and available results from background"""
         if self.completed or self.canceled:
             return
 
@@ -113,34 +109,41 @@ class Task_Proxy(object):
         self.process = None
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(processName)s - [%(levelname)s] %(name)s: %(message)s')
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(processName)s - [%(levelname)s] %(name)s: %(message)s",
+    )
 
     def example_generator(mu=0., sigma=1., steps=100):
-        '''samples `N(\mu, \sigma^2)`'''
+        """samples `N(\mu, \sigma^2)`"""
         import numpy as np
         from time import sleep
+
         for i in range(steps):
             # yield progress, datum
             yield (i + 1) / steps, sigma * np.random.randn() + mu
             sleep(np.random.rand() * .1)
 
     # initialize task proxy
-    task = Task_Proxy('Background', example_generator, args=(5., 3.), kwargs={'steps': 100})
+    task = Task_Proxy(
+        "Background", example_generator, args=(5., 3.), kwargs={"steps": 100}
+    )
 
     from time import time, sleep
+
     start = time()
     maximal_duration = 2.
     while time() - start < maximal_duration:
         # fetch all available results
         for progress, random_number in task.fetch():
-            logger.debug('[{:3.0f}%] {:0.2f}'.format(progress * 100, random_number))
+            logger.debug("[{:3.0f}%] {:0.2f}".format(progress * 100, random_number))
 
         # test if task is completed
         if task.completed:
             break
         sleep(1.)
 
-    logger.debug('Canceling task')
+    logger.debug("Canceling task")
     task.cancel(timeout=1)
-    logger.debug('Task done')
+    logger.debug("Task done")
