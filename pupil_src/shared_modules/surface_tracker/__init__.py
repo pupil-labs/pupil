c3604ea467bf224a15eb43cb71a74d91639d7b8d
Marc Tonsen <mt@pupil-labs.com>
2018-10-26 15:34:43 +0200

    Improvee _Surface_Marker_Aggregate docstring

--- pupil_src/shared_modules/surface_tracker/__init__.py
+++ pupil_src/shared_modules/surface_tracker/__init__.py
@@ -26,9 +26,12 @@ class Heatmap_Mode(Enum):
 
 class _Surface_Marker_Aggregate(object):
     """
-    A Surface Marker Aggregate is located in normalized surface space, unlike regular Markers which are
-    located in image space. It's location on the surface is aggregated over a list of
-    observations.
+    Stores a list of detections of a specific square marker and aggregates them over
+    time to get a more robust localisation.
+
+    A marker detection is represented by the location of the marker vertices. The
+    vertices are expected to be in normalized surface coordinates, unlike the
+    vertices of a regular Marker, which are located in image pixel space.
     """
 
     def __init__(self, id, verts_uv=None):
@@ -39,8 +42,8 @@ class _Surface_Marker_Aggregate(object):
         if verts_uv is not None:
             self.verts_uv = np.array(verts_uv)
 
-    def add_observation(self, uv_coords):
-        self.observations.append(uv_coords)
+    def add_observation(self, verts_uv):
+        self.observations.append(verts_uv)
         self._compute_robust_mean()
 
     def _compute_robust_mean(self):
