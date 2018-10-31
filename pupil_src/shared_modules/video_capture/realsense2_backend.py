d91fd768b90816a6afd35ee44b7d9b82b5de61e8
emredog <emredog@gmail.com>
2018-10-31 16:13:00 +0100

    Realsense2: Access & display color and depth frames.

--- pupil_src/shared_modules/video_capture/realsense2_backend.py
+++ pupil_src/shared_modules/video_capture/realsense2_backend.py
@@ -42,6 +42,118 @@ logger.setLevel(logging.DEBUG)
 # FIXME
 TIMEOUT = 1000  # ms
 
+# very thin wrapper for rs.frame objects
+class ColorFrame(object):
+    def __init__(self, data, timestamp, index):
+        self.timestamp = timestamp
+        self.index = index
+
+        # data.shape: (480, 640) for yuyv
+        logger.debug("ColorFrame data.shape : " + str(data.shape))
+        # data is 480x640 uint16, but we need 480x640x2 uint8 to work
+
+        data = data[:, :, np.newaxis].view(dtype=np.uint8)
+        total_size = data.size
+        y_plane = total_size // 2
+        u_plane = y_plane // 2
+        logger.debug(data.shape)
+        self._yuv = np.empty(total_size, dtype=np.uint8)
+        self._yuv[:y_plane] = data[:, :, 0].ravel()
+        self._yuv[y_plane : y_plane + u_plane] = data[:, ::2, 1].ravel()
+        self._yuv[y_plane + u_plane :] = data[:, 1::2, 1].ravel()
+        self._shape = data.shape[:2]  # (480, 640)
+
+        # channel1 = (data >> 8) & 0xFF
+        # channel2 = data & 0xFF
+
+        # self._yuv422 = np.stack((channel1, channel2), axis=2)
+
+        # logger.debug("ColorFrame self._yuv422.shape : " + str(self._yuv422.shape))
+
+        # self._shape = self._yuv422.shape[:2]  # (480, 640)
+        # logger.debug("ColorFrame self._shape : " + str(self._shape))
+        # self._yuv = np.empty(self._yuv422.size, dtype=np.uint8)
+        # logger.debug("ColorFrame self._yuv.shape : " + str(self._yuv.shape))
+        # (307200,)
+        self._bgr = None
+        self._gray = None
+
+    @property
+    def height(self):
+        return self._shape[0]
+
+    @property
+    def width(self):
+        return self._shape[1]
+
+    @property
+    def yuv_buffer(self):
+        return self._yuv
+
+    @property
+    def yuv422(self):
+        Y = self._yuv[: self._yuv.size // 2]
+        U = self._yuv[self._yuv.size // 2 : 3 * self._yuv.size // 4]
+        V = self._yuv[3 * self._yuv.size // 4 :]
+
+        Y.shape = self._shape
+        U.shape = self._shape[0], self._shape[1] // 2
+        V.shape = self._shape[0], self._shape[1] // 2
+
+        return Y, U, V
+
+    @property
+    def bgr(self):
+        if self._bgr is None:
+            self._bgr = cv2.cvtColor(self._yuv422, cv2.COLOR_YUV2BGR_YUYV)
+        return self._bgr
+
+    @property
+    def img(self):
+        return self.bgr
+
+    @property
+    def gray(self):
+        if self._gray is None:
+            self._gray = self._yuv[: self._yuv.size // 2]
+            self._gray.shape = self._shape
+        return self._gray
+
+
+class DepthFrame(object):
+    def __init__(self, data, timestamp, index):
+        self.timestamp = timestamp
+        self.index = index
+
+        self._bgr = None
+        self._gray = None
+        self.depth = data
+        self.yuv_buffer = None
+
+    @property
+    def height(self):
+        return self.depth.shape[0]
+
+    @property
+    def width(self):
+        return self.depth.shape[1]
+
+    @property
+    def bgr(self):
+        if self._bgr is None:
+            self._bgr = cython_methods.cumhist_color_map16(self.depth)
+        return self._bgr
+
+    @property
+    def img(self):
+        return self.bgr
+
+    @property
+    def gray(self):
+        if self._gray is None:
+            self._gray = cv2.cvtColor(self.bgr, cv2.cv2.COLOR_BGR2GRAY)
+        return self._gray
+
 
 class Realsense2_Source(Base_Source):
     def __init__(
@@ -63,7 +175,7 @@ class Realsense2_Source(Base_Source):
         self._intrinsics = None
         self.color_frame_index = 0
         self.depth_frame_index = 0
-        self.device_id = device_id  # we'll use serial_number for this
+        self.device_id = None  # we'll use serial_number for this
         self.context = rs.context()
         self.pipeline = rs.pipeline(self.context)
         self.pipeline_profile = None
@@ -106,6 +218,7 @@ class Realsense2_Source(Base_Source):
 
         self.last_color_frame_ts = None
         self.last_depth_frame_ts = None
+
         self._recent_frame = None
         self._recent_depth_frame = None
 
@@ -116,7 +229,7 @@ class Realsense2_Source(Base_Source):
             self.update_menu()
             return
 
-        if self.device_id is not None:
+        if self.pipeline_profile:
             try:
                 self.pipeline.stop()  # only call Device.stop() if its context
             except:
@@ -134,8 +247,8 @@ class Realsense2_Source(Base_Source):
         color_fps = 30
         depth_frame_size = (640, 480)
         depth_fps = 30
-        # FIXME shoule be YUV? See class ColorFrame in realsense_backend.py
-        color_format = rs.format.bgr8
+
+        color_format = rs.format.yuyv
         depth_format = rs.format.z16
 
         config = rs.config()
@@ -153,14 +266,20 @@ class Realsense2_Source(Base_Source):
             color_format,
             color_fps,
         )
-
-        self.pipeline_profile = self.pipeline.start(config)
-        self.streams = self.pipeline_profile.get_streams()
-        self.stream_profiles = {
-            s.stream_type(): s for s in self.pipeline_profile.get_streams()
-        }
-        self.update_menu()
-        self._needs_restart = False
+        try:
+            self.pipeline_profile = self.pipeline.start(config)
+        except RuntimeError as re:
+            logger.error("Cannot start pipeline! " + str(re))
+            self.pipeline_profile = None
+        else:
+            self.device_id = device_id
+            self.streams = self.pipeline_profile.get_streams()
+            self.stream_profiles = {
+                s.stream_type(): s for s in self.pipeline_profile.get_streams()
+            }
+            print(self.stream_profiles)
+            self.update_menu()
+            self._needs_restart = False
 
     def _enumerate_formats(self, device_id):
         """Enumerate formats into hierachical structure:
@@ -171,6 +290,8 @@ class Realsense2_Source(Base_Source):
         """
         formats = {}
 
+        # FIXME see output of rs-enumerate-devices (via terminal)
+
         # FIXME
         formats[rs.stream.color] = {(640, 480): [30]}
         formats[rs.stream.depth] = {(640, 480): [30]}
@@ -178,16 +299,89 @@ class Realsense2_Source(Base_Source):
         return formats
 
     def cleanup(self):
-        raise NotImplementedError("cleanup requested")
+        if self.pipeline_profile:
+            self.pipeline.stop()
+        # raise NotImplementedError("cleanup requested")
 
     def get_init_dict(self):
-        raise NotImplementedError("get_init_dict requested")
+        return {"device_id": self.device_id}
+
+    # raise NotImplementedError("get_init_dict requested")
 
     def get_frames(self):
-        raise NotImplementedError("get_frames requested")
+        # FIXME do we really need the check profle?
+        if self.pipeline and self.pipeline_profile:
+            try:
+                frames = self.pipeline.wait_for_frames(TIMEOUT)
+            except RuntimeError as e:
+                logger.error(
+                    "Cannot wait for frames. Is the pipeline running? " + str(e)
+                )
+                return None, None
+            else:
+                current_time = self.g_pool.get_timestamp()
+
+                color = None
+
+                # if we're expecting color frames
+                if rs.stream.color in self.stream_profiles:
+                    color_frame = frames.get_color_frame()
+                    last_color_frame_ts = color_frame.get_timestamp()
+                    if self.last_color_frame_ts != last_color_frame_ts:
+                        self.last_color_frame_ts = last_color_frame_ts
+                        color = ColorFrame(
+                            np.asanyarray(color_frame.get_data()),
+                            current_time,
+                            self.color_frame_index,
+                        )
+                        self.color_frame_index += 1
+
+                depth = None
+
+                # if we're expecting color frames
+                if rs.stream.depth in self.stream_profiles:
+                    depth_frame = frames.get_depth_frame()
+                    last_depth_frame_ts = depth_frame.get_timestamp()
+                    if self.last_depth_frame_ts != last_depth_frame_ts:
+                        self.last_depth_frame_ts = last_depth_frame_ts
+                        depth = DepthFrame(
+                            np.asanyarray(depth_frame.get_data()),
+                            current_time,
+                            self.depth_frame_index,
+                        )
+                        self.depth_frame_index += 1
+
+                return color, depth
+        return None, None
 
     def recent_events(self, events):
-        raise NotImplementedError()
+        if self._needs_restart:
+            logger.debeg("recent_events -> needs restart")
+            self.restart_device()
+            time.sleep(0.05)
+        elif not self.online:
+            logger.debeg("recent_events -> not online!")
+            time.sleep(0.05)
+            return
+
+        try:
+            color_frame, depth_frame = self.get_frames()
+        except Error:  # FIXME what kind of error?
+            logger.warning("Realsense failed to provide frames. Attempting to reinit.")
+            self._recent_frame = None
+            self._recent_depth_frame = None
+            self._needs_restart = True
+        else:
+            if color_frame is not None:
+                self._recent_frame = color_frame
+                events["frame"] = color_frame
+
+            if depth_frame is not None:
+                self._recent_depth_frame = depth_frame
+                events["depth_frame"] = depth_frame
+
+                if self.depth_video_writer is not None:
+                    self.depth_video_writer.write_video_frame(depth_frame)
 
     def deinit_ui(self):
         self.remove_menu()
@@ -325,7 +519,33 @@ class Realsense2_Source(Base_Source):
         self.menu.append(sensor_control)
 
     def gl_display(self):
-        raise NotImplementedError()
+        if self.depth_window is not None and glfw.glfwWindowShouldClose(
+            self.depth_window
+        ):
+            glfw.glfwDestroyWindow(self.depth_window)
+            self.depth_window = None
+
+        if self.preview_depth and self._recent_depth_frame is not None:
+            self.g_pool.image_tex.update_from_ndarray(self._recent_depth_frame.bgr)
+            gl_utils.glFlush()
+            gl_utils.make_coord_system_norm_based()
+            self.g_pool.image_tex.draw()
+        elif self._recent_frame is not None:
+            self.g_pool.image_tex.update_from_yuv_buffer(
+                self._recent_frame.yuv_buffer,
+                self._recent_frame.width,
+                self._recent_frame.height,
+            )
+            gl_utils.glFlush()
+            gl_utils.make_coord_system_norm_based()
+            self.g_pool.image_tex.draw()
+
+        if not self.online:
+            super().gl_display()
+
+        gl_utils.make_coord_system_pixel_based(
+            (self.frame_size[1], self.frame_size[0], 3)
+        )
 
     def restart_device(
         self,
@@ -412,14 +632,13 @@ class Realsense2_Source(Base_Source):
 
     @property
     def frame_size(self):
-        logger.debug("get frame_size")
+        # logger.debug("get frame_size")
         try:
             stream_profile = self.stream_profiles[rs.stream.color]
             stream_profile = stream_profile.as_video_stream_profile()
             return stream_profile.width(), stream_profile.height()
         except AttributeError as a:
             logger.info("Stream profiles are not yet created (color): {}".format(a))
-            return self.frame_size
         except KeyError as k:
             logger.error("Color stream is not found: {}".format(k))
 
