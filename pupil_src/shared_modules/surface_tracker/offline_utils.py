c80891ea7a3ec24fa522831131d85038cada7a90
Pablo Prietz <pablo@prietz.org>
2018-11-30 11:22:50 +0100

    surface_tracker.offline_utils: Convert inner functions to callable class
    Fixes pickling error of inner functions on systems using `spawn`

--- pupil_src/shared_modules/surface_tracker/offline_utils.py
+++ pupil_src/shared_modules/surface_tracker/offline_utils.py
@@ -3,17 +3,21 @@ from .surface import Surface
 from .surface_tracker import Square_Marker_Detection
 
 
-def marker_detection_callable(min_marker_perimeter, inverted_markers):
-    def callable(frame):
+class marker_detection_callable:
+    def __init__(self, min_marker_perimeter, inverted_markers):
+        self.min_marker_perimeter = min_marker_perimeter
+        self.inverted_markers = inverted_markers
+
+    def __call__(self, frame):
         markers = square_marker_detect.detect_markers_robust(
             frame.gray,
             grid_size=5,
             prev_markers=[],
-            min_marker_perimeter=min_marker_perimeter,
+            min_marker_perimeter=self.min_marker_perimeter,
             aperture=9,
             visualize=0,
             true_detect_every_frame=1,
-            invert_image=inverted_markers,
+            invert_image=self.inverted_markers,
         )
         markers = [
             Square_Marker_Detection(
@@ -23,16 +27,20 @@ def marker_detection_callable(min_marker_perimeter, inverted_markers):
         ]
         return markers
 
-    return callable
 
+class surface_locater_callable:
+    def __init__(
+        self, camera_model, registered_markers_undist, registered_markers_dist
+    ):
+        self.camera_model = camera_model
+        self.registered_markers_undist = registered_markers_undist
+        self.registered_markers_dist = registered_markers_dist
 
-def surface_locater_callable(
-    camera_model, registered_markers_undist, registered_markers_dist
-):
-    def callable(markers):
+    def __call__(self, markers):
         markers = {m.id: m for m in markers}
         return Surface.locate(
-            markers, camera_model, registered_markers_undist, registered_markers_dist
+            markers,
+            self.camera_model,
+            self.registered_markers_undist,
+            self.registered_markers_dist,
         )
-
-    return callable
