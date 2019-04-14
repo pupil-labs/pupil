d4d5ad2f1b50cdd6973ce3657b5c551888ced6d7
Pablo Prietz <pablo@prietz.org>
2019-04-14 21:26:02 +0200

    Generic Video Overlay: Extract ui setup functions

--- pupil_src/shared_modules/video_overlay/plugins/generic_overlay.py
+++ pupil_src/shared_modules/video_overlay/plugins/generic_overlay.py
@@ -1,3 +1,4 @@
+import collections
 import os
 
 from plugin import Plugin
@@ -35,28 +36,44 @@ class Vis_Generic_Video_Overlay(Plugin):
 
     def init_ui(self):
         self.add_menu()
-        self._refresh_menu()
-        self.controller.add_observer("attempt_to_load_video", self._refresh_menu)
+        self.refresh_menu()
+        self.controller.add_observer("attempt_to_load_video", self.refresh_menu)
 
     def deinit_ui(self):
-        self.controller.remove_observer("attempt_to_load_video", self._refresh_menu)
+        self.controller.remove_observer("attempt_to_load_video", self.refresh_menu)
         self.remove_menu()
 
-    def _refresh_menu(self, *args, **kwargs):
-
-        if self.controller.valid_video_loaded:
-            menu_elements = generic_overlay_elements(
-                self.controller.video_path, self.controller.config
-            )
-            icon_chr = "O"
-            title = "Video Overlay: {}".format(
-                os.path.basename(self.controller.video_path)
-            )
-        else:
-            menu_elements = no_valid_video_elements()
-            icon_chr = "!"
-            title = "Video Overlay: No valid video loaded"
+    def refresh_menu(self, *args, **kwargs):
+        ui_setup = self._ui_setup()
         # first element corresponds to `Close` button, added in add_menu()
-        self.menu[1:] = menu_elements
-        self.menu_icon.label = icon_chr
-        self.menu.label = title
+        self.menu_icon.label = ui_setup.icon
+        self.menu.label = ui_setup.title
+        self.menu[1:] = ui_setup.menu_elements
+
+    def _ui_setup(self):
+        return (
+            self._ui_setup_if_valid()
+            if self.controller.valid_video_loaded
+            else self._ui_setup_if_not_valid()
+        )
+
+    def _ui_setup_if_valid(self):
+        video_basename = os.path.basename(self.controller.video_path)
+        menu_elements = generic_overlay_elements(
+            self.controller.video_path, self.controller.config
+        )
+        return UISetup(
+            icon="O",
+            title="Video Overlay: {}".format(video_basename),
+            menu_elements=menu_elements,
+        )
+
+    def _ui_setup_if_not_valid(self):
+        return UISetup(
+            icon="!",
+            title="Video Overlay: No valid video loaded",
+            menu_elements=no_valid_video_elements(),
+        )
+
+
+UISetup = collections.namedtuple("UISetup", ("icon", "title", "menu_elements"))
