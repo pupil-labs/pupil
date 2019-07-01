a683fe8a41682dd43e947d2ee92c4a801f63d445
Roman Roibu <romanroibu@gmail.com>
2019-07-01 17:17:26 +0200

    Add surface marker classes for square markers and apriltag markers

--- pupil_src/shared_modules/surface_tracker/surface_marker_detector.py
+++ pupil_src/shared_modules/surface_tracker/surface_marker_detector.py
@@ -1,8 +1,178 @@
 
 import abc
+import typing
+import collections
+
 import square_marker_detect
-from surface_tracker import Square_Marker_Detection
+import apriltag
+
 import stdlib_utils
+
+
+
+
+class Surface_Base_Marker(metaclass=abc.ABCMeta):
+
+    @staticmethod
+    @abc.abstractmethod
+    def from_tuple(state: tuple) -> typing.Optional['Surface_Base_Marker']:
+        pass
+
+    @abc.abstractmethod
+    def to_tuple(self) -> tuple:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def id(self) -> str:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def id_confidence(self) -> float:
+        #TODO: Why is it called "id_confidence" instead of "confidence"?
+        pass
+
+    @property
+    @abc.abstractmethod
+    def verts_px(self) -> list:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def perimeter(self) -> float:
+        #TODO: Is this used/useful outside surface_marker_detector.py? If not - remove
+        pass
+
+
+
+
+# TODO The square marker detection should return an object like this already. Also
+# this object should offer a mean/centroid function to be used when drawing the
+# marker toggle buttons
+class _Square_Marker_Detection(Surface_Base_Marker, collections.namedtuple(
+    "Square_Marker_Detection", ["id", "id_confidence", "verts_px", "perimeter"]
+)):
+    __slots__ = ()
+
+    marker_type = "square" #TODO: Is there a better name?
+
+    @staticmethod
+    def from_tuple(state: tuple) -> typing.Optional['_Square_Marker_Detection']:
+        if state[-1] != _Square_Marker_Detection.marker_type:
+            return None
+        state = state[:-1]
+        return _Square_Marker_Detection(*state)
+
+    def to_tuple(self) -> tuple:
+        state = tuple(self)
+        state = (*state, self.marker_type)
+        return state
+
+
+
+class _Apriltag_V2_Marker_Detection(Surface_Base_Marker, collections.namedtuple(
+    "Apriltag_V2_Marker_Detection",
+    [
+        "tag_family",
+        "tag_id",
+        "hamming",
+        "goodness",
+        "decision_margin",
+        "homography",
+        "center",
+        "corners",
+    ]
+)):
+    __slots__ = ()
+
+    marker_type = "apriltag_v2"
+
+    @staticmethod
+    def from_tuple(state: tuple) -> typing.Optional['_Apriltag_V2_Marker_Detection']:
+        if state[-1] != _Apriltag_V2_Marker_Detection.marker_type:
+            return None
+        state = state[:-1]
+        return _Apriltag_V2_Marker_Detection(*state)
+
+    def to_tuple(self) -> tuple:
+        state = tuple(self)
+        state = (*state, self.marker_type)
+        return state
+
+    @property
+    def id(self) -> str:
+        return self.tag_id
+
+    @property
+    def id_confidence(self) -> float:
+        #TODO: Why is it called "id_confidence" instead of "confidence"?
+        return 0.95 #FIXME
+
+    @property
+    @abc.abstractmethod
+    def verts_px(self) -> list:
+        # Wrapping each point in a list is needed for compatibility with square detector
+        #TODO: See if this wrapping makes sense or if it should be refactored
+        return [[point] for point in self.corners]
+
+    @property
+    @abc.abstractmethod
+    def perimeter(self) -> float:
+        #TODO: Is this used/useful outside surface_marker_detector.py? If not - remove
+        return 80 #FIXME
+
+
+
+
+class Surface_Marker(Surface_Base_Marker):
+
+    def __getstate__(self) -> tuple:
+        return self.to_tuple()
+
+    def __setstate__(self, state: tuple):
+        raw_marker = None
+        raw_marker = raw_marker or _Square_Marker_Detection.from_tuple(state)
+        raw_marker = raw_marker or _Apriltag_V2_Marker_Detection.from_tuple(state)
+        raw_marker = raw_marker or _Square_Marker_Detection(*state) #Legacy
+        assert raw_marker is not None
+        self._raw_marker = raw_marker
+
+    def __init__(self, *args):
+        self.__setstate__(tuple(args))
+
+    @staticmethod
+    def from_tuple(state: tuple) -> 'Surface_Marker':
+        return Surface_Marker(*state)
+
+    def to_tuple(self) -> tuple:
+        return self._raw_marker.to_tuple()
+
+    @property
+    def id(self) -> str:
+        return self._raw_marker.id
+
+    @property
+    def id_confidence(self) -> float:
+        #TODO: Why is it called "id_confidence" instead of "confidence"?
+        return self._raw_marker.id_confidence
+
+    @property
+    def verts_px(self) -> list:
+        return self._raw_marker.verts_px
+
+    @property
+    def perimeter(self) -> float:
+        #TODO: Is this used/useful outside surface_marker_detector.py? If not - remove
+        return self._raw_marker.perimeter
+
+
+
+
+
+
+
+
 
 
 class Surface_Base_Marker_Detector(metaclass=abc.ABCMeta):
