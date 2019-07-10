0db55c2d02c77de40beeee0e166390aebdf39efa
Roman Roibu <romanroibu@gmail.com>
2019-07-10 12:13:23 +0200

    Redefine marker_detection_callable in terms of Surface_Marker_Detector

--- pupil_src/shared_modules/surface_tracker/offline_utils.py
+++ pupil_src/shared_modules/surface_tracker/offline_utils.py
@@ -1,29 +1,22 @@
 import square_marker_detect
 from .surface import Surface
-from .surface_marker_detector import Surface_Marker
+from .surface_marker_detector import Surface_Marker, Surface_Marker_Detector
 
 
-# TODO: Refactor clients of this to use Surface_Marker_Detector instances
-class marker_detection_callable:
+class marker_detection_callable(Surface_Marker_Detector):
+
     def __init__(self, min_marker_perimeter, inverted_markers):
-        self.min_marker_perimeter = min_marker_perimeter
-        self.inverted_markers = inverted_markers
+        super().__init__(
+            square_marker_min_perimeter=min_marker_perimeter,
+            square_marker_inverted_markers=inverted_markers,
+        )
 
     def __call__(self, frame):
-        markers = square_marker_detect.detect_markers_robust(
-            frame.gray,
-            grid_size=5,
-            prev_markers=[],
-            min_marker_perimeter=self.min_marker_perimeter,
-            aperture=9,
-            visualize=0,
-            true_detect_every_frame=1,
-            invert_image=self.inverted_markers,
-        )
-        return [ Surface_Marker.from_square_tag_detection(m) for m in markers]
+        return self.detect_markers(gray_img=frame.gray)
 
 
 class surface_locater_callable:
+
     def __init__(
         self, camera_model, registered_markers_undist, registered_markers_dist
     ):
