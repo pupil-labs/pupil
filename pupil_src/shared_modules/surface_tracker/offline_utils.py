0acaba93ea5d011c20ab7cedb1425825f94e282a
Roman Roibu <romanroibu@gmail.com>
2019-07-15 16:39:23 +0200

    Update copyright headers inside surface_tracker package

--- pupil_src/shared_modules/surface_tracker/offline_utils.py
+++ pupil_src/shared_modules/surface_tracker/offline_utils.py
@@ -1,3 +1,14 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2019 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
 from .surface import Surface
 from .surface_marker_detector import Surface_Marker_Detector
 
