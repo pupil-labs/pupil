d0a561353544c19f7d3de13c8bff4ab6c84c67fb
Roman Roibu <romanroibu@gmail.com>
2019-08-22 13:32:02 +0200

    Unify plugin name for head pose tracker plugins

---
+++ pupil_src/shared_modules/head_pose_tracker/base_head_pose_tracker.py
@@ -0,0 +1,23 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2019 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
+from observable import Observable
+from plugin import Plugin
+
+
+class Head_Pose_Tracker_Base(Plugin, Observable):
+
+    icon_chr = chr(0xEC07)
+    icon_font = "pupil_icons"
+
+    @classmethod
+    def parse_pretty_class_name(cls) -> str:
+        return "Head Pose Tracker"
