54ede413a49553366347e341da80892a41bc0a1d
Roman Roibu <romanroibu@gmail.com>
2019-08-23 10:26:57 +0200

    Transform background_helper module into a package

---
+++ pupil_src/shared_modules/background_helper/__init__.py
@@ -0,0 +1,13 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2019 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
+# Keep source compatibility
+from .multiprocessing import EarlyCancellationError, Task_Proxy, IPC_Logging_Task_Proxy
