a4c789ce82779de2b0491c0ba3de02252c0fd306
Patrick Faion <pfa@pupil-labs.com>
2019-09-05 11:10:47 +0200

    Pass fake frame flag to image manipulation pipeline in overlay

--- pupil_src/shared_modules/video_overlay/workers/overlay_renderer.py
+++ pupil_src/shared_modules/video_overlay/workers/overlay_renderer.py
@@ -39,8 +39,21 @@ class OverlayRenderer:
             return
         overlay_frame = self.video.closest_frame_to_ts(target_frame.timestamp)
         overlay_image = overlay_frame.img
+        try:
+            is_fake_frame = overlay_frame.is_fake
+        except AttributeError:
+            # TODO: this is only fore extra safety until we have a unified Frame class
+            # and can be sure that everything ending up here has the 'is_fake'
+            # attribute!
+            logger.debug(
+                f"Frame passed to overlay renderer does not have 'is_fake' attribute!"
+                f" Frame: {overlay_frame}"
+            )
+            is_fake_frame = False
         for param, manipulation in self.pipeline:
-            overlay_image = manipulation.apply_to(overlay_image, param.value)
+            overlay_image = manipulation.apply_to(
+                overlay_image, param.value, is_fake_frame=overlay_frame.is_fake
+            )
 
         self._adjust_origin_constraint(target_frame.img, overlay_image)
         self._render_overlay(target_frame.img, overlay_image)
