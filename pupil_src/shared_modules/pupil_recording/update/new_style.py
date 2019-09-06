88159f82bcacda767f4b7f53ae8c5c9139115e82
Pablo Prietz <pablo@prietz.org>
2019-09-06 16:59:43 +0200

    Check minimal player version

--- pupil_src/shared_modules/pupil_recording/update/new_style.py
+++ pupil_src/shared_modules/pupil_recording/update/new_style.py
@@ -1,2 +1,17 @@
+from pupil_recording import RecordingInfoFile, Version, InvalidRecordingException
+from version_utils import pupil_version
+
+
 def recording_update_to_latest_new_style(rec_dir: str):
-    pass
+    info_file = RecordingInfoFile.read_file_from_recording(rec_dir)
+
+    if info_file.min_player_version > Version(pupil_version()):
+        player_out_of_date = (
+            "Recording requires a newer version of Player: "
+            f"{info_file.min_player_version}"
+        )
+        raise InvalidRecordingException(reason=player_out_of_date)
+
+    # if info_file.min_player_version < Version("1.17"):
+    #     ... incremental update ...
+    #     Increases min_player_version
