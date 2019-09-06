e3275464c4a96e6995176cd608e12fd14aef3f5f
Patrick Faion <pfa@pupil-labs.com>
2019-09-06 14:21:20 +0200

    Extract RecordingType logic

--- pupil_src/shared_modules/pupil_recording/recording.py
+++ pupil_src/shared_modules/pupil_recording/recording.py
@@ -10,7 +10,12 @@ import csv_utils
 from video_capture.utils import VIDEO_EXTS as VALID_VIDEO_EXTENSIONS
 
 
-from .info.recording_info import RecordingInfo, RecordingInfoFile, RecordingInfoInvalidError, RecordingVersion
+from .info.recording_info import (
+    RecordingInfo,
+    RecordingInfoFile,
+    RecordingInfoInvalidError,
+    RecordingVersion,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -25,6 +30,72 @@ class InvalidRecordingException(Exception):
 
     def __str__(self):
         return f"{type(self).__name__}: {super().__str__()}"
+
+
+class RecordingType(enum.Enum):
+    MOBILE = enum.auto()
+    INVISIBLE = enum.auto()
+    OLD_STYLE = enum.auto()
+    NEW_STYLE = enum.auto()
+
+
+def get_recording_type(rec_dir: str) -> RecordingType:
+    _assert_valid_rec_dir()
+
+    if RecordingInfoFile.does_recording_contain_info_file(rec_dir):
+        return RecordingType.NEW_STYLE
+
+    elif was_recording_opened_in_player_before(rec_dir):
+        return RecordingType.OLD_STYLE
+
+    elif is_pupil_invisible_recording(rec_dir):
+        return RecordingType.INVISIBLE
+
+    elif is_pupil_mobile_recording(rec_dir):
+        return RecordingType.MOBILE
+
+    raise InvalidRecordingException(
+        reason=f"There is no info file in the target directory.", recovery=""
+    )
+
+
+def _assert_valid_rec_dir(rec_dir: str):
+    rec_dir = Path(rec_dir).resolve()
+
+    def normalize_extension(ext: str) -> str:
+        if ext.startswith("."):
+            ext = ext[1:]
+        return ext
+
+    def is_video_file(file_path: Path):
+        if not file_path.is_file():
+            return False
+        ext = file_path.suffix
+        ext = normalize_extension(ext)
+        valid_video_extensions = map(normalize_extension, VALID_VIDEO_EXTENSIONS)
+        if ext not in valid_video_extensions:
+            return False
+        return True
+
+    if not rec_dir.exists():
+        raise InvalidRecordingException(
+            reason=f"Target at path does not exist: {rec_dir}", recovery=""
+        )
+
+    if not rec_dir.is_dir():
+        if is_video_file(rec_dir):
+            raise InvalidRecordingException(
+                reason=f"The provided path is a video, not a recording directory",
+                recovery="Please provide a recording directory",
+            )
+        else:
+            raise InvalidRecordingException(
+                reason=f"Target at path is not a directory: {rec_dir}", recovery=""
+            )
+
+
+def assert_valid_recording_type(rec_dir: str):
+    assert get_recording_type(rec_dir) in RecordingType
 
 
 class PupilRecording(RecordingInfo):
@@ -169,46 +240,17 @@ class PupilRecording(RecordingInfo):
     def load(self, rec_dir):
         rec_dir = Path(rec_dir).resolve()
 
-        def normalize_extension(ext: str) -> str:
-            if ext.startswith("."):
-                ext = ext[1:]
-            return ext
-
-        def is_video_file(file_path):
-            if not file_path.is_file():
-                return False
-            ext = file_path.suffix
-            ext = normalize_extension(ext)
-            valid_video_extensions = map(normalize_extension, VALID_VIDEO_EXTENSIONS)
-            if ext not in valid_video_extensions:
-                return False
-            return True
-
-        if not rec_dir.exists():
-            raise InvalidRecordingException(
-                reason=f"Target at path does not exist: {rec_dir}", recovery=""
-            )
-
-        if not rec_dir.is_dir():
-            if is_video_file(rec_dir):
-                raise InvalidRecordingException(
-                    reason=f"The provided path is a video, not a recording directory",
-                    recovery="Please provide a recording directory",
-                )
-            else:
-                raise InvalidRecordingException(
-                    reason=f"Target at path is not a directory: {rec_dir}", recovery=""
-                )
+        _assert_valid_rec_dir(rec_dir)
 
         try:
             info_file = RecordingInfoFile.read_file_from_recording(rec_dir=rec_dir)
         except FileNotFoundError:
             raise InvalidRecordingException(
                 reason=f"There is no {RecordingInfoFile.file_name} in the target directory",
-                recovery=""
+                recovery="",
             )
         except RecordingInfoInvalidError as err:
-            raise  InvalidRecordingException(f"{err}")
+            raise InvalidRecordingException(f"{err}")
 
         self._info_file = info_file
 
