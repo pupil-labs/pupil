5af11bc06e3606dbf5262f1b8b3d4f2d88312dfe
Patrick Faion <pfa@pupil-labs.com>
2019-09-11 14:03:08 +0200

    Add comment for getting rid of is_fake_frame parameter in the future

--- pupil_src/shared_modules/video_overlay/workers/overlay_renderer.py
+++ pupil_src/shared_modules/video_overlay/workers/overlay_renderer.py
@@ -41,6 +41,9 @@ class OverlayRenderer:
         overlay_image = overlay_frame.img
         try:
             is_fake_frame = overlay_frame.is_fake
+            # TODO: once we have the unified Frame class, we should just pass the frames
+            # to the image manipulation pipeline (instead of the images). Then we can
+            # get rid of the additional parameter stuff in ImageManipulation!
         except AttributeError:
             # TODO: this is only fore extra safety until we have a unified Frame class
             # and can be sure that everything ending up here has the 'is_fake'
