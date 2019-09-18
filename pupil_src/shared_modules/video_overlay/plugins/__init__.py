4b954104f0d04aad5c5d5d0d1576aa9f3fe42c4d
Patrick Faion <pfa@pupil-labs.com>
2019-09-18 16:20:33 +0200

    Add other missing license headers

--- pupil_src/shared_modules/video_overlay/plugins/__init__.py
+++ pupil_src/shared_modules/video_overlay/plugins/__init__.py
@@ -1,2 +1,13 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2019 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
 from video_overlay.plugins.generic_overlay import Video_Overlay
 from video_overlay.plugins.eye_overlay import Eye_Overlay
