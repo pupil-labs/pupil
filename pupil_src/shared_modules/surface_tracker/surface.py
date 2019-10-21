d9a421f392ba6e8545961d5e9c6d89fc93173aaf
Patrick Faion <pfa@pupil-labs.com>
2019-10-21 15:46:37 +0200

    Fix mismatch when dragging surface corners
    See the #NOTE in the code on why this did not work before.

--- pupil_src/shared_modules/surface_tracker/surface.py
+++ pupil_src/shared_modules/surface_tracker/surface.py
@@ -333,9 +333,29 @@ class Surface(abc.ABC):
         points_A = points_A.reshape((-1, 1, 2))
         points_B = points_B.reshape((-1, 1, 2))
 
-        B_to_A, mask = cv2.findHomography(
-            points_A, points_B, method=cv2.RANSAC, ransacReprojThreshold=100
-        )
+        B_to_A, mask = cv2.findHomography(points_A, points_B)
+        # NOTE: cv2.findHomography(A, B) will not produce the inverse of
+        # cv2.findHomography(B, A)! The errors can actually be quite large, resulting in
+        # on-screen discrepancies of up to 50 pixels. We try to find the inverse
+        # analytically instead with fallbacks.
+
+        try:
+            A_to_B = np.linalg.inv(B_to_A)
+            return A_to_B, B_to_A
+        except np.linalg.LinAlgError as e:
+            logger.debug(
+                "Failed to calculate inverse homography with np.inv()! "
+                "Trying with np.pinv() instead."
+            )
+
+        try:
+            A_to_B = np.linalg.pinv(B_to_A)
+            return A_to_B, B_to_A
+        except np.linalg.LinAlgError as e:
+            logger.warning(
+                "Failed to calculate inverse homography with np.pinv()! "
+                "Falling back to inaccurate manual computation!"
+            )
 
         A_to_B, mask = cv2.findHomography(points_B, points_A)
         return A_to_B, B_to_A
