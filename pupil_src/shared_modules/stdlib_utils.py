5b81d4b29aa784a5f943426a984fdf3e641c6923
Roman Roibu <romanroibu@gmail.com>
2019-11-05 16:53:20 +0100

    Move stdlib_utils tests to pupil_src/tests/test_stdlib_utils.py

--- pupil_src/shared_modules/stdlib_utils.py
+++ pupil_src/shared_modules/stdlib_utils.py
@@ -62,29 +62,3 @@ class unique(collections.abc.Iterable):
                 by_key[key] = self._select(old_elem, new_elem)
 
         return iter(by_key.values())
-
-
-def test_unique():
-    arr = [1, 2, 3, 1, 4, 2, 2, 4, 5, 6, 0]
-    assert list(unique(arr)) == [1, 2, 3, 4, 5, 6, 0]
-    assert list(unique(arr)) == list(unique(unique(arr)))
-
-    assert list(unique(arr, key=lambda _: 0)) == [1]
-    assert list(unique(arr, key=lambda _: 0, select=lambda old, new: new)) == [0]
-    assert list(unique(arr, select=lambda old, new: old*10 + new)) == [11, 222, 3, 44, 5, 6, 0]
-
-
-def test_operators():
-
-    assert is_none(None) == True
-    assert is_not_none(None) == False
-
-    things = [0, 5, [], [1,2,3], "", "abc"]
-
-    for thing in things:
-        assert is_none(thing) == False
-        assert is_not_none(thing) == True
-
-
-if __name__ == '__main__':
-    test_unique()
