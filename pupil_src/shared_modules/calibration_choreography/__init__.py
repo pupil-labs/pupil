66201b9076951dfdc0dfa3ec795aac2be920dddf
Roman Roibu <romanroibu@gmail.com>
2020-02-03 17:32:44 +0100

    Import default plugin and available plugins from calibration_choreography module

--- pupil_src/shared_modules/calibration_choreography/__init__.py
+++ pupil_src/shared_modules/calibration_choreography/__init__.py
@@ -1,1 +1,10 @@
-from .base_plugin import CalibrationChoreographyPlugin, register_calibration_choreography_plugin
+from .base_plugin import CalibrationChoreographyPlugin
+from .screen_marker_plugin import ScreenMarkerChoreographyPlugin
+# from .single_marker_plugin import SingleMarkerChoreographyPlugin
+# from .hmd_plugin import HMDChoreographyPlugin
+
+
+def available_calibration_choreography_plugins():
+    default_plugin = ScreenMarkerChoreographyPlugin
+    available_plugins = list(CalibrationChoreographyPlugin.registered_choreographies().values())
+    return default_plugin, available_plugins
