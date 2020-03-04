329aa174e6007810534ab8b419b6b2ad9be3a31a
Roman Roibu <romanroibu@gmail.com>
2020-03-04 14:52:17 +0100

    Define _FakeGpool for create_calibration.py to stub out calls to g_pool.ipc_pub.notify in gazer

--- pupil_src/shared_modules/gaze_producer/worker/create_calibration.py
+++ pupil_src/shared_modules/gaze_producer/worker/create_calibration.py
@@ -41,7 +41,7 @@ def create_task(calibration, all_reference_locations):
         if frame_start <= ref.frame_index <= frame_end
     ]
 
-    fake_gpool = _setup_fake_gpool(
+    fake_gpool = _FakeGpool(
         g_pool.capture.frame_size,
         g_pool.capture.intrinsics,
         g_pool.rec_dir,
@@ -66,17 +66,25 @@ def _create_ref_dict(ref):
     }
 
 
-def _setup_fake_gpool(frame_size, intrinsics, rec_dir, min_calibration_confidence):
-    cap = SimpleNamespace()
-    cap.frame_size = frame_size
-    cap.intrinsics = intrinsics
-    pool = SimpleNamespace()
-    pool.capture = cap
-    pool.get_timestamp = time
-    pool.min_calibration_confidence = min_calibration_confidence
-    pool.rec_dir = rec_dir
-    pool.app = "player"
-    return pool
+class _FakeGpool(SimpleNamespace):
+
+    class _FakeIPC(SimpleNamespace):
+        def notify(self, notification, *args, **kwargs):
+            name = notification.get("subject", None) or notification.get("topic", None)
+            logger.debug(f'Received background notification "{name}"; it will be ignored.')
+
+    def __init__(self, frame_size, intrinsics, rec_dir, min_calibration_confidence):
+        cap = SimpleNamespace()
+        cap.frame_size = frame_size
+        cap.intrinsics = intrinsics
+
+        self.capture = cap
+        self.get_timestamp = time
+        self.min_calibration_confidence = min_calibration_confidence
+        self.rec_dir = rec_dir
+        self.app = "player"
+        self.ipc_pub = _FakeGpool._FakeIPC()
+
 
 
 def _create_calibration(
