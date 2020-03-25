aaa6e685cad98581582775f355a44cf60ac362ef
Pablo Prietz <pablo@prietz.org>
2020-03-25 18:18:08 +0100

    Introduce InvalidContainerError

--- pupil_src/shared_modules/video_capture/utils.py
+++ pupil_src/shared_modules/video_capture/utils.py
@@ -169,6 +169,10 @@ class Check_Frame_Stripes(object):
             return None
 
 
+class InvalidContainerError(RuntimeError):
+    pass
+
+
 class Video:
     def __init__(self, path: str) -> None:
         self.path = path
@@ -179,26 +183,30 @@ class Video:
     @property
     def is_valid(self):
         if self._is_valid is None:
-            self._is_valid = self.load_valid_container() is not None
+            try:
+                self.load_container()
+                self._is_valid = True
+            except InvalidContainerError:
+                self._is_valid = False
         return self._is_valid
 
-    def load_valid_container(self):
+    def load_container(self):
         try:
-            cont = self.attempt_open_container()
+            cont = self._open_container()
             # Three failure scenarios:
             # 1. Broken video -> AVError
             # 2. decode() does not yield anything
             # 3. decode() yields None
             first_frame = next(cont.decode(video=0), None)
             if first_frame is None:
-                return None  # container does not contain frames
-        except av.AVError:
-            return None
+                raise InvalidContainerError("Container does not contain frames")
+        except av.AVError as averr:
+            raise InvalidContainerError from averr
         else:
             cont.seek(0)
             return cont
 
-    def attempt_open_container(self):
+    def _open_container(self):
         cont = av.open(self.path)
         return cont
 
@@ -210,9 +218,7 @@ class Video:
             return
         self.ts = self._fix_negative_time_jumps(self.ts)
 
-    def load_pts(self, container=None):
-        if container is None:
-            container = self.attempt_open_container()
+    def load_pts(self, container):
         packets = container.demux(video=0)
         # last pts is invalid
         self._pts = np.array([packet.pts for packet in packets][:-1])
@@ -289,7 +295,7 @@ class VideoSet:
         return self._videos
 
     def get_container(self, index) -> T.Optional[av.container.input.InputContainer]:
-        return self.videos[index].load_valid_container()
+        return self.videos[index].load_container()
 
     @property
     def lookup_loc(self) -> str:
@@ -344,12 +350,9 @@ class VideoSet:
 
         lookup = self._setup_lookup(loaded_ts)
         for container_idx, vid in enumerate(self.videos):
-            container = vid.load_valid_container()
-            if container is None:
-                # For invalid videos, we still try to load the timestamps (might be empty)
-                lookup_mask = np.isin(lookup.timestamp, vid.timestamps)
-                lookup.container_frame_idx[lookup_mask] = np.arange(vid.timestamps.size)
-            else:
+            try:
+                container = vid.load_container()
+
                 # NOTE: For unknown reasons we sometimes have more timestamps than
                 # frames. We don't know how to match non-matching timestamps and
                 # pts, so we might introduce a systematic bias when fixing this! The
@@ -376,6 +379,12 @@ class VideoSet:
                 lookup.container_frame_idx[lookup_mask] = np.arange(vid_timestamps.size)
                 lookup.container_idx[lookup_mask] = container_idx
                 lookup.pts[lookup_mask] = vid_pts
+
+            except InvalidContainerError:
+                # For invalid videos, we still try to load the timestamps (might be empty)
+                lookup_mask = np.isin(lookup.timestamp, vid.timestamps)
+                lookup.container_frame_idx[lookup_mask] = np.arange(vid.timestamps.size)
+
         self.lookup = lookup
         np.save(self.lookup_loc, self.lookup)
         # filter gaps (after saving!)
