d2516d29bca3704044759d84d0e9c09a2c474a34
Pablo Prietz <pablo@prietz.org>
2020-04-20 18:19:13 +0200

    Fix blink detection: Prefer 2d data, fallback to 3d if not available

--- pupil_src/shared_modules/blink_detection.py
+++ pupil_src/shared_modules/blink_detection.py
@@ -230,6 +230,13 @@ class Offline_Blink_Detection(Observable, Blink_Detection):
             "on_data_changed", self._on_pupil_positions_changed
         )
 
+    def _pupil_data(self):
+        data = self.g_pool.pupil_positions[:, "2d"]
+        if not data:
+            # Fall back to 3d data
+            data = self.g_pool.pupil_positions[:, "3d"]
+        return data
+
     def init_ui(self):
         super().init_ui()
         self.glfont = fs.Context()
@@ -330,7 +337,7 @@ class Offline_Blink_Detection(Observable, Blink_Detection):
         import time
 
         t0 = time.time()
-        all_pp = self.g_pool.pupil_positions
+        all_pp = self._pupil_data()
         if not all_pp:
             self.filter_response = []
             self.response_classification = []
@@ -381,6 +388,9 @@ class Offline_Blink_Detection(Observable, Blink_Detection):
         blink_stop_ts = deque()
         counter = 1
 
+        # NOTE: Cache result for performance reasons
+        pupil_data = self._pupil_data()
+
         def start_blink(idx):
             nonlocal blink
             nonlocal state
@@ -404,7 +414,7 @@ class Offline_Blink_Detection(Observable, Blink_Detection):
             blink["end_timestamp"] = self.timestamps[idx]
             blink["timestamp"] = (blink["end_timestamp"] + blink["start_timestamp"]) / 2
             blink["duration"] = blink["end_timestamp"] - blink["start_timestamp"]
-            blink["base_data"] = self.g_pool.pupil_positions[start_idx:idx]
+            blink["base_data"] = pupil_data[start_idx:idx]
             blink["filter_response"] = self.filter_response[start_idx:idx].tolist()
             # blink confidence is the mean of the absolute filter response
             # during the blink event, clamped at 1.
