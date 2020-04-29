773dc7f69eba6c84798f41b83585c96cee96b041
Pablo Prietz <pablo@prietz.org>
2020-04-29 14:56:01 +0200

    Convert ndarray to list before serializing

--- pupil_src/shared_modules/blink_detection.py
+++ pupil_src/shared_modules/blink_detection.py
@@ -414,7 +414,7 @@ class Offline_Blink_Detection(Observable, Blink_Detection):
             blink["end_timestamp"] = self.timestamps[idx]
             blink["timestamp"] = (blink["end_timestamp"] + blink["start_timestamp"]) / 2
             blink["duration"] = blink["end_timestamp"] - blink["start_timestamp"]
-            blink["base_data"] = pupil_data[start_idx:idx]
+            blink["base_data"] = pupil_data[start_idx:idx].tolist()
             blink["filter_response"] = self.filter_response[start_idx:idx].tolist()
             # blink confidence is the mean of the absolute filter response
             # during the blink event, clamped at 1.
