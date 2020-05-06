80c3f9a643314a0e2a0f970b2ae3ad47b49c3cf8
Roman Roibu <romanroibu@gmail.com>
2020-05-06 09:22:40 +0200

    Replace Gaze_Mapping_Plugin with GazerBase in Plugin_Manager

--- pupil_src/shared_modules/plugin_manager.py
+++ pupil_src/shared_modules/plugin_manager.py
@@ -11,8 +11,8 @@ See COPYING and COPYING.LESSER for license details.
 
 from plugin import System_Plugin_Base, Producer_Plugin_Base
 from pyglui import ui
-from calibration_routines import Gaze_Mapping_Plugin  # TODO: Replace with GazerPlugin from gaze_mapping
 from calibration_choreography import CalibrationChoreographyPlugin
+from gaze_mapping.gazer_base import GazerBase
 from video_capture import Base_Manager, Base_Source
 
 
@@ -27,7 +27,7 @@ class Plugin_Manager(System_Plugin_Base):
             Base_Manager,
             Base_Source,
             CalibrationChoreographyPlugin,
-            Gaze_Mapping_Plugin,
+            GazerBase,
             Producer_Plugin_Base,
         )
         self.user_plugins = [
