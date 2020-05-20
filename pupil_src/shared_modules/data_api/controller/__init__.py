5d5df29a488b1061eb9ea4ae7a05536029fdbf33
Roman Roibu <romanroibu@gmail.com>
2020-05-20 10:01:33 +0200

    Expose public classes in controller module

---
+++ pupil_src/shared_modules/data_api/controller/__init__.py
@@ -0,0 +1,12 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2020 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+from .frame_publisher_controller import FramePublisherController
+from .pupil_remote_controller import PupilRemoteController
