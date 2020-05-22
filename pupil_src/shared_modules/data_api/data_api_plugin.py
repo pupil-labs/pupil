50c84446a122c4459f9c9a8011c2ec6156a97b3d
Roman Roibu <romanroibu@gmail.com>
2020-05-22 08:35:22 +0200

    Add pretty plugin name to DataApiPlugin

--- pupil_src/shared_modules/data_api/data_api_plugin.py
+++ pupil_src/shared_modules/data_api/data_api_plugin.py
@@ -29,6 +29,10 @@ class DataApiPlugin(Plugin):
     icon_font = "pupil_icons"
     order = 0.01  # excecute first
 
+    @property
+    def pretty_class_name(self):
+        return self.menu_label
+
     def __init__(self, g_pool, **kwargs):
         super().__init__(g_pool)
         self.__frame_publisher = FramePublisherController(**kwargs)
