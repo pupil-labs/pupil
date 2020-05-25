e4f9384006a0a3debb042e14bd90298470be24d4
Patrick Faion <p.faion@web.de>
2020-05-25 17:46:49 +0200

    Add recording_info version 2.2

--- pupil_src/shared_modules/pupil_recording/info/__init__.py
+++ pupil_src/shared_modules/pupil_recording/info/__init__.py
@@ -13,6 +13,8 @@ from .. import Version
 from .recording_info import RecordingInfoFile
 from .recording_info_2_0 import _RecordingInfoFile_2_0
 from .recording_info_2_1 import _RecordingInfoFile_2_1
+from .recording_info_2_2 import _RecordingInfoFile_2_2
 
 RecordingInfoFile.register_child_class(Version("2.0"), _RecordingInfoFile_2_0)
 RecordingInfoFile.register_child_class(Version("2.1"), _RecordingInfoFile_2_1)
+RecordingInfoFile.register_child_class(Version("2.2"), _RecordingInfoFile_2_2)
---
+++ pupil_src/shared_modules/pupil_recording/info/recording_info_2_2.py
@@ -0,0 +1,36 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2020 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
+from . import RecordingInfoFile, Version
+from .recording_info_2_0 import _RecordingInfoFile_2_0
+from . import recording_info_utils as utils
+
+
+class _RecordingInfoFile_2_2(_RecordingInfoFile_2_0):
+
+    # Used to make Pupil v2.0 recordings backwards incompatible with v1.*
+
+    @property
+    def meta_version(self) -> Version:
+        return Version("2.2")
+
+    @property
+    def min_player_version(self) -> Version:
+        return Version("2.0")
+
+    @property
+    def _private_key_schema(self) -> RecordingInfoFile._KeyValueSchema:
+        return {
+            **super()._private_key_schema,
+            # overwrite meta_version key from parent
+            "meta_version": (utils.validator_version_string, lambda _: "2.2"),
+            "min_player_version": (utils.validator_version_string, lambda _: "2.0"),
+        }
