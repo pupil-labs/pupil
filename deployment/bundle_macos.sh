dfed72e7c3fb443c70c1de106ec09dae94a67cea
Pablo Prietz <pablo@prietz.org>
2020-06-03 17:29:20 +0200

    Extract macos bundle script

--- deployment/bundle_macos.sh
+++ deployment/bundle_macos.sh
@@ -2,41 +2,34 @@
 
 # get most major.minor tag, without trailing count
 current_tag=$(git describe --tags --long)
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    release_dir=$(echo "pupil_${current_tag}_linux_x64")
-    ext=*.deb
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    release_dir=$(echo "pupil_${current_tag}_macos_x64")
-    ext=*.NOTANEXTENSION
-fi
+release_dir=$(echo "pupil_${current_tag}_macos_x64")
 echo "release_dir:  ${release_dir}"
 mkdir ${release_dir}
+
+ext=app
 
 # bundle Pupil Capture
 printf "\n##########\nBundling Pupil Capture\n##########\n\n"
 cd deploy_capture
 ./bundle.sh
-mv *.$ext ../$release_dir
+mv dist/*.$ext ../$release_dir
+cd ..
 
 # bundle Pupil Service
 printf "\n##########\nBundling Pupil Service\n##########\n\n"
-cd ../deploy_service
+cd deploy_service
 ./bundle.sh
-mv *.$ext ../$release_dir
+mv dist/*.$ext ../$release_dir
+cd ..
 
 # bundle Pupil Player
 printf "\n##########\nBundling Pupil Player\n##########\n\n"
-cd ../deploy_player
+cd deploy_player
 ./bundle.sh
-mv *.$ext ../$release_dir
-
+mv dist/*.$ext ../$release_dir
 cd ..
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    printf "\n########## Moving applications into $release_dir"
-    mv **/*.app $release_dir
-
     printf "\n########## Creating dmg file"
     ln -s /Applications/ $release_dir/Applications
     dir_size = $(du -m -d 0 $release_dir | cut -f1)
