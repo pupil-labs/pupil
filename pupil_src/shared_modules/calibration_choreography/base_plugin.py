642dd07c58f5584f09bf5eff1a983f422f30b54e
Roman Roibu <romanroibu@gmail.com>
2020-06-03 16:48:59 +0200

    Rename Gazer selection dropdown to Method

--- pupil_src/shared_modules/calibration_choreography/base_plugin.py
+++ pupil_src/shared_modules/calibration_choreography/base_plugin.py
@@ -349,7 +349,7 @@ class CalibrationChoreographyPlugin(Plugin):
         self.__ui_selector_gazer = ui.Selector(
             "selected_gazer_class",
             self,
-            label="Gazer",
+            label="Method",
             labels=[g.label for g in self.user_selectable_gazer_classes()],
             selection=self.user_selectable_gazer_classes(),
         )
