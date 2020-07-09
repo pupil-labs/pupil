89b1251623c0591539979fe2cde21730b955302a
Pablo Prietz <pablo@prietz.org>
2020-07-09 14:46:19 +0200

    Add `os_utils.patch_pyre_zhelper_cdll()`
    Fixes https://github.com/pupil-labs/pupil/issues/1919
    
    When running the v2.0 bundle on macOS 10.14, `ctypes.CDLL("libSystem.dylib")` fails
    to load which is required by pyre.zhelper. `libSystem.dylib` is not part of the
    bundle on purpose, as `ctypes.CDLL` is usually able to fallback to the system-
    provided library at `/usr/lib/libSystem.dylib`.
    
    The fallback mechanism is provided by the $DYLD_FALLBACK_LIBRARY_PATH variable.
    PyInstaller makes use of it to inject the bundle path to prioritize bundled
    libraries over system libraries:
    https://github.com/pyinstaller/pyinstaller/blob/v3.6/PyInstaller/loader/pyiboot01_bootstrap.py#L203
    
    Upon inspection, `/usr/lib` is correcly present in $DYLD_FALLBACK_LIBRARY_PATH when
    running the macOS bundle on macOS 10.14. This indicates an underlying deeper issue.
    
    Curiously, `ctypes.util.find_library("libSystem.dylib")` works as expected and
    returns `/usr/lib/libSystem.dylib` when running the above setup. This patch makes
    use of that fact by implementing an additional fallback that attempts to load the
    initially missing library based on its absolute path.
    
    As `pyre.zhelper` is the only place calling `ctypes.CDLL("libSystem.dylib")`, we
    will only patch this specific CDLL instance.
    
    This patch will only be applied if the problematic setup is detected.

--- pupil_src/shared_modules/os_utils.py
+++ pupil_src/shared_modules/os_utils.py
@@ -58,6 +58,53 @@ else:
             return etype is KeyboardInterrupt
 
 
+def patch_pyre_zhelper_cdll():
+    """Fixes https://github.com/pupil-labs/pupil/issues/1919
+    
+    When running the v2.0 bundle on macOS 10.14, `ctypes.CDLL("libSystem.dylib")` fails
+    to load which is required by pyre.zhelper. `libSystem.dylib` is not part of the
+    bundle on purpose, as `ctypes.CDLL` is usually able to fallback to the system-
+    provided library at `/usr/lib/libSystem.dylib`.
+
+    The fallback mechanism is provided by the $DYLD_FALLBACK_LIBRARY_PATH variable.
+    PyInstaller makes use of it to inject the bundle path to prioritize bundled
+    libraries over system libraries:
+    https://github.com/pyinstaller/pyinstaller/blob/v3.6/PyInstaller/loader/pyiboot01_bootstrap.py#L203
+
+    Upon inspection, `/usr/lib` is correcly present in $DYLD_FALLBACK_LIBRARY_PATH when
+    running the macOS bundle on macOS 10.14. This indicates an underlying deeper issue.
+
+    Curiously, `ctypes.util.find_library("libSystem.dylib")` works as expected and
+    returns `/usr/lib/libSystem.dylib` when running the above setup. This patch makes
+    use of that fact by implementing an additional fallback that attempts to load the
+    initially missing library based on its absolute path.
+
+    As `pyre.zhelper` is the only place calling `ctypes.CDLL("libSystem.dylib")`, we
+    will only patch this specific CDLL instance.
+
+    This patch will only be applied if the problematic setup is detected.
+    """
+    running_from_bundle = getattr(sys, "frozen", False)
+    if os_name != "Darwin" or not running_from_bundle:
+        return
+
+    import ctypes.util
+    import pyre.zhelper
+
+    class AbsolutePathFallbackCDLL(ctypes.CDLL):
+        def __init__(self, name, *args, **kwargs):
+            try:
+                super().__init__(name, *args, **kwargs)
+            except Exception:
+                abs_library_path = ctypes.util.find_library(name)
+                if abs_library_path is not None:
+                    super().__init__(abs_library_path, *args, **kwargs)
+                else:
+                    raise
+
+    pyre.zhelper.CDLL = AbsolutePathFallbackCDLL
+
+
 if __name__ == "__main__":
     with Prevent_Idle_Sleep():
         pass
