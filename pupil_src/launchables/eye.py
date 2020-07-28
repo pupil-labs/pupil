290c3553385fcbca2bc29b1e18d7f3117a06171b
Roman Roibu <romanroibu@gmail.com>
2020-07-28 17:34:33 +0200

    Save unscaled window size in session settings on Windows
    This fixes the issue that a window closed on a scaled window would reopen bigger by the content scale factor on init.

--- pupil_src/launchables/eye.py
+++ pupil_src/launchables/eye.py
@@ -724,7 +724,13 @@ def eye(
             session_settings["window_position"] = glfw.glfwGetWindowPos(main_window)
             session_window_size = glfw.glfwGetWindowSize(main_window)
             if 0 not in session_window_size:
-                session_settings["window_size"] = session_window_size
+                f_width, f_height = session_window_size
+                if platform.system() == "Windows":
+                    f_width, f_height = (
+                        f_width / content_scale,
+                        f_height / content_scale,
+                    )
+                session_settings["window_size"] = int(f_width), int(f_height)
 
         session_settings.close()
 
