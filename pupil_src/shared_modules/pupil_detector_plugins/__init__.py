bfe21d4d4f4373f7c9113a63090da875caef958e
Pablo Prietz <pablo@prietz.org>
2020-12-02 12:42:30 +0100

    Add version check to pupil_detector_plugins

--- pupil_src/shared_modules/pupil_detector_plugins/__init__.py
+++ pupil_src/shared_modules/pupil_detector_plugins/__init__.py
@@ -12,10 +12,23 @@ import logging
 import traceback
 import typing as T
 
+import pupil_detectors
+from version_utils import parse_version
+
 from .detector_2d_plugin import Detector2DPlugin
 from .detector_base_plugin import EVENT_KEY, PupilDetectorPlugin
 
 logger = logging.getLogger(__name__)
+
+required_version_str = "2.0.0"
+if parse_version(pupil_detectors.__version__) < parse_version(required_version_str):
+    msg = (
+        f"This version of Pupil requires pupil_detectors >= {required_version_str}."
+        f" You are running with pupil_detectors == {pupil_detectors.__version__}."
+        f" Please upgrade to a newer version!"
+    )
+    logger.error(msg)
+    raise RuntimeError(msg)
 
 
 def available_detector_plugins() -> T.List[T.Type[PupilDetectorPlugin]]:
