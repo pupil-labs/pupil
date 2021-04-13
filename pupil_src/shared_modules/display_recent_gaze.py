0ed4b46bdeaa4c1fd4b7aa88ec772ea345674359
Roman Roibu <romanroibu@gmail.com>
2021-04-13 10:13:06 +0200

    Update Display_Recent_Gaze to draw gaze point with filled polygon

--- pupil_src/shared_modules/display_recent_gaze.py
+++ pupil_src/shared_modules/display_recent_gaze.py
@@ -11,6 +11,8 @@ See COPYING and COPYING.LESSER for license details.
 
 from plugin import System_Plugin_Base
 from pyglui.cygl.utils import draw_points_norm, RGBA
+from gl_utils import draw_circle_filled
+from methods import denormalize
 
 
 class Display_Recent_Gaze(System_Plugin_Base):
@@ -25,14 +27,23 @@ class Display_Recent_Gaze(System_Plugin_Base):
         self.pupil_display_list = []
 
     def recent_events(self, events):
+        frame = events.get("frame", None)
+        if not frame:
+            return
+        frame_size = frame.width, frame.height
         for pt in events.get("gaze", []):
-            self.pupil_display_list.append((pt["norm_pos"], pt["confidence"] * 0.8))
+            point = denormalize(pt["norm_pos"], frame_size, flip_y=True)
+            self.pupil_display_list.append((point, pt["confidence"] * 0.8))
         self.pupil_display_list[:-3] = []
 
     def gl_display(self):
         for pt, a in self.pupil_display_list:
             # This could be faster if there would be a method to also add multiple colors per point
-            draw_points_norm([pt], size=35, color=RGBA(1.0, 0.2, 0.4, a))
+            draw_circle_filled(
+                tuple(pt),
+                size=35 / 2,
+                color=RGBA(1.0, 0.2, 0.4, a),
+            )
 
     def get_init_dict(self):
         return {}
