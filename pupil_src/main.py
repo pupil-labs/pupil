b20310b563c1e75729a0b68b50817609e5c08bc0
Pablo Prietz <pablo@prietz.org>
2022-11-08 16:26:57 +0100

    Always use RichHandler in log_loop

--- pupil_src/main.py
+++ pupil_src/main.py
@@ -182,6 +182,7 @@ def launcher():
     # Recv log records from other processes.
     def log_loop(ipc_sub_url, log_level_debug):
         import logging
+        from rich.logging import RichHandler
 
         # Get the root logger
         logger = logging.getLogger()
@@ -200,24 +201,13 @@ def launcher():
         )
         logger.addHandler(fh)
         # Stream to console.
-        try:
-            from rich.logging import RichHandler
 
-            ch = RichHandler(
-                level=logging.DEBUG if log_level_debug else logging.INFO,
-                rich_tracebacks=False,
-            )
-        except ImportError:
-            ch = logging.StreamHandler()
-            ch.setFormatter(
-                logging.Formatter(
-                    "%(processName)s - [%(levelname)s] %(name)s: %(message)s"
-                )
-            )
-            if log_level_debug:
-                ch.setLevel(logging.DEBUG)
-            else:
-                ch.setLevel(logging.INFO)
+        ch = RichHandler(
+            level=logging.DEBUG if log_level_debug else logging.INFO,
+            rich_tracebacks=False,
+        )
+        ch.setFormatter(logging.Formatter("%(processName)s - %(name)s: %(message)s"))
+
         logger.addHandler(ch)
         # IPC setup to receive log messages. Use zmq_tools.ZMQ_handler to send messages to here.
         sub = zmq_tools.Msg_Receiver(zmq.Context(), ipc_sub_url, topics=("logging",))
