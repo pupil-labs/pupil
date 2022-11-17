abf7904bd5a64134d68a3a34b7a1425e9aca769b
Pablo Prietz <pablo@prietz.org>
2022-11-17 11:51:59 +0100

    GA: Checkout tags

--- deployment/reproducible_build.ps1
+++ deployment/reproducible_build.ps1
@@ -1,11 +1,12 @@
 Set-PSDebug -Trace 1
 $Env:PYTHONHASHSEED = 42
 
+$release_dir = "pupil_$(git describe --tags --long)_windows_x64"
+
 Write-Output "Downloading and installing packaging dependencies"
 Invoke-WebRequest "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-611.exe" -OutFile winrar.exe
 .\winrar.exe /s
 
-$release_dir = "pupil_$(git describe --tags --long)_windows_x64"
 Write-Output "Creating bundle at $release_dir"
 pyinstaller pupil_core.spec --noconfirm --log-level INFO --distpath $release_dir
 python generate_msi_installer.py
