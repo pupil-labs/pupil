b6c6d54f15c2662d6ca6577dcf69e99849cfeec2
Pablo Prietz <pablo@prietz.org>
2023-01-02 15:50:19 +0100

    Notarize macOS bundle

--- deployment/_packaging/macos.py
+++ deployment/_packaging/macos.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import pathlib
@@ -7,11 +8,12 @@ from . import ParsedVersion
 
 
 def package_bundles_as_dmg(base: pathlib.Path, version: ParsedVersion):
-    # for app in base.glob("*.app"):
-    #     sign_app(app)
+    for app in base.glob("*.app"):
+        sign_app(app)
     logging.info(f"Creating dmg file for Pupil Core {version}")
     dmg_file = create_dmg(create_and_fill_dmg_srcfolder(base), base.name, version)
     sign_object(dmg_file)
+    notarize_bundle(dmg_file)
 
 
 def create_and_fill_dmg_srcfolder(
@@ -77,6 +79,7 @@ def sign_object(path: pathlib.Path):
             "codesign",
             "--all-architectures",
             "--force",
+            "--timestamp",
             "--strict=all",
             "--options",
             "runtime",
@@ -91,3 +94,61 @@ def sign_object(path: pathlib.Path):
         ]
     )
     logging.info(f"Successfully signed '{path}'")
+
+
+def notarize_bundle(path: pathlib.Path):
+    logging.info(f"Attempting to notarize '{path}'")
+    auth_args = [
+        "--apple-id",
+        os.environ["MACOS_NOTARYTOOL_APPLE_ID"],
+        "--team-id",
+        os.environ["MACOS_NOTARYTOOL_TEAM_ID"],
+        "--password",
+        os.environ["MACOS_NOTARYTOOL_APPSPECIFIC_PASSWORD"],
+    ]
+    format_args = ["-f", "json"]
+    submit_result = subprocess.check_output(
+        ["xcrun", "notarytool", "submit", str(path), *auth_args, *format_args]
+    )
+    submit_result = json.loads(submit_result)
+    logging.info(f"{submit_result['message']} (ID: {submit_result['id']})")
+
+    try:
+        wait_result = subprocess.check_output(
+            [
+                "xcrun",
+                "notarytool",
+                "wait",
+                submit_result["id"],
+                "--timeout",
+                "1h",
+                *auth_args,
+                *format_args,
+            ]
+        )
+        wait_result = json.loads(wait_result)
+        logging.info(f"{wait_result['message']}. Status: {wait_result['status']}")
+        if wait_result["status"] == "Accepted":
+            staple_bundle_notarization(path)
+            logging.info(f"Successfully notarized '{path}'")
+
+    except subprocess.CalledProcessError:
+        logging.exception("Issue during processing notarization:")
+
+    log_result = subprocess.check_output(
+        [
+            "xcrun",
+            "notarytool",
+            "log",
+            submit_result["id"],
+            str(path.with_suffix(".json")),
+            *auth_args,
+            *format_args,
+        ]
+    )
+    log_result = json.loads(log_result)
+    logging.info(f"Notarization logs saved to {log_result['location']}")
+
+
+def staple_bundle_notarization(path: pathlib.Path):
+    subprocess.check_call(["xcrun", "stapler", "staple", "-v", str(path)])
