4ed286f706716a4987d5e8446c98955c577b23b7
Pablo Prietz <pablo@prietz.org>
2023-02-02 09:14:27 +0100

    Apply suggestions from @willpatera's review
    Co-authored-by: Will <willpatera@gmail.com>

--- pupil_src/shared_modules/pupil_recording/update/__init__.py
+++ pupil_src/shared_modules/pupil_recording/update/__init__.py
@@ -42,12 +42,12 @@ def update_recording(rec_dir: str):
 
     if recording_type == RecordingType.CLOUD_CSV_EXPORT:
         raise InvalidRecordingException(
-            "Pupil Player does not support\nPupil Cloud CSV exports",
+            "Pupil Cloud CSV exports are not supported in Pupil Player",
             recovery="Open the CSV files in the analysis tool of your choice instead",
         )
     if recording_type == RecordingType.NEON:
         raise InvalidRecordingException(
-            "Pupil Player does not support\nNeon Companion recordings",
+            "Neon Companion app recordings are not supported in Pupil Player",
             recovery="You can view them in Pupil Cloud instead",
         )
 
